hypothesis/h

Rename hashid -> id on the client-side

h/static/scripts/directive/annotation.coffee
@@ -61,7 +61,7 @@ AnnotationController = [
 
     model = $scope.annotationGet()
     if not model.group
-      model.group = group.focusedGroup().hashid
+      model.group = group.focusedGroup().id
 
     highlight = model.$highlight
     original = null

hypothesis/client

h/static/scripts/directive/annotation.coffee
@@ -60,7 +60,7 @@ AnnotationController = [
 
     model = $scope.annotationGet()
     if not model.group
-      model.group = groups.focused().hashid
+      model.group = groups.focused().id
 
     highlight = model.$highlight
     original = null