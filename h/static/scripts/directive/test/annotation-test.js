hypothesis/h

commit e334fea02412828eb27c1c44d79ba4ea223f4dae

Convert filters used only once in the annotation directive to plain functions
The annotation directive used a set of filters for formatting
document domains, titles and usernames.

Converting these to plain functions eliminates a bunch of boilerplate
and as noted at https://www.binpress.com/tutorial/speeding-up-angular-js-with-simple-optimizations/135
is a small performance optimization.

h/templates/client/annotation.html
@@ -10,7 +10,7 @@
         <a class="annotation-user"
            target="_blank"
            ng-href="{{::vm.serviceUrl}}u/{{vm.user()}}"
-           >{{vm.user() | persona}}</a>
+           >{{vm.username()}}</a>
       </span>
 
       <span class="annotation-collapsed-replies">
@@ -32,11 +32,11 @@
         </span>
         <i class="h-icon-border-color" ng-show="vm.isHighlight() && !vm.editing()" title="This is a highlight. Click 'edit' to add a note or tag."></i>
         <span class="annotation-citation"
-              ng-bind-html="vm.document() | documentTitle"
+              ng-bind-html="vm.documentTitle"
               ng-if="::!vm.isSidebar">
         </span>
         <span class="annotation-citation-domain"
-              ng-bind-html="vm.document() | documentDomain"
+              ng-bind-html="vm.documentDomain"
               ng-if="::!vm.isSidebar">
         </span>
       </span>
@@ -104,7 +104,7 @@
        ng-if="(vm.canCollapseBody || vm.form.tags.length) && !vm.editing()">
     <ul class="tag-list">
       <li class="tag-item" ng-repeat="tag in vm.form.tags">
-        <a href="{{::vm.serviceUrl}}stream?q=tag:'{{tag.text|urlencode}}'" target="_blank">{{tag.text}}</a>
+        <a ng-href="vm.tagStreamURL(tag.text)" target="_blank">{{tag.text}}</a>
       </li>
     </ul>
     <div class="u-stretch"></div>

h/static/scripts/directive/test/annotation-test.js
@@ -2,12 +2,30 @@
 'use strict';
 
 var Promise = require('core-js/library/es6/promise');
+var proxyquire = require('proxyquire');
 
 var events = require('../../events');
 var util = require('./util');
 
 var module = angular.mock.module;
 var inject = angular.mock.inject;
+
+/**
+ * Returns the annotation directive with helpers stubbed out.
+ */
+function annotationDirective() {
+  var noop = function () { return '' };
+
+  var annotation = proxyquire('../annotation', {
+    '../filter/document-domain': noop,
+    '../filter/document-title': noop,
+    '../filter/persona': {
+      username: noop,
+    }
+  });
+
+  return annotation.directive;
+}
 
 /** Return Angular's $compile service. */
 function compileService() {
@@ -534,7 +552,7 @@ describe('annotation', function() {
 
     before(function() {
       angular.module('h', [])
-        .directive('annotation', require('../annotation').directive);
+        .directive('annotation', annotationDirective());
     });
 
     beforeEach(module('h'));
@@ -588,16 +606,6 @@ describe('annotation', function() {
         setDefault: sandbox.stub()
       };
 
-      fakePersonaFilter = sandbox.stub().returnsArg(0);
-
-      fakeDocumentTitleFilter = function(arg) {
-        return '';
-      };
-
-      fakeDocumentDomainFilter = function(arg) {
-        return '';
-      };
-
       fakeSession = {
         state: {
           userid: 'acct:bill@localhost'
@@ -618,10 +626,6 @@ describe('annotation', function() {
         decayingInterval: function () {},
       };
 
-      fakeUrlEncodeFilter = function(v) {
-        return encodeURIComponent(v);
-      };
-
       fakeGroups = {
         focused: function() {
           return {};
@@ -636,14 +640,10 @@ describe('annotation', function() {
       $provide.value('flash', fakeFlash);
       $provide.value('momentFilter', fakeMomentFilter);
       $provide.value('permissions', fakePermissions);
-      $provide.value('personaFilter', fakePersonaFilter);
-      $provide.value('documentTitleFilter', fakeDocumentTitleFilter);
-      $provide.value('documentDomainFilter', fakeDocumentDomainFilter);
       $provide.value('session', fakeSession);
       $provide.value('settings', fakeSettings);
       $provide.value('tags', fakeTags);
       $provide.value('time', fakeTime);
-      $provide.value('urlencodeFilter', fakeUrlEncodeFilter);
       $provide.value('groups', fakeGroups);
     }));
 
@@ -1598,7 +1598,7 @@ describe('annotation', function() {
   describe('AnnotationController', function() {
     before(function() {
       angular.module('h', [])
-        .directive('annotation', require('../annotation').directive);
+        .directive('annotation', annotationDirective());
     });
 
     beforeEach(module('h'));

h/static/scripts/app.coffee
@@ -113,10 +113,6 @@ module.exports = angular.module('h', [
 .directive('topBar', require('./directive/top-bar'))
 
 .filter('converter', require('./filter/converter'))
-.filter('persona', require('./filter/persona').filter)
-.filter('urlencode', require('./filter/urlencode'))
-.filter('documentTitle', require('./filter/document-title'))
-.filter('documentDomain', require('./filter/document-domain'))
 
 .provider('identity', require('./identity'))
 

h/static/scripts/directive/annotation.js
@@ -4,7 +4,10 @@
 var Promise = require('core-js/library/es6/promise');
 
 var dateUtil = require('../date-util');
+var documentDomain = require('../filter/document-domain');
+var documentTitle = require('../filter/document-title');
 var events = require('../events');
+var persona = require('../filter/persona');
 
 /** Return a domainModel tags array from the given vm tags array.
  *
@@ -192,6 +195,10 @@ function updateViewModel($scope, time, domainModel, vm, permissions) {
      });
     updateTimestamp();
   }
+
+  var documentMetadata = extractDocumentMetadata(domainModel);
+  vm.documentTitle = documentTitle(documentMetadata);
+  vm.documentDomain = documentDomain(documentMetadata);
 }
 
 /** Return a vm tags array from the given domainModel tags array.
@@ -460,11 +467,6 @@ function AnnotationController(
     }, true);
   };
 
-  /** Return some metadata extracted from the annotated document. */
-  vm.document = function() {
-    return extractDocumentMetadata(domainModel);
-  };
-
   /**
     * @ngdoc method
     * @name annotation.AnnotationController#edit
@@ -728,6 +730,10 @@ function AnnotationController(
     return $q.when(tags.filter(query));
   };
 
+  vm.tagStreamURL = function(tag) {
+    return vm.serviceUrl + 'stream?q=tag:' + encodeURIComponent(tag);
+  };
+
   vm.target = function() {
     return domainModel.target;
   };
@@ -738,7 +744,11 @@ function AnnotationController(
 
   vm.user = function() {
     return domainModel.user;
-  }
+  };
+
+  vm.username = function() {
+    return persona.username(domainModel.user);
+  };
 
   /** Sets whether or not the controls for
    * expanding/collapsing the body of lengthy annotations

h/static/scripts/filter/document-domain.js
@@ -2,34 +2,31 @@
 
 var escapeHtml = require('escape-html');
 
-module.exports = function() {
-  /**
-   * Return a nice displayable string representation of a document's domain.
-   *
-   * @returns {String} The document's domain in braces, e.g. '(example.com)'.
-   *   Returns '' if the document has no domain or if the document's domain is
-   *   the same as its title (because we assume that the title is already
-   *   displayed elsewhere and displaying it twice would be redundant).
-   *
-   */
-  function documentDomainFilter(document) {
-    var uri = escapeHtml(document.uri || '');
-    var domain = escapeHtml(document.domain || '');
-    var title = escapeHtml(document.title || '');
+/**
+ * Return a nice displayable string representation of a document's domain.
+ *
+ * @returns {String} The document's domain in braces, e.g. '(example.com)'.
+ *   Returns '' if the document has no domain or if the document's domain is
+ *   the same as its title (because we assume that the title is already
+ *   displayed elsewhere and displaying it twice would be redundant).
+ *
+ */
+module.exports = function documentDomain(document) {
+  var uri = escapeHtml(document.uri || '');
+  var domain = escapeHtml(document.domain || '');
+  var title = escapeHtml(document.title || '');
 
-    if (uri.indexOf('file://') === 0 && title) {
-      var parts = uri.split('/');
-      var filename = parts[parts.length - 1];
-      if (filename) {
-        return '(' + decodeURIComponent(filename) + ')';
-      }
-    }
-
-    if (domain && domain !== title) {
-      return '(' + decodeURIComponent(domain) + ')';
-    } else {
-      return '';
+  if (uri.indexOf('file://') === 0 && title) {
+    var parts = uri.split('/');
+    var filename = parts[parts.length - 1];
+    if (filename) {
+      return '(' + decodeURIComponent(filename) + ')';
     }
   }
-  return documentDomainFilter;
+
+  if (domain && domain !== title) {
+    return '(' + decodeURIComponent(domain) + ')';
+  } else {
+    return '';
+  }
 };

h/static/scripts/filter/document-title.js
@@ -2,32 +2,29 @@
 
 var escapeHtml  = require('escape-html');
 
-module.exports = function() {
-  /**
-   * Return a nice displayable string representation of a document's title.
-   *
-   * @returns {String} The document's title preceded on "on " and hyperlinked
-   *   to the document's URI. If the document has no http(s) URI then don't
-   *   hyperlink the title. If the document has no title then return ''.
-   *
-   */
-  function documentTitleFilter(document) {
-    var title = escapeHtml(document.title || '');
-    var uri = escapeHtml(document.uri || '');
+/**
+ * Return a nice displayable string representation of a document's title.
+ *
+ * @returns {String} The document's title preceded on "on " and hyperlinked
+ *   to the document's URI. If the document has no http(s) URI then don't
+ *   hyperlink the title. If the document has no title then return ''.
+ *
+ */
+module.exports = function documentTitle(document) {
+  var title = escapeHtml(document.title || '');
+  var uri = escapeHtml(document.uri || '');
 
-    if (uri && !(uri.indexOf('http://') === 0 || uri.indexOf('https://') === 0)) {
-      // We only link to http(s) URLs.
-      uri = null;
-    }
-
-    if (title && uri) {
-      return ('on &ldquo;<a target="_blank" href="' + uri + '">' + title +
-              '</a>&rdquo;');
-    } else if (title) {
-      return 'on &ldquo;' + title + '&rdquo;';
-    } else {
-      return '';
-    }
+  if (uri && !(uri.indexOf('http://') === 0 || uri.indexOf('https://') === 0)) {
+    // We only link to http(s) URLs.
+    uri = null;
   }
-  return documentTitleFilter;
+
+  if (title && uri) {
+    return ('on &ldquo;<a target="_blank" href="' + uri + '">' + title +
+            '</a>&rdquo;');
+  } else if (title) {
+    return 'on &ldquo;' + title + '&rdquo;';
+  } else {
+    return '';
+  }
 };

h/static/scripts/filter/persona.js
@@ -17,17 +17,18 @@ function parseAccountID(user) {
   };
 }
 
+/**
+ * Returns the username part of an account ID or an empty string.
+ */
+function username(user) {
+  var account = parseAccountID(user);
+  if (!account) {
+    return '';
+  }
+  return account.username;
+}
+
 module.exports = {
   parseAccountID: parseAccountID,
-
-  /** Export parseAccountID() as an Angular filter */
-  filter: function () {
-    return function (user, part) {
-      var account = parseAccountID(user);
-      if (!account) {
-        return null;
-      }
-      return account[part || 'username'];
-    };
-  }
+  username: username,
 };

h/static/scripts/filter/test/document-domain-test.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var documentDomainFilterProvider = require('../document-domain');
+var documentDomain = require('../document-domain');
 
 describe('documentDomain', function() {
 
   it('returns the domain in braces', function() {
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       domain: 'example.com'
     });
 
@@ -13,7 +13,7 @@ describe('documentDomain', function() {
   });
 
   it('returns an empty string if domain and title are the same', function() {
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       domain: 'example.com',
       title: 'example.com'
     });
@@ -22,7 +22,7 @@ describe('documentDomain', function() {
   });
 
   it('returns an empty string if the document has no domain', function() {
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       title: 'example.com'
     });
 
@@ -30,7 +30,7 @@ describe('documentDomain', function() {
   });
 
   it('returns the filename for local documents with titles', function() {
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       title: 'example.com',
       uri: 'file:///home/seanh/MyFile.pdf'
     });
@@ -39,7 +39,7 @@ describe('documentDomain', function() {
   });
 
   it('replaces %20 with " "', function() {
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       title: 'example.com',
       uri: 'file:///home/seanh/My%20File.pdf'
     });
@@ -50,7 +50,7 @@ describe('documentDomain', function() {
   it('escapes HTML in the document domain', function() {
     var spamLink = '<a href="http://example.com/rubies">Buy rubies!!!</a>';
 
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       title: 'title',
       domain: '</a>' + spamLink
     });
@@ -61,7 +61,7 @@ describe('documentDomain', function() {
   it('escapes HTML in the document uri', function() {
     var spamLink = '<a href="http://example.com/rubies">Buy rubies!!!</a>';
 
-    var domain = documentDomainFilterProvider()({
+    var domain = documentDomain({
       title: 'title',
       uri: 'file:///home/seanh/' + spamLink
     });

hypothesis/client

commit 04e923744f0f544b6eddada55dcf34524e309582

Fix annotation tag links
The ng-href directive does not treat its contents as
an Angular expression.

Fixes a regression introduced in e334fea0241282

h/templates/client/annotation.html
@@ -104,7 +104,7 @@
        ng-if="(vm.canCollapseBody || vm.form.tags.length) && !vm.editing()">
     <ul class="tag-list">
       <li class="tag-item" ng-repeat="tag in vm.form.tags">
-        <a ng-href="vm.tagStreamURL(tag.text)" target="_blank">{{tag.text}}</a>
+        <a ng-href="{{vm.tagStreamURL(tag.text)}}" target="_blank">{{tag.text}}</a>
       </li>
     </ul>
     <div class="u-stretch"></div>

h/static/scripts/directive/test/annotation-test.js
@@ -1465,5 +1465,21 @@ describe('annotation', function() {
         });
       });
     });
+
+    describe('tag display', function () {
+      it('displays annotation tags', function () {
+        var directive = createDirective({
+          id: '1234',
+          tags: ['atag']
+        });
+        var links = [].slice.apply(directive.element[0].querySelectorAll('a'));
+        var tagLinks = links.filter(function (link) {
+          return link.textContent === 'atag';
+        });
+        assert.equal(tagLinks.length, 1);
+        assert.equal(tagLinks[0].href,
+                     'https://test.hypothes.is/stream?q=tag:atag');
+      });
+    });
   });
 });