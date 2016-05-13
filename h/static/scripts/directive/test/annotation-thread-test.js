fchasen/h

Render annotation thread using new threading infrastructure
This replaces the rendering of conversation threads using
the new threading implementation.

 * Add <annotation-thread> component to render Thread objects
   generated by rootThread.

 * Expose the Thread generated by the `rootThread` service on
   the scope so that <annotation-thread> can render it.

 * Remove logic and tests in <annotation> for updating reply
   counts and thread states as these are no longer needed.

h/static/scripts/directive/test/annotation-thread-test.js
@@ -1,0 +1,151 @@
+'use strict';
+
+var angular = require('angular');
+
+var annotationThread = require('../annotation-thread');
+var util = require('./util');
+
+function PageObject(element) {
+  this.annotations = function () {
+    return Array.from(element[0].querySelectorAll('annotation'));
+  };
+  this.replies = function () {
+    return Array.from(element[0].querySelectorAll('annotation-thread'));
+  };
+  this.replyList = function () {
+    return element[0].querySelector('.thread-replies');
+  };
+  this.isHidden = function (element) {
+    return element.classList.contains('ng-hide');
+  };
+}
+
+describe('annotationThread', function () {
+  before(function () {
+    angular.module('app', [])
+      .directive('annotationThread', annotationThread);
+  });
+
+  beforeEach(function () {
+    angular.mock.module('app');
+  });
+
+  it('renders the tree structure of parent and child annotations', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1', text: 'text'},
+        children: [{
+          id: '2',
+          annotation: {id: '2', text: 'areply'},
+          children: [],
+        }],
+        visible: true,
+      },
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.annotations().length, 2);
+    assert.equal(pageObject.replies().length, 1);
+  });
+
+  it('does not render hidden threads', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: false,
+        children: []
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.annotations().length, 1);
+    assert.isTrue(pageObject.isHidden(pageObject.annotations()[0]));
+  });
+
+  it('shows replies if not collapsed', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: true,
+        children: [{
+          id: '2',
+          annotation: {id: '2'},
+          children: [],
+          visible: true,
+        }],
+        collapsed: false,
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.replies().length, 1);
+  });
+
+  it('does not show replies if collapsed', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: true,
+        children: [{
+          id: '2',
+          annotation: {id: '2'},
+          children: [],
+          visible: true,
+        }],
+        collapsed: true,
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.isTrue(pageObject.isHidden(pageObject.replyList()));
+  });
+
+  describe('#toggleCollapsed', function () {
+    it('toggles replies', function () {
+      var onToggleReplies = sinon.stub();
+      var element = util.createDirective(document, 'annotationThread', {
+        thread: {
+          id: '123',
+          annotation: {id: '123'},
+          children: [],
+        },
+        onToggleReplies: {
+          args: ['id'],
+          callback: onToggleReplies,
+        }
+      });
+      element.ctrl.toggleCollapsed();
+      assert.calledWith(onToggleReplies, '123');
+    });
+  });
+
+  describe('#showThreadAndReplies', function () {
+    it('reveals all parents and replies', function () {
+      var onForceVisible = sinon.stub();
+      var thread = {
+        id: '123',
+        annotation: {id: '123'},
+        children: [{
+          id: 'child-id',
+          annotation: {id: 'child-id'},
+          children: [],
+        }],
+        parent: {
+          id: 'parent-id',
+          annotation: {id: 'parent-id'},
+        },
+      };
+      var element = util.createDirective(document, 'annotationThread', {
+        thread: thread,
+        onForceVisible: {
+          args: ['thread'],
+          callback: onForceVisible,
+        },
+      });
+      element.ctrl.showThreadAndReplies();
+      assert.calledWith(onForceVisible, thread.parent);
+      assert.calledWith(onForceVisible, thread);
+      assert.calledWith(onForceVisible, thread.children[0]);
+    });
+  });
+});

h/static/scripts/test/widget-controller-test.js
@@ -27,20 +27,20 @@ function FakeSearchClient(resource, opts) {
 inherits(FakeSearchClient, EventEmitter);
 
 describe('WidgetController', function () {
-  var $scope = null;
-  var $rootScope = null;
-  var fakeAnnotationMapper = null;
-  var fakeAnnotationUI = null;
-  var fakeCrossFrame = null;
-  var fakeDrafts = null;
-  var fakeStore = null;
-  var fakeStreamer = null;
-  var fakeStreamFilter = null;
-  var fakeThreading = null;
-  var fakeGroups = null;
-  var sandbox = null;
-  var viewer = null;
-  var fakeSettings = null;
+  var $rootScope;
+  var $scope;
+  var fakeAnnotationMapper;
+  var fakeAnnotationUI;
+  var fakeCrossFrame;
+  var fakeDrafts;
+  var fakeGroups;
+  var fakeRootThread;
+  var fakeSettings;
+  var fakeStore;
+  var fakeStreamer;
+  var fakeStreamFilter;
+  var sandbox;
+  var viewer;
 
   before(function () {
     angular.module('h', [])
@@ -63,7 +63,10 @@ describe('WidgetController', function () {
       unloadAnnotations: sandbox.spy()
     };
 
-    var store = createFakeStore({selectedAnnotationMap: {}});
+    var store = createFakeStore({
+      annotations: [{id: '123'}],
+      selectedAnnotationMap: {},
+    });
     fakeAnnotationUI = {
       clearSelectedAnnotations: sandbox.spy(),
       hasSelectedAnnotations: function () {
@@ -72,6 +75,7 @@ describe('WidgetController', function () {
       },
       getState: store.getState,
       subscribe: store.subscribe,
+      setState: store.setState,
       _select: function (ids) {
         if (!ids.length) {
           store.setState({selectedAnnotationMap: null});
@@ -84,6 +88,9 @@ describe('WidgetController', function () {
           }, {}),
         });
       },
+      addAnnotations: sinon.spy(),
+      setForceVisible: sinon.spy(),
+      setCollapsed: sinon.spy(),
     };
     fakeCrossFrame = {
       call: sinon.stub(),
@@ -103,17 +110,18 @@ describe('WidgetController', function () {
       getFilter: sandbox.stub().returns({})
     };
 
-    fakeThreading = {
-      root: {},
-      thread: sandbox.stub(),
-      annotationList: function () {
-        return [{id: '123'}];
-      },
-    };
-
     fakeGroups = {
       focused: function () { return {id: 'foo'}; },
       focus: sinon.stub(),
+    };
+
+    fakeRootThread = {
+      thread: sinon.stub().returns({
+        totalChildren: 0,
+      }),
+      // TODO - Test for usage
+      setSearchQuery: sinon.stub(),
+      sortBy: sinon.stub(),
     };
 
     fakeSettings = {
@@ -128,10 +136,10 @@ describe('WidgetController', function () {
     $provide.value('annotationUI', fakeAnnotationUI);
     $provide.value('crossframe', fakeCrossFrame);
     $provide.value('drafts', fakeDrafts);
+    $provide.value('rootThread', fakeRootThread);
     $provide.value('store', fakeStore);
     $provide.value('streamer', fakeStreamer);
     $provide.value('streamFilter', fakeStreamFilter);
-    $provide.value('threading', fakeThreading);
     $provide.value('groups', fakeGroups);
     $provide.value('settings', fakeSettings);
   }));
@@ -156,7 +164,7 @@ describe('WidgetController', function () {
       fakeCrossFrame.frames.push({uri: 'http://example.com/page-b'});
       $scope.$digest();
       assert.calledWith(fakeAnnotationMapper.unloadAnnotations,
-        fakeThreading.annotationList());
+        fakeAnnotationUI.getState().annotations);
     });
 
     it('loads all annotations for a frame', function () {
@@ -191,6 +199,10 @@ describe('WidgetController', function () {
         $scope.$digest();
       });
 
+      it('selectedAnnotationCount is > 0', function () {
+        assert.equal($scope.selectedAnnotationCount(), 1);
+      });
+
       it('switches to the selected annotation\'s group', function () {
         assert.calledWith(fakeGroups.focus, '__world__');
         assert.calledOnce(fakeAnnotationMapper.loadAnnotations);
@@ -215,6 +227,10 @@ describe('WidgetController', function () {
         fakeCrossFrame.frames = [{uri: uri}];
         fakeGroups.focused = function () { return { id: 'a-group' }; };
         $scope.$digest();
+      });
+
+      it('selectedAnnotationCount is 0', function () {
+        assert.equal($scope.selectedAnnotationCount(), 0);
       });
 
       it('fetches annotations for the current group', function () {
@@ -254,11 +270,7 @@ describe('WidgetController', function () {
         $$tag: 'atag',
         id: '123',
       };
-      fakeThreading.idTable = {
-        '123': {
-          message: annot,
-        },
-      };
+      fakeAnnotationUI.setState({annotations: [annot]});
       $scope.$digest();
       $rootScope.$broadcast(events.ANNOTATIONS_SYNCED, [{tag: 'atag'}]);
       assert.calledWith(fakeCrossFrame.call, 'focusAnnotations', ['atag']);
@@ -274,7 +286,7 @@ describe('WidgetController', function () {
 
       $scope.$broadcast(events.GROUP_FOCUSED);
       assert.calledWith(fakeAnnotationMapper.unloadAnnotations, [{id: '123'}]);
-      assert.calledWith(fakeThreading.thread, fakeDrafts.unsaved());
+      assert.calledWith(fakeAnnotationUI.addAnnotations, fakeDrafts.unsaved());
       $scope.$digest();
       assert.calledWith(loadSpy, [sinon.match({id: uri + '123'})]);
       assert.calledWith(loadSpy, [sinon.match({id: uri + '456'})]);
@@ -312,14 +324,14 @@ describe('WidgetController', function () {
   describe('direct linking messages', function () {
     it('displays a message if the selection is unavailable', function () {
       fakeAnnotationUI._select(['missing']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isTrue($scope.selectedAnnotationUnavailable());
     });
 
     it('does not show a message if the selection is available', function () {
       fakeAnnotationUI._select(['123']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isFalse($scope.selectedAnnotationUnavailable());
     });
@@ -335,7 +347,7 @@ describe('WidgetController', function () {
         status: 'signed-out'
       };
       fakeAnnotationUI._select(['123']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isTrue($scope.shouldShowLoggedOutMessage());
     });
@@ -345,7 +357,7 @@ describe('WidgetController', function () {
         status: 'signed-out'
       };
       fakeAnnotationUI._select(['missing']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
     });
@@ -364,7 +376,7 @@ describe('WidgetController', function () {
         status: 'signed-in'
       };
       fakeAnnotationUI._select(['123']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
     });
@@ -375,9 +387,26 @@ describe('WidgetController', function () {
       };
       delete fakeSettings.annotations;
       fakeAnnotationUI._select(['123']);
-      fakeThreading.idTable = {'123': {}};
+      fakeAnnotationUI.setState({annotations: [{id: '123'}]});
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
+    });
+  });
+
+  describe('#forceVisible', function () {
+    it('shows the thread', function () {
+      var thread = {id: '1'};
+      $scope.forceVisible(thread);
+      assert.calledWith(fakeAnnotationUI.setForceVisible, thread.id, true);
+    });
+
+    it('uncollapses the parent', function () {
+      var thread = {
+        id: '2',
+        parent: {id: '3'},
+      };
+      $scope.forceVisible(thread);
+      assert.calledWith(fakeAnnotationUI.setCollapsed, thread.parent.id, false);
     });
   });
 });

hypothesis/client

h/static/scripts/directive/test/annotation-thread-test.js
@@ -1,0 +1,152 @@
+'use strict';
+
+var angular = require('angular');
+
+var annotationThread = require('../annotation-thread');
+var util = require('./util');
+
+function PageObject(element) {
+  this.annotations = function () {
+    return Array.from(element[0].querySelectorAll('annotation'));
+  };
+  this.replies = function () {
+    return Array.from(element[0].querySelectorAll('annotation-thread'));
+  };
+  this.replyList = function () {
+    return element[0].querySelector('.thread-replies');
+  };
+  this.isHidden = function (element) {
+    return element.classList.contains('ng-hide');
+  };
+}
+
+describe('annotationThread', function () {
+  before(function () {
+    angular.module('app', [])
+      .directive('annotationThread', annotationThread);
+  });
+
+  beforeEach(function () {
+    angular.mock.module('app');
+  });
+
+  it('renders the tree structure of parent and child annotations', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1', text: 'text'},
+        children: [{
+          id: '2',
+          annotation: {id: '2', text: 'areply'},
+          children: [],
+        }],
+        visible: true,
+      },
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.annotations().length, 2);
+    assert.equal(pageObject.replies().length, 1);
+  });
+
+  it('does not render hidden threads', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: false,
+        children: []
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.annotations().length, 1);
+    assert.isTrue(pageObject.isHidden(pageObject.annotations()[0]));
+  });
+
+  it('shows replies if not collapsed', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: true,
+        children: [{
+          id: '2',
+          annotation: {id: '2'},
+          children: [],
+          visible: true,
+        }],
+        collapsed: false,
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.equal(pageObject.replies().length, 1);
+  });
+
+  it('does not show replies if collapsed', function () {
+    var element = util.createDirective(document, 'annotationThread', {
+      thread: {
+        id: '1',
+        annotation: {id: '1'},
+        visible: true,
+        children: [{
+          id: '2',
+          annotation: {id: '2'},
+          children: [],
+          visible: true,
+        }],
+        collapsed: true,
+      }
+    });
+    var pageObject = new PageObject(element);
+    assert.isTrue(pageObject.isHidden(pageObject.replyList()));
+  });
+
+  describe('#toggleCollapsed', function () {
+    it('toggles replies', function () {
+      var onChangeCollapsed = sinon.stub();
+      var element = util.createDirective(document, 'annotationThread', {
+        thread: {
+          id: '123',
+          annotation: {id: '123'},
+          children: [],
+          collapsed: true,
+        },
+        onChangeCollapsed: {
+          args: ['id', 'collapsed'],
+          callback: onChangeCollapsed,
+        }
+      });
+      element.ctrl.toggleCollapsed();
+      assert.calledWith(onChangeCollapsed, '123', false);
+    });
+  });
+
+  describe('#showThreadAndReplies', function () {
+    it('reveals all parents and replies', function () {
+      var onForceVisible = sinon.stub();
+      var thread = {
+        id: '123',
+        annotation: {id: '123'},
+        children: [{
+          id: 'child-id',
+          annotation: {id: 'child-id'},
+          children: [],
+        }],
+        parent: {
+          id: 'parent-id',
+          annotation: {id: 'parent-id'},
+        },
+      };
+      var element = util.createDirective(document, 'annotationThread', {
+        thread: thread,
+        onForceVisible: {
+          args: ['thread'],
+          callback: onForceVisible,
+        },
+      });
+      element.ctrl.showThreadAndReplies();
+      assert.calledWith(onForceVisible, thread.parent);
+      assert.calledWith(onForceVisible, thread);
+      assert.calledWith(onForceVisible, thread.children[0]);
+    });
+  });
+});

h/static/scripts/test/widget-controller-test.js
@@ -27,20 +27,20 @@ function FakeSearchClient(resource, opts) {
 inherits(FakeSearchClient, EventEmitter);
 
 describe('WidgetController', function () {
-  var $scope = null;
-  var $rootScope = null;
-  var annotationUI = null;
-  var fakeAnnotationMapper = null;
-  var fakeCrossFrame = null;
-  var fakeDrafts = null;
-  var fakeStore = null;
-  var fakeStreamer = null;
-  var fakeStreamFilter = null;
-  var fakeThreading = null;
-  var fakeGroups = null;
-  var sandbox = null;
-  var viewer = null;
-  var fakeSettings = null;
+  var $rootScope;
+  var $scope;
+  var annotationUI;
+  var fakeAnnotationMapper;
+  var fakeCrossFrame;
+  var fakeDrafts;
+  var fakeGroups;
+  var fakeRootThread;
+  var fakeSettings;
+  var fakeStore;
+  var fakeStreamer;
+  var fakeStreamFilter;
+  var sandbox;
+  var viewer;
 
   before(function () {
     angular.module('h', [])
@@ -64,12 +64,13 @@ describe('WidgetController', function () {
     };
 
     annotationUI = annotationUIFactory({});
+
     fakeCrossFrame = {
       call: sinon.stub(),
       frames: [],
     };
     fakeDrafts = {
-      unsaved: sandbox.stub()
+      unsaved: sandbox.stub().returns([]),
     };
 
     fakeStreamer = {
@@ -82,17 +83,17 @@ describe('WidgetController', function () {
       getFilter: sandbox.stub().returns({})
     };
 
-    fakeThreading = {
-      root: {},
-      thread: sandbox.stub(),
-      annotationList: function () {
-        return [{id: '123'}];
-      },
-    };
-
     fakeGroups = {
       focused: function () { return {id: 'foo'}; },
       focus: sinon.stub(),
+    };
+
+    fakeRootThread = {
+      thread: sinon.stub().returns({
+        totalChildren: 0,
+      }),
+      setSearchQuery: sinon.stub(),
+      sortBy: sinon.stub(),
     };
 
     fakeSettings = {
@@ -107,10 +108,10 @@ describe('WidgetController', function () {
     $provide.value('annotationUI', annotationUI);
     $provide.value('crossframe', fakeCrossFrame);
     $provide.value('drafts', fakeDrafts);
+    $provide.value('rootThread', fakeRootThread);
     $provide.value('store', fakeStore);
     $provide.value('streamer', fakeStreamer);
     $provide.value('streamFilter', fakeStreamFilter);
-    $provide.value('threading', fakeThreading);
     $provide.value('groups', fakeGroups);
     $provide.value('settings', fakeSettings);
   }));
@@ -129,13 +130,14 @@ describe('WidgetController', function () {
     it('unloads any existing annotations', function () {
       // When new clients connect, all existing annotations should be unloaded
       // before reloading annotations for each currently-connected client
+      annotationUI.addAnnotations([{id: '123'}]);
       fakeCrossFrame.frames.push({uri: 'http://example.com/page-a'});
       $scope.$digest();
       fakeAnnotationMapper.unloadAnnotations = sandbox.spy();
       fakeCrossFrame.frames.push({uri: 'http://example.com/page-b'});
       $scope.$digest();
       assert.calledWith(fakeAnnotationMapper.unloadAnnotations,
-        fakeThreading.annotationList());
+        annotationUI.getState().annotations);
     });
 
     it('loads all annotations for a frame', function () {
@@ -170,6 +172,10 @@ describe('WidgetController', function () {
         $scope.$digest();
       });
 
+      it('selectedAnnotationCount is > 0', function () {
+        assert.equal($scope.selectedAnnotationCount(), 1);
+      });
+
       it('switches to the selected annotation\'s group', function () {
         assert.calledWith(fakeGroups.focus, '__world__');
         assert.calledOnce(fakeAnnotationMapper.loadAnnotations);
@@ -194,6 +200,10 @@ describe('WidgetController', function () {
         fakeCrossFrame.frames = [{uri: uri}];
         fakeGroups.focused = function () { return { id: 'a-group' }; };
         $scope.$digest();
+      });
+
+      it('selectedAnnotationCount is 0', function () {
+        assert.equal($scope.selectedAnnotationCount(), 0);
       });
 
       it('fetches annotations for the current group', function () {
@@ -233,11 +243,7 @@ describe('WidgetController', function () {
         $$tag: 'atag',
         id: '123',
       };
-      fakeThreading.idTable = {
-        '123': {
-          message: annot,
-        },
-      };
+      annotationUI.addAnnotations([annot]);
       $scope.$digest();
       $rootScope.$broadcast(events.ANNOTATIONS_SYNCED, [{tag: 'atag'}]);
       assert.calledWith(fakeCrossFrame.call, 'focusAnnotations', ['atag']);
@@ -248,12 +254,18 @@ describe('WidgetController', function () {
   describe('when the focused group changes', function () {
     it('should load annotations for the new group', function () {
       var uri = 'http://example.com';
+
+      annotationUI.addAnnotations([{id: '123'}]);
+      annotationUI.addAnnotations = sinon.stub();
+
+      fakeDrafts.unsaved.returns([{id: uri + '123'}, {id: uri + '456'}]);
+
       fakeCrossFrame.frames.push({uri: uri});
       var loadSpy = fakeAnnotationMapper.loadAnnotations;
 
       $scope.$broadcast(events.GROUP_FOCUSED);
       assert.calledWith(fakeAnnotationMapper.unloadAnnotations, [{id: '123'}]);
-      assert.calledWith(fakeThreading.thread, fakeDrafts.unsaved());
+      assert.calledWith(annotationUI.addAnnotations, fakeDrafts.unsaved());
       $scope.$digest();
       assert.calledWith(loadSpy, [sinon.match({id: uri + '123'})]);
       assert.calledWith(loadSpy, [sinon.match({id: uri + '456'})]);
@@ -291,14 +303,13 @@ describe('WidgetController', function () {
   describe('direct linking messages', function () {
     it('displays a message if the selection is unavailable', function () {
       annotationUI.selectAnnotations(['missing']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isTrue($scope.selectedAnnotationUnavailable());
     });
 
     it('does not show a message if the selection is available', function () {
+      annotationUI.addAnnotations([{id: '123'}]);
       annotationUI.selectAnnotations(['123']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isFalse($scope.selectedAnnotationUnavailable());
     });
@@ -313,8 +324,8 @@ describe('WidgetController', function () {
       $scope.auth = {
         status: 'signed-out'
       };
+      annotationUI.addAnnotations([{id: '123'}]);
       annotationUI.selectAnnotations(['123']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isTrue($scope.shouldShowLoggedOutMessage());
     });
@@ -324,7 +335,6 @@ describe('WidgetController', function () {
         status: 'signed-out'
       };
       annotationUI.selectAnnotations(['missing']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
     });
@@ -342,8 +352,8 @@ describe('WidgetController', function () {
       $scope.auth = {
         status: 'signed-in'
       };
+      annotationUI.addAnnotations([{id: '123'}]);
       annotationUI.selectAnnotations(['123']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
     });
@@ -353,10 +363,28 @@ describe('WidgetController', function () {
         status: 'signed-out'
       };
       delete fakeSettings.annotations;
+      annotationUI.addAnnotations([{id: '123'}]);
       annotationUI.selectAnnotations(['123']);
-      fakeThreading.idTable = {'123': {}};
       $scope.$digest();
       assert.isFalse($scope.shouldShowLoggedOutMessage());
+    });
+  });
+
+  describe('#forceVisible', function () {
+    it('shows the thread', function () {
+      var thread = {id: '1'};
+      $scope.forceVisible(thread);
+      assert.deepEqual(annotationUI.getState().forceVisible, {1: true});
+    });
+
+    it('uncollapses the parent', function () {
+      var thread = {
+        id: '2',
+        parent: {id: '3'},
+      };
+      assert.equal(annotationUI.getState().expanded[thread.parent.id], undefined);
+      $scope.forceVisible(thread);
+      assert.equal(annotationUI.getState().expanded[thread.parent.id], true);
     });
   });
 });