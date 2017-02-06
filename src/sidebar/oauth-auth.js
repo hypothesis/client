5691c2426e63e4c51c41f420910b70c2ab902ae2
Sean Hammond <git@snhmnd.fastmail.com>
2017-02-06 17:10:05 +0000

    Deduplicate code in the oauth-auth service

--- src/sidebar/oauth-auth.js
+++ src/sidebar/oauth-auth.js
@@ -16,50 +16,62 @@ function auth($http, settings) {
   var cachedToken;
   var tokenUrl = resolve('token', settings.apiUrl);
 
-  // Exchange the JWT grant token for an access token.
-  // See https://tools.ietf.org/html/rfc7523#section-4
-  function exchangeToken(grantToken) {
-    var data = queryString.stringify({
-      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-      assertion: grantToken,
-    });
+  // Return a cachedToken object (of the form taken by the cachedToken variable
+  // in this module) from the given tokenURL endpoint response body.
+  function cachedTokenFromData(data) {
+    return {
+      token:        data.access_token,
+      refreshToken: data.refresh_token,
+      expires_in:   data.expires_in,
+    };
+  }
+
+  // Post the given data to the tokenUrl endpoint as a form submission.
+  // Return a Promise for the POST request.
+  function postToTokenUrl(data) {
+    data = queryString.stringify(data);
     var requestConfig = {
       headers: {'Content-Type': 'application/x-www-form-urlencoded'},
     };
-    return $http.post(tokenUrl, data, requestConfig)
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error('Failed to retrieve access token');
-        }
-        return response.data;
-      });
+
+    return $http.post(tokenUrl, data, requestConfig);
   }
 
   // Exchange the refresh token for a new access token and refresh token pair.
   // See https://tools.ietf.org/html/rfc6749#section-6
   function refreshAccessToken(refreshToken) {
-    var data = queryString.stringify({
-      grant_type: 'refresh_token',
-      refresh_token: refreshToken,
+    var data = {grant_type: 'refresh_token', refresh_token: refreshToken};
+    postToTokenUrl(data).then(function (response) {
+      cachedToken = cachedTokenFromData(response.data);
+      refreshAccessTokenBeforeItExpires(cachedToken);
     });
+  }
 
-    var requestConfig = {
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+  // Set a timeout to refresh the access token a few minutes before it expires.
+  function refreshAccessTokenBeforeItExpires(cachedToken) {
+    var delay = cachedToken.expires_in * 1000;
+
+    // We actually have to refresh the access token _before_ it expires.
+    // If the access token expires in one hour, this should refresh it in
+    // about 55 mins.
+    delay = Math.floor(delay * 0.91);
+
+    window.setTimeout(refreshAccessToken, delay, cachedToken.refreshToken);
+  }
+
+  // Exchange the JWT grant token for an access token.
+  // See https://tools.ietf.org/html/rfc7523#section-4
+  function exchangeToken(grantToken) {
+    var data = {
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: grantToken,
     };
-
-    $http.post(tokenUrl, data, requestConfig).then(function (response) {
-      cachedToken.token = response.data.access_token;
-      cachedToken.refreshToken = response.data.refresh_token;
-
-      var delay = response.expires_in * 1000;
-      // We actually have to refresh the access token _before_ it expires.
-      // If the access token expires in one hour, this should refresh it in
-      // about 55 mins.
-      delay = Math.floor(delay * 0.91);
-
-      window.setTimeout(refreshAccessToken, delay, cachedToken.refreshToken);
+    return postToTokenUrl(data).then(function (response) {
+      if (response.status !== 200) {
+        throw new Error('Failed to retrieve access token');
+      }
+      return response.data;
     });
-
   }
 
   function tokenGetter() {
@@ -77,19 +89,8 @@ function auth($http, settings) {
       }
 
       return exchangeToken(grantToken).then(function (tokenInfo) {
-        cachedToken = {
-          token: tokenInfo.access_token,
-          refreshToken: tokenInfo.refresh_token,
-        };
-
-        var delay = tokenInfo.expires_in;
-        // We actually have to refresh the access token _before_ it expires.
-        // If the access token expires in one hour, this should refresh it in
-        // about 55 mins.
-        delay = Math.floor(delay * 0.91);
-
-        window.setTimeout(refreshAccessToken, delay, cachedToken.refreshToken);
-
+        cachedToken = cachedTokenFromData(tokenInfo);
+        refreshAccessTokenBeforeItExpires(cachedToken);
         return cachedToken.token;
       });
     }
