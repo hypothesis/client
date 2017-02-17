edaa1bccf6cefc6b9204a09e6bd97890fea66969
Robert Knight <robertknight@gmail.com>
2017-02-17 13:01:19 +0000

    Make package content server behave more like unpkg
    Make the package content server mirror unpkg's behavior in that:
    
     - The entry point for a package is located at `/{packageName}@{version}`
    
     - Requests without a package version specified redirect to the
       corresponding path for the latest package version

--- scripts/gulp/serve-package.js
+++ scripts/gulp/serve-package.js
@@ -1,15 +1,19 @@
 'use strict';
 
+var { readFileSync } = require('fs');
+
 var express = require('express');
 var { log } = require('gulp-util');
-var { readFileSync } = require('fs');
+
+var { version } = require('../../package.json');
 
 /**
  * An express server which serves the contents of the package.
  *
- * The server behaves similarly to npm CDNs, such as unpkg.com. The '/' route
- * returns the contents of the package's entry point. Other routes return the
- * contents of the specified file within the package.
+ * The server behaves similarly to npm CDNs, such as unpkg.com. The
+ * '/hypothesis@VERSION' route returns the contents of the package's entry
+ * point. Other routes return the contents of the specified file within the
+ * package.
  *
  * When developing the client, the Hypothesis service should be configured to
  * use the URL of this service as the client URL, so that the boot script is
@@ -19,13 +23,23 @@ var { readFileSync } = require('fs');
 function servePackage(port) {
   var app = express();
 
-  app.get('/', function (req, res) {
+  // Set up redirects which mirror unpkg's behavior
+  app.get('/hypothesis', function (req, res) {
+    res.redirect(302, `/hypothesis@${version}`);
+  });
+
+  app.get('/hypothesis/*', function (req, res) {
+    var path = req.path.replace('/hypothesis/', '');
+    res.redirect(302, `/hypothesis@${version}/${path}`);
+  });
+
+  app.get(`/hypothesis@${version}`, function (req, res) {
     var entryPath = require.resolve('../..');
     var entryScript = readFileSync(entryPath).toString('utf-8');
     res.send(entryScript);
   });
 
-  app.use(express.static('.'));
+  app.use(`/hypothesis@${version}/`, express.static('.'));
 
   app.listen(port, function () {
     log(`Package served at http://localhost:${port}`);
