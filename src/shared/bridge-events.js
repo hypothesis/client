473fa634b32913866252f4ec28321e6ef8b83fd6
Sean Hammond <git@snhmnd.fastmail.com>
2017-02-22 17:09:13 +0000

    Separate bridge events by direction
    Separate bridge events sent from sidebar to annotator and from annotator
    to sidebar into two separate sections in the source code. Just to make
    documentation of the events clearer.

--- src/shared/bridge-events.js
+++ src/shared/bridge-events.js
@@ -5,6 +5,12 @@
  * across the bridge between the sidebar and annotator
  */
 module.exports = {
+  // Events that the sidebar sends to the annotator
+  // ----------------------------------------------
+
   /** The set of annotations was updated. */
   PUBLIC_ANNOTATION_COUNT_CHANGED: 'publicAnnotationCountChanged',
+
+  // Events that the annotator sends to the sidebar
+  // ----------------------------------------------
 };
