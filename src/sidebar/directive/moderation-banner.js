e0a71995c313a0ee5127113cc39f75c50c654470
Robert Knight <robertknight@gmail.com>
2017-03-13 16:59:44 +0000

    Implement <moderation-banner> component
    Implement the UI component for the moderation banner at the top of
    annotation cards, following the designs on [1].
    
    [1] https://github.com/hypothesis/product-backlog/issues/183

---
+++ src/sidebar/directive/moderation-banner.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// @ngInject
+function ModerationBannerController(annotationUI, flash, store) {
+  this.flagCount = function () {
+    return annotationUI.flagCount(this.annotationId);
+  };
+
+  this.isHidden = function () {
+    return annotationUI.isHiddenByModerator(this.annotationId);
+  };
+
+  /**
+   * Hide an annotation from non-moderator users.
+   */
+  this.hideAnnotation = function () {
+    store.annotation.hide({id: this.annotationId}).then(function () {
+      annotationUI.annotationHiddenChanged(this.annotationId, true);
+    }).catch(function (err) {
+      flash.error(err.message);
+    });
+  };
+
+  /**
+   * Un-hide an annotation from non-moderator users.
+   */
+  this.unhideAnnotation = function () {
+    store.annotation.unhide({id: this.annotationId}).then(function () {
+      annotationUI.annotationHiddenChanged(this.annotationId, false);
+    }).catch(function (err) {
+      flash.error(err.message);
+    });
+  };
+}
+
+/**
+ * Banner shown above flagged annotations to allow moderators to hide/unhide the
+ * annotation from other users.
+ */
+function moderationBanner() {
+  return {
+    restrict: 'E',
+    bindToController: true,
+    controllerAs: 'vm',
+    controller: ModerationBannerController,
+    template: require('../templates/moderation_banner.html'),
+    scope: {
+      /**
+       * The ID of the annotation whose moderation status the banner should
+       * reflect.
+       */
+      annotationId: '<',
+    },
+  };
+}
+
+module.exports = moderationBanner;
