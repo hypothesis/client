33f0450a446817ccf39be222bf3900d3cbab869a
Robert Knight <robertknight@gmail.com>
2017-03-16 06:51:42 +0000

    Convert `<excerpt>` to a component

--- src/sidebar/app.js
+++ src/sidebar/app.js
@@ -140,6 +140,7 @@ module.exports = angular.module('h', [
   .component('annotationShareDialog', require('./components/annotation-share-dialog'))
   .component('annotationThread', require('./components/annotation-thread'))
   .component('dropdownMenuBtn', require('./components/dropdown-menu-btn'))
+  .component('excerpt', require('./directive/excerpt').component)
   .component('groupList', require('./components/group-list'))
   .component('helpLink', require('./components/help-link'))
   .component('helpPanel', require('./components/help-panel'))
@@ -163,7 +164,6 @@ module.exports = angular.module('h', [
   .directive('markdown', require('./directive/markdown'))
   .directive('topBar', require('./directive/top-bar'))
 
-  .directive('excerpt', require('./directive/excerpt').directive)
   .directive('formInput', require('./directive/form-input'))
   .directive('formValidate', require('./directive/form-validate'))
   .directive('hAutofocus', require('./directive/h-autofocus'))
--- src/sidebar/directive/excerpt.js
+++ src/sidebar/directive/excerpt.js
@@ -1,6 +1,9 @@
 'use strict';
 
-function ExcerptController() {
+// @ngInject
+function ExcerptController($element, $scope, ExcerptOverflowMonitor) {
+  var self = this;
+
   if (this.collapse === undefined) {
     this.collapse = true;
   }
@@ -47,144 +50,135 @@ function ExcerptController() {
       'is-hidden': !this.isExpandable(),
     };
   };
+
+  // Test if the element or any of its parents have been hidden by
+  // an 'ng-show' directive
+  function isElementHidden() {
+    var el = $element[0];
+    while (el) {
+      if (el.classList.contains('ng-hide')) {
+        return true;
+      }
+      el = el.parentElement;
+    }
+    return false;
+  }
+
+  var overflowMonitor = new ExcerptOverflowMonitor({
+    getState: function () {
+      return {
+        enabled: self.enabled,
+        animate: self.animate,
+        collapsedHeight: self.collapsedHeight,
+        collapse: self.collapse,
+        overflowHysteresis: self.overflowHysteresis,
+      };
+    },
+    contentHeight: function () {
+      var contentElem = $element[0].querySelector('.excerpt');
+      if (!contentElem) {
+        return null;
+      }
+      return contentElem.scrollHeight;
+    },
+    onOverflowChanged: function (overflowing) {
+      self.overflowing = overflowing;
+      if (self.onCollapsibleChanged) {
+        self.onCollapsibleChanged({collapsible: overflowing});
+      }
+      // Even though this change happens outside the framework, we use
+      // $digest() rather than $apply() here to avoid a large number of full
+      // digest cycles if many excerpts update their overflow state at the
+      // same time. The onCollapsibleChanged() handler, if any, is
+      // responsible for triggering any necessary digests in parent scopes.
+      $scope.$digest();
+    },
+  }, window.requestAnimationFrame);
+
+  this.contentStyle = overflowMonitor.contentStyle;
+
+  // Listen for document events which might affect whether the excerpt
+  // is overflowing, even if its content has not changed.
+  $element[0].addEventListener('load', overflowMonitor.check, false /* capture */);
+  window.addEventListener('resize', overflowMonitor.check);
+  $scope.$on('$destroy', function () {
+    window.removeEventListener('resize', overflowMonitor.check);
+  });
+
+  // Watch for changes to the visibility of the excerpt.
+  // Unfortunately there is no DOM API for this, so we rely on a digest
+  // being triggered after the visibility changes.
+  $scope.$watch(isElementHidden, function (hidden) {
+    if (!hidden) {
+      overflowMonitor.check();
+    }
+  });
+
+  // Watch input properties which may affect the overflow state
+  $scope.$watch('vm.contentData', overflowMonitor.check);
+  $scope.$watch('vm.enabled', overflowMonitor.check);
+
+  // Trigger an initial calculation of the overflow state.
+  //
+  // This is performed asynchronously so that the content of the <excerpt>
+  // has settled - ie. all Angular directives have been fully applied and
+  // the DOM has stopped changing. This may take several $digest cycles.
+  overflowMonitor.check();
 }
 
 /**
- * @ngdoc directive
- * @name excerpt
- * @restrict E
- * @description This directive truncates the height of its contents to a
+ * @description This component truncates the height of its contents to a
  *              specified number of lines and provides controls for expanding
  *              and collapsing the resulting truncated element.
  */
-// @ngInject
-function excerpt(ExcerptOverflowMonitor) {
-  return {
-    bindToController: true,
-    controller: ExcerptController,
-    controllerAs: 'vm',
-    link: function (scope, elem, attrs, ctrl) {
-      // Test if the element or any of its parents have been hidden by
-      // an 'ng-show' directive
-      function isElementHidden() {
-        var el = elem[0];
-        while (el) {
-          if (el.classList.contains('ng-hide')) {
-            return true;
-          }
-          el = el.parentElement;
-        }
-        return false;
-      }
-
-      var overflowMonitor = new ExcerptOverflowMonitor({
-        getState: function () {
-          return {
-            enabled: ctrl.enabled,
-            animate: ctrl.animate,
-            collapsedHeight: ctrl.collapsedHeight,
-            collapse: ctrl.collapse,
-            overflowHysteresis: ctrl.overflowHysteresis,
-          };
-        },
-        contentHeight: function () {
-          var contentElem = elem[0].querySelector('.excerpt');
-          if (!contentElem) {
-            return null;
-          }
-          return contentElem.scrollHeight;
-        },
-        onOverflowChanged: function (overflowing) {
-          ctrl.overflowing = overflowing;
-          if (ctrl.onCollapsibleChanged) {
-            ctrl.onCollapsibleChanged({collapsible: overflowing});
-          }
-          // Even though this change happens outside the framework, we use
-          // $digest() rather than $apply() here to avoid a large number of full
-          // digest cycles if many excerpts update their overflow state at the
-          // same time. The onCollapsibleChanged() handler, if any, is
-          // responsible for triggering any necessary digests in parent scopes.
-          scope.$digest();
-        },
-      }, window.requestAnimationFrame);
-
-      ctrl.contentStyle = overflowMonitor.contentStyle;
-
-      // Listen for document events which might affect whether the excerpt
-      // is overflowing, even if its content has not changed.
-      elem[0].addEventListener('load', overflowMonitor.check, false /* capture */);
-      window.addEventListener('resize', overflowMonitor.check);
-      scope.$on('$destroy', function () {
-        window.removeEventListener('resize', overflowMonitor.check);
-      });
-
-      // Watch for changes to the visibility of the excerpt.
-      // Unfortunately there is no DOM API for this, so we rely on a digest
-      // being triggered after the visibility changes.
-      scope.$watch(isElementHidden, function (hidden) {
-        if (!hidden) {
-          overflowMonitor.check();
-        }
-      });
-
-      // Watch input properties which may affect the overflow state
-      scope.$watch('vm.contentData', overflowMonitor.check);
-      scope.$watch('vm.enabled', overflowMonitor.check);
-
-      // Trigger an initial calculation of the overflow state.
-      //
-      // This is performed asynchronously so that the content of the <excerpt>
-      // has settled - ie. all Angular directives have been fully applied and
-      // the DOM has stopped changing. This may take several $digest cycles.
-      overflowMonitor.check();
-    },
-    scope: {
-      /** Whether or not expansion should be animated. Defaults to true. */
-      animate: '<?',
-      /**
-       * The data which is used to generate the excerpt's content.
-       * When this changes, the excerpt will recompute whether the content
-       * is overflowing.
-       */
-      contentData: '<',
-      /** Whether or not truncation should be enabled */
-      enabled: '<?',
-      /**
-       * Specifies whether controls to expand and collapse
-       * the excerpt should be shown inside the <excerpt> component.
-       * If false, external controls can expand/collapse the excerpt by
-       * setting the 'collapse' property.
-       */
-      inlineControls: '<',
-      /** Sets whether or not the excerpt is collapsed. */
-      collapse: '=?',
-      /**
-       * Called when the collapsibility of the excerpt (that is, whether or
-       * not the content height exceeds the collapsed height), changes.
-       *
-       * Note: This function is *not* called from inside a digest cycle,
-       * the caller is responsible for triggering any necessary digests.
-       */
-      onCollapsibleChanged: '&?',
-      /** The height of this container in pixels when collapsed.
-       */
-      collapsedHeight: '<',
-      /**
-       * The number of pixels by which the height of the excerpt's content
-       * must extend beyond the collapsed height in order for truncation to
-       * be activated. This prevents the 'More' link from being shown to expand
-       * the excerpt if it has only been truncated by a very small amount, such
-       * that expanding the excerpt would reveal no extra lines of text.
-       */
-      overflowHysteresis: '<?',
-    },
-    restrict: 'E',
-    transclude: true,
-    template: require('../templates/excerpt.html'),
-  };
-}
+var component = {
+  controller: ExcerptController,
+  controllerAs: 'vm',
+  bindings: {
+    /** Whether or not expansion should be animated. Defaults to true. */
+    animate: '<?',
+    /**
+     * The data which is used to generate the excerpt's content.
+     * When this changes, the excerpt will recompute whether the content
+     * is overflowing.
+     */
+    contentData: '<',
+    /** Whether or not truncation should be enabled */
+    enabled: '<?',
+    /**
+     * Specifies whether controls to expand and collapse
+     * the excerpt should be shown inside the <excerpt> component.
+     * If false, external controls can expand/collapse the excerpt by
+     * setting the 'collapse' property.
+     */
+    inlineControls: '<',
+    /** Sets whether or not the excerpt is collapsed. */
+    collapse: '=?',
+    /**
+     * Called when the collapsibility of the excerpt (that is, whether or
+     * not the content height exceeds the collapsed height), changes.
+     *
+     * Note: This function is *not* called from inside a digest cycle,
+     * the caller is responsible for triggering any necessary digests.
+     */
+    onCollapsibleChanged: '&?',
+    /** The height of this container in pixels when collapsed.
+     */
+    collapsedHeight: '<',
+    /**
+     * The number of pixels by which the height of the excerpt's content
+     * must extend beyond the collapsed height in order for truncation to
+     * be activated. This prevents the 'More' link from being shown to expand
+     * the excerpt if it has only been truncated by a very small amount, such
+     * that expanding the excerpt would reveal no extra lines of text.
+     */
+    overflowHysteresis: '<?',
+  },
+  transclude: true,
+  template: require('../templates/excerpt.html'),
+};
 
 module.exports = {
-  directive: excerpt,
+  component: component,
   Controller: ExcerptController,
 };
