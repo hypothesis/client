ea9069a7048c0f2c62790f0247ccfeb5305c46b7
Sean Hammond <git@snhmnd.fastmail.com>
2017-04-12 15:39:36 +0100

    Change serviceUrl to use the /api/links
    Change serviceUrl to return URLs by expanding URL templates received
    from h's /api/links resource, rather than using hardcoded URL templates
    as it previously did.
    
    The interface and contract that serviceUrl presents to its users remains
    the same - the fact it's now sending an API request to get the URL
    templates, and waiting for the response, is completely hidden from its
    users.
    
    This hiding is achieved by two tricks:
    
    1. Always returning "" for an expanded URL if the API response hasn't
       been received yet, rather than, for example, changing the interface
       to return Promises instead of strings (which would then require
       changes to every user of the interface, and possibly their users...)
    
    2. Updating annotationUI, the Redux state store, with the new URL
       templates when the API response is received. This causes any
       AngularJS components that might have used an (empty string) URL to be
       re-rendered, they will call sericeUrl URL again and this time get the
       actual URLs.
    
       This is completely transparent to the components - annotationUI
       causes AngularJS to call them again and this time they will get a
       different result from serviceUrl. The code of the components
       themselves doesn't need to change.
    
    Additionally, serviceUrl will never throw errors if it hasn't received
    the API response yet. For example there is no error if an unknown link
    name is requested, or if unused template params are given. It just
    always returns "". Once it _does_ have the API response then it will
    start throwing errors for bad requests, the same as it did previously.

--- src/sidebar/test/service-url-test.js
+++ src/sidebar/test/service-url-test.js
@@ -1,20 +1,157 @@
 'use strict';
 
-var serviceUrl = require('../service-url');
+var proxyquire = require('proxyquire');
 
-describe('serviceUrl', function () {
-  var service;
+/** Return a fake annotationUI object. */
+function fakeAnnotationUI() {
+  var links = null;
+  return {
+    updateLinks: function(newLinks) {
+      links = newLinks;
+    },
+    getState: function() {
+      return {links: links};
+    },
+  };
+}
 
-  beforeEach(function () {
-    service = serviceUrl({serviceUrl: 'https://test.hypothes.is/'});
+function createServiceUrl(linksPromise) {
+  var replaceURLParams = sinon.stub().returns(
+    {url: 'EXPANDED_URL', params: {}}
+  );
+
+  var serviceUrlFactory = proxyquire('../service-url', {
+    './util/url-util': { replaceURLParams: replaceURLParams },
   });
 
-  it('returns route URLs', function () {
-    assert.equal(service('help'), 'https://test.hypothes.is/docs/help');
+  var annotationUI = fakeAnnotationUI();
+
+  var store = {
+    links: sinon.stub().returns(linksPromise),
+  };
+
+  return {
+    annotationUI: annotationUI,
+    store: store,
+    serviceUrl: serviceUrlFactory(annotationUI, store),
+    replaceURLParams: replaceURLParams,
+  };
+}
+
+describe('links', function () {
+  context('before the API response has been received', function() {
+    var serviceUrl;
+    var store;
+
+    beforeEach(function() {
+      // Create a serviceUrl function with an unresolved Promise that will
+      // never be resolved - it never receives the links from store.links().
+      var parts = createServiceUrl(new Promise(function() {}));
+
+      serviceUrl = parts.serviceUrl;
+      store = parts.store;
+    });
+
+    it('sends one API request for the links at boot time', function() {
+      assert.calledOnce(store.links);
+      assert.isTrue(store.links.calledWithExactly());
+    });
+
+    it('returns an empty string for any link', function() {
+      assert.equal(serviceUrl('foo'), '');
+    });
+
+    it('returns an empty string even if link params are given', function() {
+      assert.equal(serviceUrl('foo', {bar: 'bar'}), '');
+    });
   });
 
-  it('expands route parameters', function () {
-    assert.equal(service('user', {user: 'jim'}),
-      'https://test.hypothes.is/u/jim');
+  context('if the API request fails', function() {
+    it('just keeps returning empty strings for URLs', function() {
+      var linksPromise = Promise.reject();
+
+      var serviceUrl = createServiceUrl(linksPromise).serviceUrl;
+
+      assert.equal(serviceUrl('second_link'), '');
+    });
+  });
+
+  context('after the API response has been received', function() {
+    var annotationUI;
+    var linksPromise;
+    var replaceURLParams;
+    var serviceUrl;
+
+    beforeEach(function() {
+      // The links Promise that store.links() will return.
+      linksPromise = Promise.resolve({
+        first_link: 'http://example.com/first_page/:foo',
+        second_link: 'http://example.com/second_page',
+      });
+
+      var parts = createServiceUrl(linksPromise);
+
+      annotationUI = parts.annotationUI;
+      serviceUrl = parts.serviceUrl;
+      replaceURLParams = parts.replaceURLParams;
+    });
+
+    it('updates annotationUI with the real links', function() {
+      return linksPromise.then(function(links) {
+        assert.deepEqual(annotationUI.getState(), {links: links});
+      });
+    });
+
+    it('calls replaceURLParams with the path and given params', function() {
+      return linksPromise.then(function() {
+        var params = {foo: 'bar'};
+
+        serviceUrl('first_link', params);
+
+        assert.calledOnce(replaceURLParams);
+        assert.deepEqual(
+          replaceURLParams.args[0],
+          ['http://example.com/first_page/:foo', params]);
+      });
+    });
+
+    it('passes an empty params object to replaceURLParams if no params are given', function() {
+      return linksPromise.then(function() {
+        serviceUrl('first_link');
+
+        assert.calledOnce(replaceURLParams);
+        assert.deepEqual(replaceURLParams.args[0][1], {});
+      });
+    });
+
+    it('returns the expanded URL from replaceURLParams', function() {
+      return linksPromise.then(function() {
+        var renderedUrl = serviceUrl('first_link');
+
+        assert.equal(renderedUrl, 'EXPANDED_URL');
+      });
+    });
+
+    it("throws an error if it doesn't have the requested link", function() {
+      return linksPromise.then(function() {
+        assert.throws(
+          function() { serviceUrl('madeUpLinkName'); },
+          Error, 'Unknown link madeUpLinkName');
+      });
+    });
+
+    it('throws an error if replaceURLParams returns unused params', function() {
+      var params = {'unused_param_1': 'foo', 'unused_param_2': 'bar'};
+      replaceURLParams.returns({
+        url: 'EXPANDED_URL',
+        params: params,
+      });
+
+      return linksPromise.then(function() {
+        assert.throws(
+          function() { serviceUrl('first_link', params); },
+          Error, 'Unknown link parameters: unused_param_1, unused_param_2');
+      });
+    });
   });
 });
