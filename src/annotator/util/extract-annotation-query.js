3162fcee4cdf720fd5845a78a4f6a6f4f9e4e4bd
Sean Hammond <git@snhmnd.fastmail.com>
2017-06-01 15:21:45 +0100

    Reduce the size of a `try` block
    This both clarifies the intent of the code (what exception, thrown by
    what, it's intending to catch) and reduces the chances of a bug where it
    actually catches a different exception thrown by something else that it
    didn't intend to catch.

--- src/annotator/util/extract-annotation-query.js
+++ src/annotator/util/extract-annotation-query.js
@@ -9,20 +9,20 @@
  */
 function extractAnnotationQuery(url) {
   var filter = {};
-  try {
-    // Annotation IDs are url-safe-base64 identifiers
-    // See https://tools.ietf.org/html/rfc4648#page-7
-    var annotFragmentMatch = url.match(/#annotations:([A-Za-z0-9_-]+)$/);
-    var queryFragmentMatch = url.match(/#annotations:(query|q):(.+)$/i);
-    if (queryFragmentMatch) {
+  // Annotation IDs are url-safe-base64 identifiers
+  // See https://tools.ietf.org/html/rfc4648#page-7
+  var annotFragmentMatch = url.match(/#annotations:([A-Za-z0-9_-]+)$/);
+  var queryFragmentMatch = url.match(/#annotations:(query|q):(.+)$/i);
+  if (queryFragmentMatch) {
+    try {
       filter.query = decodeURI(queryFragmentMatch[2]);
-    } else if (annotFragmentMatch) {
-      filter.annotations = annotFragmentMatch[1];
-    } else {
+    } catch (err) {
+      // URI Error should return the page unfiltered.
       filter = null;
     }
-  } catch (err) {
-    // URI Error should return the page unfiltered.
+  } else if (annotFragmentMatch) {
+    filter.annotations = annotFragmentMatch[1];
+  } else {
     filter = null;
   }
   return filter;
