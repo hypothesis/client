147810aad1964b7604914b76cbd1c5fd1e8ac4a7
Robert Knight <robertknight@gmail.com>
2017-06-08 11:41:35 +0100

    Port tag suggestion service to JS

---
+++ src/sidebar/tags.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * @typedef Tag
+ * @property {string} text - The label of the tag
+ * @property {number} count - The number of times this tag has been used.
+ * @property {number} updated - The timestamp when this tag was last used.
+ */
+
+/**
+ * Service for fetching tag suggestions and storing data to generate them.
+ *
+ * The `tags` service stores metadata about recently used tags to local storage
+ * and provides a `filter` method to fetch tags matching a query, ranked based
+ * on frequency of usage.
+ */
+// @ngInject
+function tags(localStorage) {
+  var TAGS_LIST_KEY = 'hypothesis.user.tags.list';
+  var TAGS_MAP_KEY = 'hypothesis.user.tags.map';
+
+  /**
+   * Return a list of tag suggestions matching `query`.
+   *
+   * @param {string} query
+   * @return {Tag[]} List of matching tags
+   */
+  function filter(query) {
+    var savedTags = localStorage.getObject(TAGS_LIST_KEY) || [];
+
+    return savedTags.filter((e) => {
+      return e.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+  }
+
+  /**
+   * Update the list of stored tag suggestions based on the tags that a user has
+   * entered for a given annotation.
+   *
+   * @param {Tag} tags - List of tags.
+   */
+  function store(tags) {
+    // Update the stored (tag, frequency) map.
+    var savedTags = localStorage.getObject(TAGS_MAP_KEY) || {};
+    tags.forEach((tag) => {
+      if (savedTags[tag.text]) {
+        savedTags[tag.text].count += 1;
+        savedTags[tag.text].updated = Date.now();
+      } else {
+        savedTags[tag.text] = {
+          text: tag.text,
+          count: 1,
+          updated: Date.now(),
+        };
+      }
+    });
+    localStorage.setObject(TAGS_MAP_KEY, savedTags);
+
+    // Sort tag suggestions by frequency.
+    var tagsList = Object.keys(savedTags).sort((t1, t2) => {
+      if (savedTags[t1].count !== savedTags[t2].count) {
+        return savedTags[t2].count - savedTags[t1].count;
+      }
+      return t1.localeCompare(t2);
+    });
+    localStorage.setObject(TAGS_LIST_KEY, tagsList);
+  }
+
+  return {
+    filter,
+    store,
+  };
+}
+
+module.exports = tags;
