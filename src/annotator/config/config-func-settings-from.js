287ea62b23dc3df0231da51aeae995b84599b464
Sean Hammond <git@snhmnd.fastmail.com>
2017-06-09 18:30:26 +0100

    Move configFuncSettingsFrom() into its own file
    Cut-paste the configFuncSettingsFrom() function and its unit tests out
    of settings.js and into its own config-func-settings-from.js file.
    
    This is because in a future refactoring index.js isn't going to call
    configFuncSettingsFrom() anymore, only settings.js will call it, which
    would make configFuncSettingsFrom() a private helper function of
    settings.js, but I don't want to lose configFuncSettingsFrom()'s unit
    tests so I'm moving it into its own file from where settings.js (in the
    future, index.js currently) can import and use it. This way
    configFuncSettingsFrom() remains part of the public API of its
    containing module and can have unit tests.

---
+++ src/annotator/config/config-func-settings-from.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Return an object containing config settings from window.hypothesisConfig().
+ *
+ * Return an object containing config settings returned by the
+ * window.hypothesisConfig() function provided by the host page:
+ *
+ *   {
+ *     fooSetting: 'fooValue',
+ *     barSetting: 'barValue',
+ *     ...
+ *   }
+ *
+ * If there's no window.hypothesisConfig() function then return {}.
+ *
+ * @param {Window} window_ - The window to search for a hypothesisConfig() function
+ * @return {Object} - Any config settings returned by hypothesisConfig()
+ *
+ */
+function configFuncSettingsFrom(window_) {
+  if (!window_.hasOwnProperty('hypothesisConfig')) {
+    return {};
+  }
+
+  if (typeof window_.hypothesisConfig !== 'function') {
+    var docs = 'https://h.readthedocs.io/projects/client/en/latest/publishers/config/#window.hypothesisConfig';
+    console.warn('hypothesisConfig must be a function, see: ' + docs);
+    return {};
+  }
+
+  return window_.hypothesisConfig();
+}
+
+module.exports = configFuncSettingsFrom;
---
+++ src/annotator/config/test/config-func-settings-from-test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var configFuncSettingsFrom = require('../config-func-settings-from');
+
+describe('annotator.config.configFuncSettingsFrom', function() {
+  var sandbox = sinon.sandbox.create();
+
+  afterEach('reset the sandbox', function() {
+    sandbox.restore();
+  });
+
+  context("when there's no window.hypothesisConfig() function", function() {
+    it('returns {}', function() {
+      var fakeWindow = {};
+
+      assert.deepEqual(configFuncSettingsFrom(fakeWindow), {});
+    });
+  });
+
+  context("when window.hypothesisConfig() isn't a function", function() {
+    beforeEach('stub console.warn()', function() {
+      sandbox.stub(console, 'warn');
+    });
+
+    function fakeWindow() {
+      return {hypothesisConfig: 42};
+    }
+
+    it('returns {}', function() {
+      assert.deepEqual(configFuncSettingsFrom(fakeWindow()), {});
+    });
+
+    it('logs a warning', function() {
+      configFuncSettingsFrom(fakeWindow());
+
+      assert.calledOnce(console.warn);
+      assert.isTrue(console.warn.firstCall.args[0].startsWith(
+        'hypothesisConfig must be a function'
+      ));
+    });
+  });
+
+  context('when window.hypothesisConfig() is a function', function() {
+    it('returns whatever window.hypothesisConfig() returns', function () {
+      // It just blindly returns whatever hypothesisConfig() returns
+      // (even if it's not an object).
+      var fakeWindow = { hypothesisConfig: sinon.stub().returns(42) };
+
+      assert.equal(configFuncSettingsFrom(fakeWindow), 42);
+    });
+  });
+});
