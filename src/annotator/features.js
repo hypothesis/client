6984f33290caaa94416a29b1ddaa95678a486c81
Sean Roberts <sean.roberts90@gmail.com>
2017-06-13 10:21:31 -0400

    Enable feature flagging in the annotation layer

---
+++ src/annotator/features.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const events = require('../shared/bridge-events');
+
+let _features = {};
+
+const _set = (features) => {
+  _features = features || {};
+};
+
+
+module.exports = {
+
+  init: function(crossframe) {
+    crossframe.on(events.FEATURE_FLAGS_UPDATED, _set);
+  },
+  
+  reset: function() {
+    _set({});
+  },
+
+  flagEnabled: function(flag) {
+    if (!(flag in _features)) {
+      console.warn('looked up unknown feature', flag);
+      return false;
+    }
+    return _features[flag];
+  },
+
+};
--- src/sidebar/features.js
+++ src/sidebar/features.js
@@ -11,8 +11,27 @@
  */
 'use strict';
 
+var events = require('./events');
+var bridgeEvents = require('../shared/bridge-events');
+
+
 // @ngInject
-function features($log, session) {
+function features($log, $rootScope, bridge, session) {
+
+  var _sendFeatureFlags = function(){
+    var userFeatures = session.state.features;
+    bridge.call(bridgeEvents.FEATURE_FLAGS_UPDATED, userFeatures || {});
+  };
+
+  // user changed is currently called when we initially load
+  // the sidebar and when the user actually logs out/in.
+  $rootScope.$on(events.USER_CHANGED, _sendFeatureFlags);
+
+  // send on frame connected as well because the user_changed event
+  // alone might run before the frames ever connected. This will
+  // provide us the follow up to make sure that the frames get the flags
+  $rootScope.$on(events.FRAME_CONNECTED, _sendFeatureFlags);
+
   /**
    * Returns true if the flag with the given name is enabled for the current
    * user.
