5e1c1910df6dff253cf24f084fa0ab2ed8661fc4
Robert Knight <robertknight@gmail.com>
2017-06-14 13:12:51 +0100

    Convert localStorage tests to JS

---
+++ src/sidebar/test/local-storage-test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var angular = require('angular');
+
+describe('sidebar.localStorage', () => {
+  var fakeWindow;
+
+  before(() =>
+    angular.module('h', [])
+      .service('localStorage', require('../local-storage'))
+  );
+
+  context('when browser localStorage is *not* accessible', () => {
+    var localStorage = null;
+    var key = null;
+
+    beforeEach(() => {
+      angular.mock.module('h', {
+        $window: {
+          localStorage: {},
+        },
+      });
+    });
+
+    beforeEach(angular.mock.inject((_localStorage_) => {
+      localStorage = _localStorage_;
+      key = 'test.memory.key';
+    }));
+
+    it('sets/gets Item', () => {
+      var value = 'What shall we do with a drunken sailor?';
+      localStorage.setItem(key, value);
+      var actual = localStorage.getItem(key);
+      assert.equal(value, actual);
+    });
+
+    it('removes item', () => {
+      localStorage.setItem(key, '');
+      localStorage.removeItem(key);
+      var result = localStorage.getItem(key);
+      assert.isNull(result);
+    });
+
+    it('sets/gets Object', () => {
+      var data = {'foo': 'bar'};
+      localStorage.setObject(key, data);
+      var stringified = localStorage.getItem(key);
+      assert.equal(stringified, JSON.stringify(data));
+
+      var actual = localStorage.getObject(key);
+      assert.deepEqual(actual, data);
+    });
+  });
+
+  context('when browser localStorage is accessible', () => {
+    var localStorage;
+
+    beforeEach(() => {
+      fakeWindow = {
+        localStorage: {
+          getItem: sinon.stub(),
+          setItem: sinon.stub(),
+          removeItem: sinon.stub(),
+        },
+      };
+
+      angular.mock.module('h', {
+        $window: fakeWindow,
+      });
+    });
+
+    beforeEach(() => {
+      angular.mock.inject(_localStorage_ => localStorage = _localStorage_);
+    });
+
+    it('uses window.localStorage functions to handle data', () => {
+      var key = 'test.storage.key';
+      var data = 'test data';
+
+      localStorage.setItem(key, data);
+      assert.calledWith(fakeWindow.localStorage.setItem, key, data);
+    });
+  });
+});
