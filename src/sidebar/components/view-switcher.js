72901812ee9c265b2043baa3a1833efc3f64dab9
Sean Hammond <git@snhmnd.fastmail.com>
2017-06-26 13:49:08 +0100

    Add new feature-flagged view-switcher
    If the 'view-switcher' feature flat is enabled then use the new view
    switcher component, otherwise uses the old selection tabs component.
    
    The view switcher component is just a placeholder for now.

---
+++ src/sidebar/components/view-switcher.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var uiConstants = require('../ui-constants');
+
+module.exports = {
+  controllerAs: 'vm',
+  //@ngInject
+  controller: function ($element, annotationUI, features) {
+    this.TAB_ANNOTATIONS = uiConstants.TAB_ANNOTATIONS;
+    this.TAB_NOTES = uiConstants.TAB_NOTES;
+    this.TAB_ORPHANS = uiConstants.TAB_ORPHANS;
+
+    this.selectTab = function (type) {
+      annotationUI.clearSelectedAnnotations();
+      annotationUI.selectTab(type);
+    };
+
+    this.orphansTabFlagEnabled = function () {
+      return features.flagEnabled('orphans_tab');
+    };
+
+    this.showAnnotationsUnavailableMessage = function () {
+      return this.selectedTab === this.TAB_ANNOTATIONS &&
+        this.totalAnnotations === 0 &&
+        !this.isWaitingToAnchorAnnotations;
+    };
+
+    this.showNotesUnavailableMessage = function () {
+      return this.selectedTab === this.TAB_NOTES &&
+        this.totalNotes === 0;
+    };
+  },
+  bindings: {
+    isLoading: '<',
+    isWaitingToAnchorAnnotations: '<',
+    selectedTab: '<',
+    totalAnnotations: '<',
+    totalNotes: '<',
+    totalOrphans: '<',
+  },
+  template: require('../templates/view-switcher.html'),
+};
