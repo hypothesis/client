b4f15a6fd0a11055a91e3d564e9433217ad01cdf
Sean Roberts <sean.roberts90@gmail.com>
2017-07-11 17:24:42 -0400

    Removing flags for multiple iframe support

--- src/annotator/main.js
+++ src/annotator/main.js
@@ -51,7 +51,7 @@ $.noConflict(true)(function() {
     delete config.constructor;
   }
 
-  if (config.enableMultiFrameSupport && config.subFrameIdentifier) {
+  if (config.subFrameIdentifier) {
     Klass = Guest;
 
     // Other modules use this to detect if this
