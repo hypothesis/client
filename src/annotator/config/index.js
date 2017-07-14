091a44775c3b3d3c8ebdca68b62d5a98e7e754e8
Sean Roberts <sean.roberts90@gmail.com>
2017-07-14 14:19:24 -0400

    Adding support for onLayoutChange notifier configuration

--- src/annotator/config/index.js
+++ src/annotator/config/index.js
@@ -10,29 +10,26 @@ var settingsFrom = require('./settings');
 function configFrom(window_) {
   var settings = settingsFrom(window_);
   return {
+    annotations: settings.annotations,
     // URL where client assets are served from. Used when injecting the client
     // into child iframes.
     assetRoot: settings.hostPageSetting('assetRoot', {allowInBrowserExt: true}),
+    branding: settings.hostPageSetting('branding'),
     // URL of the client's boot script. Used when injecting the client into
     // child iframes.
     clientUrl: settings.clientUrl,
-
-    annotations: settings.annotations,
-    branding: settings.hostPageSetting('branding'),
-    services: settings.hostPageSetting('services'),
-    showHighlights: settings.showHighlights,
-    sidebarAppUrl: settings.sidebarAppUrl,
-
-    // Subframe identifier given when a frame is being embedded into
-    // by a top level client
-    subFrameIdentifier: settings.hostPageSetting('subFrameIdentifier', {allowInBrowserExt: true}),
-
+    // Temporary feature flag override for 1st-party OAuth
+    oauthEnabled: settings.hostPageSetting('oauthEnabled'),
+    onLayoutChange: settings.hostPageSetting('onLayoutChange'),
     openLoginForm: settings.hostPageSetting('openLoginForm', {allowInBrowserExt: true}),
     openSidebar: settings.hostPageSetting('openSidebar', {allowInBrowserExt: true}),
     query: settings.query,
-
-    // Temporary feature flag override for 1st-party OAuth
-    oauthEnabled: settings.hostPageSetting('oauthEnabled'),
+    services: settings.hostPageSetting('services'),
+    showHighlights: settings.showHighlights,
+    sidebarAppUrl: settings.sidebarAppUrl,
+    // Subframe identifier given when a frame is being embedded into
+    // by a top level client
+    subFrameIdentifier: settings.hostPageSetting('subFrameIdentifier', {allowInBrowserExt: true}),
   };
 }
 
