296275eaf13d8340c2bc1e1679f9ebc5993a14d7
Sean Roberts <sean.roberts90@gmail.com>
2017-07-26 16:20:43 -0400

    Use rangeUtil#getTextBoundingBoxes for calculating overlay rects

--- src/annotator/highlighter/overlay-highlighter/overlay-highlight.js
+++ src/annotator/highlighter/overlay-highlighter/overlay-highlight.js
@@ -2,7 +2,7 @@
 
 const $ = require('jquery');
 const overlayUtils = require('./overlay-utils');
-
+const rangeUtils = require('../../../annotator/range-util');
 
 class OverlayHighlight {
 
@@ -18,8 +18,15 @@ class OverlayHighlight {
     this.container = highlightContainer;
     this.container.style.position = 'absolute';
 
-    // Disable pointer events
+    // Disable pointer events so that click events and such
+    // will target the content below this highlight overlay.
+    // Note: we use event delegation/inspection to apply
+    // events to the highlight elements themselves
     this.container.setAttribute('pointer-events', 'none');
+
+    // allow the highlight background to be amplified through this
+    // overlay background color
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode
     this.container.style.mixBlendMode = 'multiply';
 
     this.element = highlightEl;
@@ -38,29 +45,10 @@ class OverlayHighlight {
     while (this.element.firstChild) {
       this.element.removeChild(this.element.firstChild);
     }
-
-    const rects = this.range.getClientRects();
-
-    // The clientRects provided for a range can include large boundary boxes
-    // that are an artifact of a selection made over a large section of the page.
-    // These large boundaries are effectively a wrapper of the contained text selections.
-    // That is, if we displayed these rects it would include all of the text selections
-    // and a single large highlight that wraps the whole area. We are going to apply
-    // a simple heuristic that seems to work well at filtering out these boundary boxes.
-    // We will calculate the average height of the client rects and make sure that
-    // all displayed rects are less than a generous multiple of that.
-    // We can optimize this in the future as we learn more about these heuristics.
-    const clientRectsHeights = Array.from(rects).reduce((sum, clientRect)=>{
-      return sum + clientRect.height;
-    }, 0);
-
-    const avgHeight = clientRectsHeights / rects.length;
-
-    this.filteredRangeRects = Array.from(rects).filter((rect)=>{
-      return rect.height < (avgHeight * 3);
-    });
-
+    
     const offset = this.element.getBoundingClientRect();
+
+    this.filteredRangeRects = rangeUtils.getTextBoundingBoxes(this.range);
 
     this.highlightElementRefs = this.filteredRangeRects.map((rect) => {
       const el = overlayUtils.createSVGElement('rect');
