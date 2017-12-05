d11be3fb0ac168d80c65603bad4b735bd455200b
Sean Hammond <git@snhmnd.fastmail.com>
2017-12-05 18:32:56 +0000

    Don't show page share button on eLife pages
    Don't show the "Share this page" button in the top-right of the sidebar
    on pages, such as eLife's, that have both an embedded Hypothesis client
    and a services config including their own `authority` string.
    
    The "Share this page" button is a Via link, and Via doesn't work with
    pages using a third-party authority config.
    
    Fixes https://github.com/hypothesis/client/issues/615

---
+++ src/sidebar/util/is-third-party-service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const serviceConfig = require('../service-config');
+
+/**
+ * Return `true` if the first configured service is a "third-party" service.
+ *
+ * Return `true` if the first custom annotation service configured in the
+ * services array in the host page is a third-party service, `false` otherwise.
+ *
+ * If no custom annotation services are configured then return `false`.
+ *
+ * @param {Object} settings - the sidebar settings object
+ *
+ */
+function isThirdPartyService(settings) {
+  const service = serviceConfig(settings);
+
+  if (service === null) {
+    return false;
+  }
+
+  if (!service.hasOwnProperty('authority')) {
+    return false;
+  }
+
+  return (service.authority !== settings.authDomain);
+}
+
+module.exports = isThirdPartyService;
