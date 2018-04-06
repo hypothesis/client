2e4e294b7193e166ff0e07787034277b0afda1f1
Robert Knight <robertknight@gmail.com>
2018-04-06 07:46:01 +0100

    Convert `Adder` to an ES2015 class
    This commit and the next few refactor `Adder` to a "proper" class for
    easier readability.

--- src/annotator/adder.js
+++ src/annotator/adder.js
@@ -119,170 +119,176 @@ function createAdderDOM(container) {
 /**
  * Annotation 'adder' toolbar which appears next to the selection
  * and provides controls for the user to create new annotations.
- *
- * @param {Element} container - The DOM element into which the adder will be created
- * @param {Object} options - Options object specifying `onAnnotate` and `onHighlight`
- *        event handlers.
  */
-function Adder(container, options) {
-
-  var self = this;
-  self.element = createAdderDOM(container);
-
-  // Set initial style
-  Object.assign(container.style, {
-    display: 'block',
-
-    // take position out of layout flow initially
-    position: 'absolute',
-    top: 0,
-
-    // Assign a high Z-index so that the adder shows above any content on the
-    // page
-    zIndex: 999,
-  });
-
-  // The adder is hidden using the `visibility` property rather than `display`
-  // so that we can compute its size in order to position it before display.
-  self.element.style.visibility = 'hidden';
-
-  var view = self.element.ownerDocument.defaultView;
-  var enterTimeout;
-
-  self.element.querySelector(ANNOTATE_BTN_SELECTOR)
-    .addEventListener('click', handleCommand);
-  self.element.querySelector(HIGHLIGHT_BTN_SELECTOR)
-    .addEventListener('click', handleCommand);
-
-  function handleCommand(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    var isAnnotateCommand = this.classList.contains(ANNOTATE_BTN_CLASS);
-
-    if (isAnnotateCommand) {
-      options.onAnnotate();
-    } else {
-      options.onHighlight();
-    }
-
-    self.hide();
-  }
-
-  function width() {
-    return self.element.getBoundingClientRect().width;
-  }
-
-  function height() {
-    return self.element.getBoundingClientRect().height;
-  }
-
-  /** Hide the adder */
-  this.hide = function () {
-    clearTimeout(enterTimeout);
-    self.element.className = classnames({'annotator-adder': true});
-    self.element.style.visibility = 'hidden';
-  };
-
+class Adder {
   /**
-   * Return the best position to show the adder in order to target the
-   * selected text in `targetRect`.
+   * Construct the toolbar and populate the UI.
    *
-   * @param {Rect} targetRect - The rect of text to target, in viewport
-   *        coordinates.
-   * @param {boolean} isSelectionBackwards - True if the selection was made
-   *        backwards, such that the focus point is mosty likely at the top-left
-   *        edge of `targetRect`.
-   * @return {Target}
-   */
-  this.target = function (targetRect, isSelectionBackwards) {
-    // Set the initial arrow direction based on whether the selection was made
-    // forwards/upwards or downwards/backwards.
-    var arrowDirection;
-    if (isSelectionBackwards) {
-      arrowDirection = ARROW_POINTING_DOWN;
-    } else {
-      arrowDirection = ARROW_POINTING_UP;
-    }
-    var top;
-    var left;
-
-    // Position the adder such that the arrow it is above or below the selection
-    // and close to the end.
-    var hMargin = Math.min(ARROW_H_MARGIN, targetRect.width);
-    if (isSelectionBackwards) {
-      left = targetRect.left - width() / 2 + hMargin;
-    } else {
-      left = targetRect.left + targetRect.width - width() / 2 - hMargin;
-    }
-
-    // Flip arrow direction if adder would appear above the top or below the
-    // bottom of the viewport.
-    if (targetRect.top - height() < 0 &&
-        arrowDirection === ARROW_POINTING_DOWN) {
-      arrowDirection = ARROW_POINTING_UP;
-    } else if (targetRect.top + height() > view.innerHeight) {
-      arrowDirection = ARROW_POINTING_DOWN;
-    }
-
-    if (arrowDirection === ARROW_POINTING_UP) {
-      top = targetRect.top + targetRect.height + ARROW_HEIGHT;
-    } else {
-      top = targetRect.top - height() - ARROW_HEIGHT;
-    }
-
-    // Constrain the adder to the viewport.
-    left = Math.max(left, 0);
-    left = Math.min(left, view.innerWidth - width());
-
-    top = Math.max(top, 0);
-    top = Math.min(top, view.innerHeight - height());
-
-    return {top, left, arrowDirection};
-  };
-
-  /**
-   * Show the adder at the given position and with the arrow pointing in
-   * `arrowDirection`.
+   * The adder is initially hidden.
    *
-   * @param {number} left - Horizontal offset from left edge of viewport.
-   * @param {number} top - Vertical offset from top edge of viewport.
+   * @param {Element} container - The DOM element into which the adder will be created
+   * @param {Object} options - Options object specifying `onAnnotate` and `onHighlight`
+   *        event handlers.
    */
-  this.showAt = function (left, top, arrowDirection) {
-    self.element.className = classnames({
-      'annotator-adder': true,
-      'annotator-adder--arrow-down': arrowDirection === ARROW_POINTING_DOWN,
-      'annotator-adder--arrow-up': arrowDirection === ARROW_POINTING_UP,
-    });
+  constructor(container, options) {
+    var self = this;
+    self.element = createAdderDOM(container);
 
-    // Some sites make big assumptions about interactive
-    // elements on the page. Some want to hide interactive elements
-    // after use. So we need to make sure the button stays displayed
-    // the way it was originally displayed - without the inline styles
-    // See: https://github.com/hypothesis/client/issues/137
-    self.element.querySelector(ANNOTATE_BTN_SELECTOR).style.display = '';
-    self.element.querySelector(HIGHLIGHT_BTN_SELECTOR).style.display = '';
-
-    // Translate the (left, top) viewport coordinates into positions relative to
-    // the adder's nearest positioned ancestor (NPA).
-    //
-    // Typically the adder is a child of the `<body>` and the NPA is the root
-    // `<html>` element. However page styling may make the `<body>` positioned.
-    // See https://github.com/hypothesis/client/issues/487.
-    var positionedAncestor = nearestPositionedAncestor(container);
-    var parentRect = positionedAncestor.getBoundingClientRect();
-
+    // Set initial style
     Object.assign(container.style, {
-      top: toPx(top - parentRect.top),
-      left: toPx(left - parentRect.left),
-    });
-    self.element.style.visibility = 'visible';
+      display: 'block',
 
-    clearTimeout(enterTimeout);
-    enterTimeout = setTimeout(function () {
-      self.element.className += ' is-active';
-    }, 1);
-  };
+      // take position out of layout flow initially
+      position: 'absolute',
+      top: 0,
+
+      // Assign a high Z-index so that the adder shows above any content on the
+      // page
+      zIndex: 999,
+    });
+
+    // The adder is hidden using the `visibility` property rather than `display`
+    // so that we can compute its size in order to position it before display.
+    self.element.style.visibility = 'hidden';
+
+    var view = self.element.ownerDocument.defaultView;
+    var enterTimeout;
+
+    self.element.querySelector(ANNOTATE_BTN_SELECTOR)
+      .addEventListener('click', handleCommand);
+    self.element.querySelector(HIGHLIGHT_BTN_SELECTOR)
+      .addEventListener('click', handleCommand);
+
+    function handleCommand(event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      var isAnnotateCommand = this.classList.contains(ANNOTATE_BTN_CLASS);
+
+      if (isAnnotateCommand) {
+        options.onAnnotate();
+      } else {
+        options.onHighlight();
+      }
+
+      self.hide();
+    }
+
+    function width() {
+      return self.element.getBoundingClientRect().width;
+    }
+
+    function height() {
+      return self.element.getBoundingClientRect().height;
+    }
+
+    /** Hide the adder */
+    this.hide = function () {
+      clearTimeout(enterTimeout);
+      self.element.className = classnames({'annotator-adder': true});
+      self.element.style.visibility = 'hidden';
+    };
+
+    /**
+     * Return the best position to show the adder in order to target the
+     * selected text in `targetRect`.
+     *
+     * @param {Rect} targetRect - The rect of text to target, in viewport
+     *        coordinates.
+     * @param {boolean} isSelectionBackwards - True if the selection was made
+     *        backwards, such that the focus point is mosty likely at the top-left
+     *        edge of `targetRect`.
+     * @return {Target}
+     */
+    this.target = function (targetRect, isSelectionBackwards) {
+      // Set the initial arrow direction based on whether the selection was made
+      // forwards/upwards or downwards/backwards.
+      var arrowDirection;
+      if (isSelectionBackwards) {
+        arrowDirection = ARROW_POINTING_DOWN;
+      } else {
+        arrowDirection = ARROW_POINTING_UP;
+      }
+      var top;
+      var left;
+
+      // Position the adder such that the arrow it is above or below the selection
+      // and close to the end.
+      var hMargin = Math.min(ARROW_H_MARGIN, targetRect.width);
+      if (isSelectionBackwards) {
+        left = targetRect.left - width() / 2 + hMargin;
+      } else {
+        left = targetRect.left + targetRect.width - width() / 2 - hMargin;
+      }
+
+      // Flip arrow direction if adder would appear above the top or below the
+      // bottom of the viewport.
+      if (targetRect.top - height() < 0 &&
+          arrowDirection === ARROW_POINTING_DOWN) {
+        arrowDirection = ARROW_POINTING_UP;
+      } else if (targetRect.top + height() > view.innerHeight) {
+        arrowDirection = ARROW_POINTING_DOWN;
+      }
+
+      if (arrowDirection === ARROW_POINTING_UP) {
+        top = targetRect.top + targetRect.height + ARROW_HEIGHT;
+      } else {
+        top = targetRect.top - height() - ARROW_HEIGHT;
+      }
+
+      // Constrain the adder to the viewport.
+      left = Math.max(left, 0);
+      left = Math.min(left, view.innerWidth - width());
+
+      top = Math.max(top, 0);
+      top = Math.min(top, view.innerHeight - height());
+
+      return {top, left, arrowDirection};
+    };
+
+    /**
+     * Show the adder at the given position and with the arrow pointing in
+     * `arrowDirection`.
+     *
+     * @param {number} left - Horizontal offset from left edge of viewport.
+     * @param {number} top - Vertical offset from top edge of viewport.
+     */
+    this.showAt = function (left, top, arrowDirection) {
+      self.element.className = classnames({
+        'annotator-adder': true,
+        'annotator-adder--arrow-down': arrowDirection === ARROW_POINTING_DOWN,
+        'annotator-adder--arrow-up': arrowDirection === ARROW_POINTING_UP,
+      });
+
+      // Some sites make big assumptions about interactive
+      // elements on the page. Some want to hide interactive elements
+      // after use. So we need to make sure the button stays displayed
+      // the way it was originally displayed - without the inline styles
+      // See: https://github.com/hypothesis/client/issues/137
+      self.element.querySelector(ANNOTATE_BTN_SELECTOR).style.display = '';
+      self.element.querySelector(HIGHLIGHT_BTN_SELECTOR).style.display = '';
+
+      // Translate the (left, top) viewport coordinates into positions relative to
+      // the adder's nearest positioned ancestor (NPA).
+      //
+      // Typically the adder is a child of the `<body>` and the NPA is the root
+      // `<html>` element. However page styling may make the `<body>` positioned.
+      // See https://github.com/hypothesis/client/issues/487.
+      var positionedAncestor = nearestPositionedAncestor(container);
+      var parentRect = positionedAncestor.getBoundingClientRect();
+
+      Object.assign(container.style, {
+        top: toPx(top - parentRect.top),
+        left: toPx(left - parentRect.left),
+      });
+      self.element.style.visibility = 'visible';
+
+      clearTimeout(enterTimeout);
+      enterTimeout = setTimeout(function () {
+        self.element.className += ' is-active';
+      }, 1);
+    };
+  }
 }
 
 module.exports = {
