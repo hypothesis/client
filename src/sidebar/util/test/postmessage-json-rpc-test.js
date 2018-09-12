b6f3ba5ba733de1e98d99c702ff36ab6185297d1
Robert Knight <robertknight@gmail.com>
2018-09-12 18:03:00 +0100

    Add client for making JSON-RPC calls over postMessage
    We need a way for the client to request configuration from an embedding
    frame over `postMessage`.
    
    JSON-RPC is a well-known, simple and convenient format for RPC messages.
    
    There are several npm packages which claim to offer this functionality
    but at the time of writing, none appeared to be sufficiently well
    tested/supported/documented.
    
    There is an existing partial postMessage JSON-RPC server implementation in
    src/sidebar/cross-origin-rpc.js. The non-app specific parts of that will
    be moved into this module in future.

---
+++ src/sidebar/util/test/postmessage-json-rpc-test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const EventEmitter = require('tiny-emitter');
+
+const { call } = require('../postmessage-json-rpc');
+
+class FakeWindow {
+  constructor() {
+    this.emitter = new EventEmitter;
+    this.addEventListener = this.emitter.on.bind(this.emitter);
+    this.removeEventListener = this.emitter.off.bind(this.emitter);
+  }
+}
+
+function assertPromiseIsRejected(promise, expectedErr) {
+  const rejectFlag = {};
+  return promise.catch(err => {
+    assert.equal(err.message, expectedErr);
+    return rejectFlag;
+  }).then(result => {
+    assert.equal(result, rejectFlag, 'expected promise to be rejected but it was fulfilled');
+  });
+}
+
+describe('sidebar.util.postmessage-json-rpc', () => {
+  const origin = 'https://embedder.com';
+  const messageId = 42;
+
+  describe('call', () => {
+    let frame;
+    let fakeWindow;
+
+    function doCall() {
+      const timeout = 1;
+      return call(
+        frame, origin, 'testMethod', [1, 2, 3], timeout, fakeWindow, messageId
+      );
+    }
+
+    beforeEach(() => {
+      frame = { postMessage: sinon.stub() };
+      fakeWindow = new FakeWindow;
+    });
+
+    it('sends a message to the target frame', () => {
+      doCall().catch(() => {} /* Ignore timeout. */);
+
+      assert.calledWith(frame.postMessage, {
+        jsonrpc: '2.0',
+        id: messageId,
+        method: 'testMethod',
+        params: [1, 2, 3],
+      });
+    });
+
+    [{
+      // Wrong origin.
+      origin: 'https://not-the-embedder.com',
+      data: {
+        jsonrpc: '2.0',
+        id: messageId,
+      },
+    },{
+      // Non-object `data` field.
+      data: null,
+    },{
+      // No jsonrpc header
+      data: {},
+    },{
+      // No ID
+      data: {
+        jsonrpc: '2.0',
+      },
+    },{
+      // ID mismatch
+      data: {
+        jsonrpc: '2.0',
+        id: 'wrong-id',
+      },
+    }].forEach(reply => {
+      it('ignores messages that do not have required reply fields', () => {
+        const result = doCall();
+
+        fakeWindow.emitter.emit('message', reply);
+
+        const notCalled = Promise.resolve('notcalled');
+        return Promise.race([result, notCalled]).then(result => {
+          assert.equal(result, 'notcalled');
+        });
+      });
+    });
+
+    it('rejects with an error if the `error` field is set in the response', () => {
+      const result = doCall();
+      fakeWindow.emitter.emit('message', {
+        origin,
+        data: {
+          jsonrpc: '2.0',
+          id: messageId,
+          error: {
+            message: 'Something went wrong',
+          },
+        },
+      });
+
+      return assertPromiseIsRejected(result, 'Something went wrong');
+    });
+
+    it('rejects if no `error` or `result` field is set in the response', () => {
+      const result = doCall();
+      fakeWindow.emitter.emit('message', {
+        origin,
+        data: { jsonrpc: '2.0', id: messageId },
+      });
+
+      return assertPromiseIsRejected(result, 'RPC reply had no result or error');
+    });
+
+    it('resolves with the result if the `result` field is set in the response', () => {
+      const result = doCall();
+      const expectedResult = { foo: 'bar' };
+      fakeWindow.emitter.emit('message', {
+        origin,
+        data: {
+          jsonrpc: '2.0',
+          id: messageId,
+          result: expectedResult,
+        },
+      });
+
+      return result.then(result => {
+        assert.deepEqual(result, expectedResult);
+      });
+    });
+
+    it('rejects with an error if the timeout is exceeded', () => {
+      const result = doCall();
+      return assertPromiseIsRejected(result, 'Request to https://embedder.com timed out');
+    });
+  });
+});
