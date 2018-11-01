6ae6d731ed14c2c6cf62fbb501ce6b30c19f452e
Robert Knight <robertknight@gmail.com>
2018-11-01 14:21:52 +0000

    Create qa deployment of the client
    Add a new build stage to Jenkins CI builds of the master branch which
    deploys a "QA" build to S3. This QA build:
    
     - Has a pre-release version number of the form `X.Y.Z-{commit hash}`
     - Has a stable URL at https://cdn.hypothes.is/hypothesis@qa
       (as opposed to https://cdn.hypothes.is/hypothesis for the prod build)
     - Loads the sidebar app from qa-h (https://qa.hypothes.is/app.html)
     - Does not update the changelog or create a Git tag or GitHub release
    
    This build is currently not deployed to npm at all, but we might need to
    do that in future if we were to create a QA deployment of the browser
    extension with it.
    
    ---
    
     - Add a QA deploy step to the Jenkins build.
     - Skip GitHub release creation and changelog updates for pre-release
       versions used for QA deployments.
     - Remove the preversion.sh script, which is no longer needed since the
       builds always run in Jenkins and not on a developer's system.
     - Support specifying the npm tag to check in `wait-for-npm-release.sh`,
       though this is not used in the final version of this commit.

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -4,6 +4,13 @@ set -eu
 
 cd "$(dirname "$0")"
 
+# Skip GitHub release creation for QA releases.
+is_prerelease=$(node -p "require('./package.json').version.includes('-')")
+if [ $is_prerelease = "true" ]; then
+  echo "Skipping GitHub release creation for pre-release"
+  exit 0
+fi
+
 # nb. The remote refname is fully qualified because this script is run in a CI
 # environment where not all heads may have been fetched.
 git push https://github.com/hypothesis/client.git HEAD:refs/heads/$BRANCH_NAME --follow-tags
