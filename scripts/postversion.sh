cc80ffffff1dc5a8154378a9d09dff08d82058aa
Robert Knight <robertknight@gmail.com>
2018-11-02 13:08:54 +0000

    Fix a typo.
    This went unnoticed because it turns out that `yarn version` does not
    actually run the postversion hook if `--no-git-tag-version` is
    specified. See https://github.com/yarnpkg/yarn/issues/5358

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -5,7 +5,7 @@ set -eu
 cd "$(dirname "$0")"
 
 # Skip GitHub release creation for QA releases.
-is_prerelease=$(node -p "require('./package.json').version.includes('-')")
+is_prerelease=$(node -p "require('../package.json').version.includes('-')")
 if [ $is_prerelease = "true" ]; then
   echo "Skipping GitHub release creation for pre-release"
   exit 0
