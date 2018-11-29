b30373735fe3b894890bb62e3b96665d89e00d0c
Robert Knight <robertknight@gmail.com>
2018-11-29 20:36:45 +0000

    Hide non-applicable tutorial steps for third party users
    Hide steps in the sidebar tutorial which are not available to
    third-party users. It is expected that the conditions under which these
    actions are available may change in future, but they will still be
    unavailable in at least some contexts.
    
    Fixes https://github.com/hypothesis/product-backlog/issues/842

--- src/sidebar/components/sidebar-tutorial.js
+++ src/sidebar/components/sidebar-tutorial.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const sessionUtil = require('../util/session-util');
+const isThirdPartyService = require('../util/is-third-party-service');
 
 // @ngInject
 function SidebarTutorialController(session, settings) {
+  // Compute once since this doesn't change after the app starts.
+  const isThirdPartyService_ = isThirdPartyService(settings);
+
   this.isThemeClean = settings.theme === 'clean';
 
   this.showSidebarTutorial = function () {
@@ -12,6 +16,20 @@ function SidebarTutorialController(session, settings) {
 
   this.dismiss = function () {
     session.dismissSidebarTutorial();
+  };
+
+  this.canCreatePrivateGroup = () => {
+    // Private group creation in the client is limited to first party users.
+    // In future we may extend this to third party users, but still disable
+    // private group creation in certain contexts (eg. the LMS app).
+    return !isThirdPartyService_;
+  };
+
+  this.canSharePage = () => {
+    // The "Share document" icon in the toolbar is limited to first party users.
+    // In future we may extend this to third party users, but still disable it
+    // in certain contexts (eg. the LMS app).
+    return !isThirdPartyService_;
   };
 }
 
