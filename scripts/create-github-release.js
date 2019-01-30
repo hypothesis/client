f2d7b5704026d9a73bfab31fa3f1cc42c327db93
Robert Knight <robertknight@gmail.com>
2019-01-30 11:49:17 +0000

    Remove CHANGELOG from source tree
    The client deployment process currently involves a commit to master to
    update the CHANGELOG and version in package.json. This creates a problem
    when trying to deploy a build that is not the latest release of master.
    It also creates other failure modes that are not really necessary.
    
    As a first step to resolving this problem, remove the changelog in the source
    tree. The notes attached to the GitHub release will suffice, together
    with any handwritten updates that developers post in #changelog in
    Slack.
    
     - Remove CHANGELOG.md and scripts to generate it from source tree
     - Remove changelog references from build process
     - Change GitHub release script to generate the change list directly
       instead of reading it from CHANGELOG.md
     - Change the client's dev server to use the Code of Conduct instead of
       the changelog as a text source for the test page

--- scripts/create-github-release.js
+++ scripts/create-github-release.js
@@ -9,59 +9,57 @@
  * `v<VERSION>` where <VERSION> is the `version` field in package.json.
  */
 
-const fs = require('fs');
 const request = require('request');
+const octokit = require('@octokit/rest')();
 
 const pkg = require('../package.json');
+const { changelistSinceTag } = require('./generate-change-list');
 
-/**
- * Extract the release notes for a given version from a markdown changelog in
- * the format recommended by http://keepachangelog.com
- */
-function extractReleaseNotes(changelog, version) {
-  const notes = changelog
-    .split(/(\n|^)## /)
-    .find(section => section.indexOf(`[${version}]`) === 0);
+async function createGitHubRelease() {
+  // See https://github.com/docker/docker/issues/679
+  const GITHUB_ORG_REPO_PAT = /^[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/;
 
-  if (!notes) {
-    throw new Error(`Failed to find release notes for v${pkg.version}`);
+  if (!pkg.repository || !pkg.repository.match(GITHUB_ORG_REPO_PAT)) {
+    throw new Error('package.json is missing a "repository" field of the form :owner/:repo');
   }
 
-  return notes.split('\n').slice(1).join('\n');
-}
-
-// See https://github.com/docker/docker/issues/679
-const GITHUB_ORG_REPO_PAT = /^[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/;
-
-if (!pkg.repository || !pkg.repository.match(GITHUB_ORG_REPO_PAT)) {
-  throw new Error('package.json is missing a "repository" field of the form :owner/:repo');
-}
-
-if (!process.env.GITHUB_TOKEN) {
-  throw new Error('GITHUB_TOKEN env var is not set');
-}
-
-const changelog = fs.readFileSync(require.resolve('../CHANGELOG.md')).toString();
-const release = {
-  tag_name: `v${pkg.version}`,
-  name: `v${pkg.version}`,
-  body: extractReleaseNotes(changelog, pkg.version),
-  draft: false,
-  prerelease: true,
-};
-
-request.post({
-  uri: `https://api.github.com/repos/${pkg.repository}/releases`,
-  body: release,
-  json: true,
-  headers: {
-    Authorization: `token ${process.env.GITHUB_TOKEN}`,
-    'User-Agent': `${pkg.repository} Release Script`,
-  },
-}, (err, rsp, body) => {
-  if (err || rsp.statusCode !== 201) {
-    const msg = err ? err.message : `${rsp.statusCode}: ${JSON.stringify(body)}`;
-    throw new Error(`Creating GitHub release failed: ${msg}`);
+  if (!process.env.GITHUB_TOKEN) {
+    throw new Error('GITHUB_TOKEN env var is not set');
   }
-  console.info(`Created GitHub release for v${pkg.version}`);
+
+  octokit.authenticate({
+    type: 'oauth',
+    token: process.env.GITHUB_TOKEN,
+  });
+
+  const changes = await changelistSinceTag(octokit);
+
+  const release = {
+    tag_name: `v${pkg.version}`,
+    name: `v${pkg.version}`,
+    body: changes,
+    draft: false,
+    prerelease: true,
+  };
+
+  request.post({
+    uri: `https://api.github.com/repos/${pkg.repository}/releases`,
+    body: release,
+    json: true,
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      'User-Agent': `${pkg.repository} Release Script`,
+    },
+  }, (err, rsp, body) => {
+    if (err || rsp.statusCode !== 201) {
+      const msg = err ? err.message : `${rsp.statusCode}: ${JSON.stringify(body)}`;
+      throw new Error(`Creating GitHub release failed: ${msg}`);
+    }
+    console.info(`Created GitHub release for v${pkg.version}`);
+  });
+}
+
+createGitHubRelease().catch(err => {
+  console.error('Failed to create release.', err);
+  process.exit(1);
 });
