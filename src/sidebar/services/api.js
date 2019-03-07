df2bd12a45db17dedeec29f9949563530a6ef621
Robert Knight <robertknight@gmail.com>
2019-03-07 15:53:13 +0000

    Convert api service to use fetch instead of $http
    Modify the Hypothesis API client to use `fetch` instead of $http.
    
    There is one change to the interface of the "api" service which is that
    failures no longer set a `response` property on the `Error` instance.
    This functionality was not being used elsewhere in the client.

--- src/sidebar/services/api.js
+++ src/sidebar/services/api.js
@@ -1,28 +1,24 @@
 'use strict';
 
 const get = require('lodash.get');
+const queryString = require('query-string');
 
-const urlUtil = require('../util/url-util');
+const { replaceURLParams } = require('../util/url-util');
 
 /**
  * Translate the response from a failed API call into an Error-like object.
- *
- * The details of the response are available on the `response` property of the
- * error.
  */
-function translateResponseToError(response) {
+function translateResponseToError(response, data) {
   let message;
   if (response.status <= 0) {
     message = 'Service unreachable.';
   } else {
     message = response.status + ' ' + response.statusText;
-    if (response.data && response.data.reason) {
-      message = message + ': ' + response.data.reason;
+    if (data && data.reason) {
+      message = message + ': ' + data.reason;
     }
   }
-  const err = new Error(message);
-  err.response = response;
-  return err;
+  return new Error(message);
 }
 
 /**
@@ -41,62 +37,10 @@ function stripInternalProperties(obj) {
   return result;
 }
 
-function forEachSorted(obj, iterator, context) {
-  const keys = Object.keys(obj).sort();
-  for (let i = 0; i < keys.length; i++) {
-    iterator.call(context, obj[keys[i]], keys[i]);
-  }
-  return keys;
-}
-
-function serializeValue(v) {
-  if (typeof v === 'object') {
-    return v instanceof Date ? v.toISOString() : JSON.stringify(v);
-  }
-  return v;
-}
-
-function encodeUriQuery(val) {
-  return encodeURIComponent(val).replace(/%20/g, '+');
-}
-
-// Serialize an object containing parameters into a form suitable for a query
-// string.
-//
-// This is an almost identical copy of the default Angular parameter serializer
-// ($httpParamSerializer), with one important change. In Angular 1.4.x
-// semicolons are not encoded in query parameter values. This is a problem for
-// us as URIs around the web may well contain semicolons, which our backend will
-// then proceed to parse as a delimiter in the query string. To avoid this
-// problem we use a very conservative encoder, found above.
-function serializeParams(params) {
-  if (!params) {
-    return '';
-  }
-  const parts = [];
-  forEachSorted(params, function(value, key) {
-    if (value === null || typeof value === 'undefined') {
-      return;
-    }
-    if (Array.isArray(value)) {
-      value.forEach(function(v) {
-        parts.push(
-          encodeUriQuery(key) + '=' + encodeUriQuery(serializeValue(v))
-        );
-      });
-    } else {
-      parts.push(
-        encodeUriQuery(key) + '=' + encodeUriQuery(serializeValue(value))
-      );
-    }
-  });
-
-  return parts.join('&');
-}
-
 /**
  * @typedef APIResponse
- * @prop {any} data - The JSON response from the API call.
+ * @prop {any} data -
+ *  The JSON response from the API call, unless this call return 204 No Content.
  * @prop {string|null} token - The access token that was used to make the call
  *   or `null` if unauthenticated.
  */
@@ -135,8 +79,6 @@ const noop = () => {};
 /**
  * Creates a function that will make an API call to a named route.
  *
- * @param $http - The Angular HTTP service
- * @param $q - The Angular Promises ($q) service.
  * @param links - Object or promise for an object mapping named API routes to
  *                URL templates and methods
  * @param route - The dotted path of the named API route (eg. `annotation.create`)
@@ -144,8 +86,6 @@ const noop = () => {};
  * @return {APICallFunction}
  */
 function createAPICall(
-  $http,
-  $q,
   links,
   route,
   {
@@ -161,12 +101,11 @@ function createAPICall(
     // mixes native Promises with the `$q` promises returned by `$http`
     // functions gets awkward in tests.
     let accessToken;
-    return $q
-      .all([links, getAccessToken()])
+    return Promise.all([links, getAccessToken()])
       .then(([links, token]) => {
         const descriptor = get(links, route);
-        const url = urlUtil.replaceURLParams(descriptor.url, params);
         const headers = {
+          'Content-Type': 'application/json',
           'Hypothesis-Client-Version': '__VERSION__', // replaced by versionify
         };
 
@@ -175,34 +114,44 @@ function createAPICall(
           headers.Authorization = 'Bearer ' + token;
         }
 
-        const req = {
-          data: data ? stripInternalProperties(data) : null,
-          headers: headers,
+        const { url, params: queryParams } = replaceURLParams(
+          descriptor.url,
+          params
+        );
+        const apiUrl = new URL(url);
+        apiUrl.search = queryString.stringify(queryParams);
+
+        return fetch(apiUrl.toString(), {
+          body: data ? JSON.stringify(stripInternalProperties(data)) : null,
+          headers,
           method: descriptor.method,
-          params: url.params,
-          paramSerializer: serializeParams,
-          url: url.url,
-        };
-        return $http(req);
+        });
       })
-      .then(function(response) {
+      .then(response => {
+        let data;
+
+        const status = response.status;
+        if (status >= 200 && status !== 204 && status < 500) {
+          data = response.json();
+        } else {
+          data = response.text();
+        }
+        return Promise.all([response, data]);
+      })
+      .then(([response, data]) => {
         onRequestFinished();
+
+        if (response.status >= 400) {
+          // Translate the API result into an `Error` to follow the convention that
+          // Promises should be rejected with an Error or Error-like object.
+          throw translateResponseToError(response, data);
+        }
 
         if (options.includeMetadata) {
-          return { data: response.data, token: accessToken };
+          return { data, token: accessToken };
         } else {
-          return response.data;
+          return data;
         }
-      })
-      .catch(function(response) {
-        onRequestFinished();
-
-        // Translate the API result into an `Error` to follow the convention that
-        // Promises should be rejected with an Error or Error-like object.
-        //
-        // Use `$q.reject` rather than just rethrowing the Error here due to
-        // mishandling of errors thrown inside `catch` handlers in Angular < 1.6
-        return $q.reject(translateResponseToError(response));
       });
   };
 }
@@ -228,10 +177,11 @@ function createAPICall(
  * not use authentication.
  */
 // @ngInject
-function api($http, $q, apiRoutes, auth, store) {
+function api(apiRoutes, auth, store) {
   const links = apiRoutes.routes();
+
   function apiCall(route) {
-    return createAPICall($http, $q, links, route, {
+    return createAPICall(links, route, {
       getAccessToken: auth.tokenGetter,
       onRequestStarted: store.apiRequestStarted,
       onRequestFinished: store.apiRequestFinished,
