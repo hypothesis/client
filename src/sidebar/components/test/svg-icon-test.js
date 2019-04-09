e5728d69968b50164dcb8bb4faaa429fccb69104
Robert Knight <robertknight@gmail.com>
2019-04-09 14:43:31 +0100

    Add a note about why the SvgIcon tests do not use Enzyme

--- src/sidebar/components/test/svg-icon-test.js
+++ src/sidebar/components/test/svg-icon-test.js
@@ -5,6 +5,10 @@ const { createElement, render } = require('preact');
 const SvgIcon = require('../svg-icon');
 
 describe('SvgIcon', () => {
+  // Tests here use DOM APIs rather than Enzyme because SvgIcon uses
+  // `dangerouslySetInnerHTML` for its content, and that is not visible in the
+  // Enzyme tree.
+
   it("sets the element's content to the content of the SVG", () => {
     const container = document.createElement('div');
     render(<SvgIcon name="refresh" />, container);
