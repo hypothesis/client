62b99843ed36c3ff023fe8aca970947f1282ccef
Robert Knight <robertknight@gmail.com>
2019-05-29 18:29:30 +0100

    Add tests for `warnOnce` utility

--- src/sidebar/util/warn-once.js
+++ src/sidebar/util/warn-once.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const shownWarnings = {};
+let shownWarnings = {};
 
 /**
  * Log a warning if it has not already been reported.
+ *
+ * This is useful to avoid spamming the console if a warning is emitted in a
+ * context that may be called frequently.
  *
  * @param {string} warning
  */
@@ -14,5 +17,9 @@ function warnOnce(warning) {
   console.warn(warning);
   shownWarnings[warning] = true;
 }
+
+warnOnce.reset = () => {
+  shownWarnings = {};
+};
 
 module.exports = warnOnce;
