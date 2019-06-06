b62662ecdfa530f5311b0e4e33ddd5d74332b490
Lyza Danger Gardner <lyza@lyza.com>
2019-06-06 12:38:48 -0400

    Remove old-style; add help icon to non-sidebar top bar

--- src/sidebar/components/login-control.js
+++ src/sidebar/components/login-control.js
@@ -14,10 +14,6 @@ module.exports = {
     /**
      * Called when the user clicks on the "About this version" text.
      */
-    onShowHelpPanel: '&',
-    /**
-     * Called when the user clicks on the "Log in" text.
-     */
     onLogin: '&',
     /**
      * Called when the user clicks on the "Sign Up" text.
@@ -27,12 +23,6 @@ module.exports = {
      * Called when the user clicks on the "Log out" text.
      */
     onLogout: '&',
-    /**
-     * Whether or not to use the new design for the control.
-     *
-     * FIXME: should be removed when the old design is deprecated.
-     */
-    newStyle: '<',
   },
   template: require('../templates/login-control.html'),
 };
