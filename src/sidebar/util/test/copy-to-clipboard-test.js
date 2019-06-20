f4d00dfc2d6503d21431722b58d1747efbbc4302
Robert Knight <robertknight@gmail.com>
2019-06-20 18:51:21 +0100

    Remove unnecessary randomness from the test

--- src/sidebar/util/test/copy-to-clipboard-test.js
+++ src/sidebar/util/test/copy-to-clipboard-test.js
@@ -19,15 +19,23 @@ describe('copy-to-clipboard', () => {
       return document.querySelector('.copy-text');
     }
 
+    beforeEach(() => {
+      // Make no hidden element created for copying text has been left over
+      // from a previous test.
+      assert.isNull(tempSpan());
+
+      // Make sure there is nothing already selected to copy.
+      window.getSelection().removeAllRanges();
+    });
+
     it('copies the passed text to the clipboard', () => {
       // We can't actually copy to the clipboard due to security restrictions,
       // but we can verify that `execCommand("copy")` was called and that the
       // passed text was selected at the time.
-      const testStr = `test string ${Math.random()}`;
       document.execCommand.callsFake(() => {
-        assert.equal(document.getSelection().toString(), testStr);
+        assert.equal(document.getSelection().toString(), 'test string');
       });
-      copyText(testStr);
+      copyText('test string');
       assert.calledWith(document.execCommand, 'copy');
       assert.isNull(tempSpan());
     });
