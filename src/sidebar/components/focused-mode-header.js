3e0bbe146d977200142cee668a39eacf86bb5fb7
Lyza Danger Gardner <lyza@lyza.com>
2019-08-08 15:00:28 -0400

    Add UI to Focused User Mode

--- src/sidebar/components/focused-mode-header.js
+++ src/sidebar/components/focused-mode-header.js
@@ -4,42 +4,59 @@ const { createElement } = require('preact');
 
 const useStore = require('../store/use-store');
 
+const SvgIcon = require('./svg-icon');
+
 /**
  * Render a control to interact with any focused "mode" in the sidebar.
  * Currently only a user-focus mode is supported but this could be broadened
  * and abstracted if needed. Allow user to toggle in and out of the focus "mode."
  */
 function FocusedModeHeader() {
-  const store = useStore(store => ({
-    actions: {
-      setFocusModeFocused: store.setFocusModeFocused,
-    },
-    selectors: {
-      focusModeFocused: store.focusModeFocused,
-      focusModeUserPrettyName: store.focusModeUserPrettyName,
-    },
+  const actions = useStore(store => ({
+    setFocusModeFocused: store.setFocusModeFocused,
+  }));
+  const selectors = useStore(store => ({
+    focusModeFocused: store.focusModeFocused(),
+    focusModeHasUser: store.focusModeHasUser(),
+    focusModeUserPrettyName: store.focusModeUserPrettyName(),
   }));
 
+  // Nothing to do here for now if we're not focused on a user
+  if (!selectors.focusModeHasUser) {
+    return null;
+  }
+
   const toggleFocusedMode = () => {
-    store.actions.setFocusModeFocused(!store.selectors.focusModeFocused());
+    actions.setFocusModeFocused(!selectors.focusModeFocused);
   };
 
-  const buttonText = () => {
-    if (store.selectors.focusModeFocused()) {
-      return `Annotations by ${store.selectors.focusModeUserPrettyName()}`;
+  const filterStatus = (
+    <div className="focused-mode-header__filter-status">
+      {selectors.focusModeFocused ? (
+        <span>
+          Annotations by <strong>{selectors.focusModeUserPrettyName}</strong>{' '}
+          only
+        </span>
+      ) : (
+        <span>Everybody&rsquo;s annotations</span>
+      )}
+    </div>
+  );
+
+  const buttonText = (() => {
+    if (selectors.focusModeFocused) {
+      return 'Show all';
     } else {
-      return 'All annotations';
+      return `Only ${selectors.focusModeUserPrettyName}`;
     }
-  };
+  })();
 
   return (
-    <div className="focused-mode-header">
-      <button
-        onClick={toggleFocusedMode}
-        className="primary-action-btn primary-action-btn--short"
-        title={`Toggle to show annotations only by ${store.selectors.focusModeUserPrettyName()}`}
-      >
-        {buttonText()}
+    <div className="focused-mode-header sheet">
+      {filterStatus}
+      <button onClick={toggleFocusedMode} className="focused-mode-header__btn">
+        <SvgIcon name="cancel" className="focused-mode-header__btn-icon" />
+        {buttonText}
       </button>
     </div>
   );
