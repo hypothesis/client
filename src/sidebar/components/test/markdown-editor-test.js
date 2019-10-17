298518b9808e0dca3f8b290a98101c27f515448e
Robert Knight <robertknight@gmail.com>
2019-10-17 12:17:15 +0100

    Convert markdown editor to Preact
    Convert the editor part of the `<markdown>` component to Preact.
    
     - Remove the `readOnly` input as this mode is now handled by the
       separate `MarkdownView` component
     - Improve toolbar a11y by:
       - Adding the "toolbar" ARIA role and an aria-label to the toolbar container
       - Using `button` elements for toolbar buttons
       - Increasing the hit target area for buttons
     - Improve toolbar styling by:
       - Using flexbox
       - Using the modern color palette
       - Fixing the padding around the preview output

---
+++ src/sidebar/components/test/markdown-editor-test.js
@@ -0,0 +1,248 @@
+'use strict';
+
+const { createElement, render } = require('preact');
+const { act } = require('preact/test-utils');
+const { mount } = require('enzyme');
+
+const { LinkType } = require('../../markdown-commands');
+const MarkdownEditor = require('../markdown-editor');
+
+describe('MarkdownEditor', () => {
+  const formatResult = {
+    text: 'formatted text',
+    selectionStart: 0,
+    selectionEnd: 0,
+  };
+  const fakeMarkdownCommands = {
+    convertSelectionToLink: sinon.stub().returns(formatResult),
+    toggleBlockStyle: sinon.stub().returns(formatResult),
+    toggleSpanStyle: sinon.stub().returns(formatResult),
+    LinkType,
+  };
+
+  let MarkdownView;
+
+  beforeEach(() => {
+    fakeMarkdownCommands.convertSelectionToLink.resetHistory();
+    fakeMarkdownCommands.toggleBlockStyle.resetHistory();
+    fakeMarkdownCommands.toggleSpanStyle.resetHistory();
+
+    MarkdownView = function MarkdownView() {
+      return null;
+    };
+
+    MarkdownEditor.$imports.$mock({
+      '../markdown-commands': fakeMarkdownCommands,
+      './markdown-view': MarkdownView,
+    });
+  });
+
+  afterEach(() => {
+    MarkdownEditor.$imports.$restore();
+  });
+
+  const commands = [
+    {
+      command: 'Bold',
+      key: 'b',
+      effect: [fakeMarkdownCommands.toggleSpanStyle, '**', '**', 'Bold'],
+    },
+    {
+      command: 'Italic',
+      key: 'i',
+      effect: [fakeMarkdownCommands.toggleSpanStyle, '*', '*', 'Italic'],
+    },
+    {
+      command: 'Quote',
+      key: 'q',
+      effect: [fakeMarkdownCommands.toggleBlockStyle, '> '],
+    },
+    {
+      command: 'Insert link',
+      key: 'l',
+      effect: [fakeMarkdownCommands.convertSelectionToLink],
+    },
+    {
+      command: 'Insert image',
+      key: null,
+      effect: [
+        fakeMarkdownCommands.convertSelectionToLink,
+        fakeMarkdownCommands.LinkType.IMAGE_LINK,
+      ],
+    },
+    {
+      command: 'Insert math (LaTeX is supported)',
+      key: null,
+      effect: [
+        fakeMarkdownCommands.toggleSpanStyle,
+        '$$',
+        '$$',
+        'Insert LaTeX',
+      ],
+    },
+    {
+      command: 'Bulleted list',
+      key: 'u',
+      effect: [fakeMarkdownCommands.toggleBlockStyle, '* '],
+    },
+    {
+      command: 'Numbered list',
+      key: 'o',
+      effect: [fakeMarkdownCommands.toggleBlockStyle, '1. '],
+    },
+  ];
+  commands.forEach(({ command, key, effect }) => {
+    describe(`"${command}" toolbar command`, () => {
+      it('applies formatting when toolbar button is clicked', () => {
+        const onEditText = sinon.stub();
+        const wrapper = mount(
+          <MarkdownEditor
+            text="test"
+            readOnly={false}
+            onEditText={onEditText}
+          />
+        );
+        const button = wrapper.find(
+          `ToolbarButton[title="${command}"] > button`
+        );
+        const input = wrapper.find('textarea').getDOMNode();
+        input.selectionStart = 0;
+        input.selectionEnd = 4;
+
+        button.simulate('click');
+
+        assert.calledWith(onEditText, {
+          text: 'formatted text',
+        });
+        const [formatFunction, ...args] = effect;
+        assert.calledWith(
+          formatFunction,
+          sinon.match({ text: 'test', selectionStart: 0, selectionEnd: 4 }),
+          ...args
+        );
+      });
+
+      if (key) {
+        it('applies formatting when shortcut key is pressed', () => {
+          const onEditText = sinon.stub();
+          const wrapper = mount(
+            <MarkdownEditor text="test" onEditText={onEditText} />
+          );
+          const input = wrapper.find('textarea');
+          input.getDOMNode().selectionStart = 0;
+          input.getDOMNode().selectionEnd = 4;
+
+          input.simulate('keydown', {
+            ctrlKey: true,
+            key,
+          });
+
+          assert.calledWith(onEditText, {
+            text: 'formatted text',
+          });
+          const [formatFunction, ...args] = effect;
+          assert.calledWith(
+            formatFunction,
+            sinon.match({ text: 'test', selectionStart: 0, selectionEnd: 4 }),
+            ...args
+          );
+        });
+      }
+    });
+  });
+
+  [
+    {
+      // Shortcut letter but without ctrl key.
+      key: 'b',
+      ctrlKey: false,
+    },
+    {
+      // Ctrl key with non-shortcut letter
+      key: 'w',
+      ctrlKey: true,
+    },
+  ].forEach(({ ctrlKey, key }) => {
+    it('does not apply formatting when a non-shortcut key is pressed', () => {
+      const onEditText = sinon.stub();
+      const wrapper = mount(
+        <MarkdownEditor text="test" onEditText={onEditText} />
+      );
+      const input = wrapper.find('textarea');
+
+      input.simulate('keydown', {
+        ctrlKey,
+        key,
+      });
+
+      assert.notCalled(onEditText);
+    });
+  });
+
+  it('calls `onEditText` callback when text is changed', () => {
+    const onEditText = sinon.stub();
+    const wrapper = mount(
+      <MarkdownEditor text="test" onEditText={onEditText} />
+    );
+    const input = wrapper.find('textarea').getDOMNode();
+    input.value = 'changed';
+    wrapper.find('textarea').simulate('input');
+    assert.calledWith(onEditText, {
+      text: 'changed',
+    });
+  });
+
+  it('enters preview mode when Preview button is clicked', () => {
+    const wrapper = mount(<MarkdownEditor text="test" />);
+
+    const previewButton = wrapper
+      .find('button')
+      .filterWhere(el => el.text() === 'Preview');
+    previewButton.simulate('click');
+
+    assert.isFalse(wrapper.find('textarea').exists());
+    assert.isTrue(wrapper.find('MarkdownView').exists());
+    wrapper
+      .find('button')
+      .filterWhere(el => el.text() !== 'Write')
+      .forEach(el => assert.isTrue(el.prop('disabled')));
+  });
+
+  it('exits preview mode when Write button is clicked', () => {
+    const wrapper = mount(<MarkdownEditor text="test" />);
+
+    // Switch to "Preview" mode.
+    const previewButton = wrapper
+      .find('button')
+      .filterWhere(el => el.text() === 'Preview');
+    previewButton.simulate('click');
+
+    // Switch back to "Write" mode.
+    const writeButton = wrapper
+      .find('button')
+      .filterWhere(el => el.text() === 'Write');
+    writeButton.simulate('click');
+
+    assert.isTrue(wrapper.find('textarea').exists());
+    assert.isFalse(wrapper.find('MarkdownView').exists());
+    wrapper
+      .find('button')
+      .filterWhere(el => el.text() !== 'Preview')
+      .forEach(el => assert.isFalse(el.prop('disabled')));
+  });
+
+  it('focuses the input field when created', () => {
+    const container = document.createElement('div');
+    try {
+      document.body.focus();
+      document.body.appendChild(container);
+      act(() => {
+        render(<MarkdownEditor text="test" />, container);
+      });
+
+      assert.equal(document.activeElement.nodeName, 'TEXTAREA');
+    } finally {
+      container.remove();
+    }
+  });
+});
