fc037cdf6373497acca8c5c1f106e34ffa67e7c0
Lyza Danger Gardner <lyza@lyza.com>
2019-10-22 12:31:13 -0400

    Add additional SVG icons

--- src/sidebar/components/svg-icon.js
+++ src/sidebar/components/svg-icon.js
@@ -8,6 +8,9 @@ const propTypes = require('prop-types');
 // The list of supported icons
 const icons = {
   add: require('../../images/icons/add.svg'),
+  annotate: require('../../images/icons/annotate.svg'),
+  'arrow-left': require('../../images/icons/arrow-left.svg'),
+  'arrow-right': require('../../images/icons/arrow-right.svg'),
   cancel: require('../../images/icons/cancel.svg'),
   'collapse-menu': require('../../images/icons/collapse-menu.svg'),
   'expand-menu': require('../../images/icons/expand-menu.svg'),
@@ -18,11 +21,13 @@ const icons = {
   facebook: require('../../images/icons/facebook.svg'),
   groups: require('../../images/icons/groups.svg'),
   help: require('../../images/icons/help.svg'),
+  highlight: require('../../images/icons/highlight.svg'),
   leave: require('../../images/icons/leave.svg'),
   lock: require('../../images/icons/lock.svg'),
   logo: require('../../images/icons/logo.svg'),
   public: require('../../images/icons/public.svg'),
   refresh: require('../../images/icons/refresh.svg'),
+  reply: require('../../images/icons/reply.svg'),
   share: require('../../images/icons/share.svg'),
   twitter: require('../../images/icons/twitter.svg'),
 };
