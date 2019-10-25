30ef9eccc77f1f3732bececd9f7280377ee2c8d1
Manuel Pereira <manuelpereiralds@gmail.com>
2019-10-25 08:28:40 -0300

    Update authErrorRedirectionHandler.js

--- src/inspera/scripts/authErrorRedirectionHandler.js
+++ src/inspera/scripts/authErrorRedirectionHandler.js
@@ -2,12 +2,9 @@
 /*
 * This function handles authentication errors resulting after calls to the API, later it handles redirection
 * wether user is an admin or an student, checks if user is annotation in the pdfjs service.
-* @param response - The regular API response from a Promise.
 */
-function authErrorRedirectionHandler(response) {
-    if (response.status === 401) {
-        window.parent.parent.postMessage({ hypothesis: true, action: 'redirectToLogin' }, '*');
-    }
+function authErrorRedirectionHandler() {
+    window.parent.parent.postMessage({ hypothesis: true, action: 'redirectToLogin' }, '*');
 }
 
 module.exports = authErrorRedirectionHandler;
