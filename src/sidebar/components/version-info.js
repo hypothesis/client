b9fcd72f1b9f892824e9f035d478aad777be8078
Lyza Danger Gardner <lyza@lyza.com>
2019-11-08 16:31:58 -0500

    Add “copy version details” (to clipboard) feature

--- src/sidebar/components/version-info.js
+++ src/sidebar/components/version-info.js
@@ -3,25 +3,47 @@
 const propTypes = require('prop-types');
 const { createElement } = require('preact');
 
+const { copyText } = require('../util/copy-to-clipboard');
+const { withServices } = require('../util/service-context');
+
+const SvgIcon = require('./svg-icon');
+
 /**
  * Display current client version info
  */
-function VersionInfo({ versionData }) {
+function VersionInfo({ flash, versionData }) {
+  const copyVersionData = () => {
+    try {
+      copyText(versionData.asFormattedString());
+      flash.info('Copied version info to clipboard');
+    } catch (err) {
+      flash.error('Unable to copy version info');
+    }
+  };
+
   return (
-    <dl className="version-info">
-      <dt className="version-info__key">Version</dt>
-      <dd className="version-info__value">{versionData.version}</dd>
-      <dt className="version-info__key">User Agent</dt>
-      <dd className="version-info__value">{versionData.userAgent}</dd>
-      <dt className="version-info__key">URL</dt>
-      <dd className="version-info__value">{versionData.url}</dd>
-      <dt className="version-info__key">Fingerprint</dt>
-      <dd className="version-info__value">{versionData.fingerprint}</dd>
-      <dt className="version-info__key">Account</dt>
-      <dd className="version-info__value">{versionData.account}</dd>
-      <dt className="version-info__key">Date</dt>
-      <dd className="version-info__value">{versionData.timestamp}</dd>
-    </dl>
+    <div>
+      <dl className="version-info">
+        <dt className="version-info__key">Version</dt>
+        <dd className="version-info__value">{versionData.version}</dd>
+        <dt className="version-info__key">User Agent</dt>
+        <dd className="version-info__value">{versionData.userAgent}</dd>
+        <dt className="version-info__key">URL</dt>
+        <dd className="version-info__value">{versionData.url}</dd>
+        <dt className="version-info__key">Fingerprint</dt>
+        <dd className="version-info__value">{versionData.fingerprint}</dd>
+        <dt className="version-info__key">Account</dt>
+        <dd className="version-info__value">{versionData.account}</dd>
+        <dt className="version-info__key">Date</dt>
+        <dd className="version-info__value">{versionData.timestamp}</dd>
+      </dl>
+      <div className="version-info__actions">
+        <button className="version-info__copy-btn" onClick={copyVersionData}>
+          <SvgIcon name="copy" className="version-info__copy-btn-icon" />
+          Copy version details
+        </button>
+      </div>
+    </div>
   );
 }
 
@@ -32,6 +54,11 @@ VersionInfo.propTypes = {
    * @type {import('../util/version-info').VersionData}
    */
   versionData: propTypes.object.isRequired,
+
+  /** injected properties */
+  flash: propTypes.object.isRequired,
 };
 
-module.exports = VersionInfo;
+VersionInfo.injectedProps = ['flash'];
+
+module.exports = withServices(VersionInfo);
