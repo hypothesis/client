1b975962fc5202370dd057456fa04149eb83a893
Robert Knight <robertknight@gmail.com>
2019-11-23 14:35:02 +0000

    Improve compression of JavaScript bundles
    Previously only the _contents_ of modules were minified, but not the
    module infrastructure that Browserify adds around it. Given lots of modules,
    this adds up.
    
    Replace `uglifify` with a small wrapper around terser, the package that
    actually compresses the code, and use it to minify both the contents of
    modules and the generated wrapper code. Calling terser directly allows
    us to a) use the current version of it (uglifyify was stuck on Terser
    3.x) and b) upgrade/configure/replace it more directly in future.
    
    One other change here is that minifying modules is now done _after_ the
    transform that replaces `if (process.env.NODE_ENV == ...)` checks with
    `if (true)` or `if (false)`. This enables dead-code removal to remove
    the logic entirely.
    
    This reduces the minified size of `sidebar.bundle.js` from 368 KB to 311
    KB (-15%) and `annotator.bundle.js` from 170 KB to 152 KB (-12.7%).
    Some vendor bundles are smaller, others are unaffected because they were
    already fully minified. The total minified size of all bundles is reduced by ~7%.

---
+++ scripts/gulp/minify-stream.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { Transform } = require('stream');
+
+const terser = require('terser');
+
+/**
+ * Return a Node `Transform` stream that minifies JavaScript input.
+ *
+ * This is designed to be used both to process individual modules as a Browserify
+ * transform, and also to be applied to the output of the whole bundle to
+ * compress the module infrastructure that Browserify adds.
+ *
+ * @example
+ *   browserify(['src/app.js'])
+ *     .transform({ global: true }, minifyStream) // Minify individual modules
+ *     .pipe(minifyStream()) // Minify the code added by Browserify
+ *     .pipe(output);
+ */
+function minifyStream() {
+  return new Transform({
+    transform(data, encoding, callback) {
+      if (!this.chunks) {
+        this.chunks = [];
+      }
+      this.chunks.push(data);
+      callback();
+    },
+
+    flush(callback) {
+      const code = Buffer.concat(this.chunks).toString();
+      const minified = terser.minify(code, {
+        // See https://github.com/terser/terser#minify-options-structure
+        sourceMap: { url: 'inline' },
+      }).code;
+      this.push(minified);
+      callback();
+    },
+  });
+}
+
+module.exports = minifyStream;
