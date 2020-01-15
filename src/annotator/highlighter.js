a9fb9e62e3a41be11975e4c21454a99066902309
Robert Knight <robertknight@gmail.com>
2020-01-15 08:55:06 +0000

    Convert text highlighter to JS
    Remove the highlighter implementation abstraction layer in
    src/annotator/highlighter since only one type of highlighter was ever
    implemented and convert the one implementation in
    src/annotator/highlighter/dom-wrap-highlighter/index.coffee to JS as
    src/annotator/highlighter.js.

---
+++ src/annotator/highlighter.js
@@ -0,0 +1,59 @@
+import $ from 'jquery';
+
+/**
+ * Wraps the DOM Nodes within the provided range with a highlight
+ * element of the specified class and returns the highlight Elements.
+ *
+ * normedRange - A NormalizedRange to be highlighted.
+ * cssClass - A CSS class to use for the highlight (default: 'annotator-hl')
+ *
+ * Returns an array of highlight Elements.
+ */
+export function highlightRange(normedRange, cssClass = 'annotator-hl') {
+  const white = /^\s*$/;
+
+  // A custom element name is used here rather than `<span>` to reduce the
+  // likelihood of highlights being hidden by page styling.
+  const hl = $(
+    `<hypothesis-highlight class='${cssClass}'></hypothesis-highlight>`
+  );
+
+  // Ignore text nodes that contain only whitespace characters. This prevents
+  // spans being injected between elements that can only contain a restricted
+  // subset of nodes such as table rows and lists. This does mean that there
+  // may be the odd abandoned whitespace node in a paragraph that is skipped
+  // but better than breaking table layouts.
+  const nodes = $(normedRange.textNodes()).filter(function() {
+    return !white.test(this.nodeValue);
+  });
+
+  return nodes
+    .wrap(hl)
+    .parent()
+    .toArray();
+}
+
+export function removeHighlights(highlights) {
+  for (let h of highlights) {
+    if (h.parentNode) {
+      $(h).replaceWith(h.childNodes);
+    }
+  }
+}
+
+/**
+ * Get the bounding client rectangle of a collection in viewport coordinates.
+ * Unfortunately, Chrome has issues[1] with Range.getBoundingClient rect or we
+ * could just use that.
+ * [1] https://code.google.com/p/chromium/issues/detail?id=324437
+ */
+export function getBoundingClientRect(collection) {
+  // Reduce the client rectangles of the highlights to a bounding box
+  const rects = collection.map(n => n.getBoundingClientRect());
+  return rects.reduce((acc, r) => ({
+    top: Math.min(acc.top, r.top),
+    left: Math.min(acc.left, r.left),
+    bottom: Math.max(acc.bottom, r.bottom),
+    right: Math.max(acc.right, r.right),
+  }));
+}
---
+++ src/annotator/test/highlighter-test.js
@@ -0,0 +1,116 @@
+import Range from '../anchoring/range';
+
+import {
+  highlightRange,
+  removeHighlights,
+  getBoundingClientRect,
+} from '../highlighter';
+
+describe('annotator/highlighter', () => {
+  describe('highlightRange', () => {
+    it('wraps a highlight span around the given range', () => {
+      const txt = document.createTextNode('test highlight span');
+      const el = document.createElement('span');
+      el.appendChild(txt);
+      const r = new Range.NormalizedRange({
+        commonAncestor: el,
+        start: txt,
+        end: txt,
+      });
+
+      const result = highlightRange(r);
+
+      assert.equal(result.length, 1);
+      assert.strictEqual(el.childNodes[0], result[0]);
+      assert.equal(result[0].nodeName, 'HYPOTHESIS-HIGHLIGHT');
+      assert.isTrue(result[0].classList.contains('annotator-hl'));
+    });
+
+    it('skips text nodes that are only white space', () => {
+      const txt = document.createTextNode('one');
+      const blank = document.createTextNode(' ');
+      const txt2 = document.createTextNode('two');
+      const el = document.createElement('span');
+      el.appendChild(txt);
+      el.appendChild(blank);
+      el.appendChild(txt2);
+      const r = new Range.NormalizedRange({
+        commonAncestor: el,
+        start: txt,
+        end: txt2,
+      });
+
+      const result = highlightRange(r);
+
+      assert.equal(result.length, 2);
+      assert.strictEqual(el.childNodes[0], result[0]);
+      assert.strictEqual(el.childNodes[2], result[1]);
+    });
+  });
+
+  describe('removeHighlights', () => {
+    it('unwraps all the elements', () => {
+      const txt = document.createTextNode('word');
+      const el = document.createElement('span');
+      const hl = document.createElement('span');
+      const div = document.createElement('div');
+      el.appendChild(txt);
+      hl.appendChild(el);
+      div.appendChild(hl);
+
+      removeHighlights([hl]);
+
+      assert.isNull(hl.parentNode);
+      assert.strictEqual(el.parentNode, div);
+    });
+
+    it('does not fail on nodes with no parent', () => {
+      const txt = document.createTextNode('no parent');
+      const hl = document.createElement('span');
+      hl.appendChild(txt);
+
+      removeHighlights([hl]);
+    });
+  });
+
+  describe('getBoundingClientRect', () => {
+    it('returns the bounding box of all the highlight client rectangles', () => {
+      const rects = [
+        {
+          top: 20,
+          left: 15,
+          bottom: 30,
+          right: 25,
+        },
+        {
+          top: 10,
+          left: 15,
+          bottom: 20,
+          right: 25,
+        },
+        {
+          top: 15,
+          left: 20,
+          bottom: 25,
+          right: 30,
+        },
+        {
+          top: 15,
+          left: 10,
+          bottom: 25,
+          right: 20,
+        },
+      ];
+      const fakeHighlights = rects.map(r => {
+        return { getBoundingClientRect: () => r };
+      });
+
+      const result = getBoundingClientRect(fakeHighlights);
+
+      assert.equal(result.left, 10);
+      assert.equal(result.top, 10);
+      assert.equal(result.right, 30);
+      assert.equal(result.bottom, 30);
+    });
+  });
+});
