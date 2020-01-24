18836006dbef29a3e3191932683db86578b8c97d
Kyle Keating <kkeating@hypothes.is>
2020-01-24 11:31:23 -0800

    Add Datalist component
    Allows for more customizatoin than the native html <datalist> element.

---
+++ src/sidebar/components/datalist.js
@@ -0,0 +1,105 @@
+import { createElement } from 'preact';
+import classnames from 'classnames';
+import propTypes from 'prop-types';
+import { useMemo } from 'preact/hooks';
+
+const defaultListFormatter = item => item;
+
+/**
+ * Custom datalist component. Use this in conjunction with an input field.
+ */
+
+function Datalist({
+  activeItem = -1,
+  id,
+  itemPrefixId,
+  list,
+  listFormatter = defaultListFormatter,
+  onSelectItem,
+  open = false,
+}) {
+  const items = useMemo(() => {
+    return list.map((item, index) => {
+      // only add an id if itemPrefixId is passed
+      const props = itemPrefixId ? { id: `${itemPrefixId}${index}` } : {};
+
+      return (
+        <li
+          key={`datalist-${index}`}
+          role="option"
+          aria-selected={activeItem === index}
+          className={classnames(
+            {
+              'is-selected': activeItem === index,
+            },
+            'datalist__li'
+          )}
+          onClick={() => {
+            onSelectItem(item);
+          }}
+          {...props}
+        >
+          {listFormatter(item, index)}
+        </li>
+      );
+    });
+  }, [activeItem, itemPrefixId, list, listFormatter, onSelectItem]);
+
+  const props = id ? { id } : {}; // only add the id if its passed
+  return (
+    <div className="datalist">
+      {list.length > 0 && open && (
+        <div className="datalist__items">
+          <ul
+            tabIndex="-1"
+            aria-label="Suggestions"
+            role="listbox"
+            className="datalist__ul"
+            {...props}
+          >
+            {items}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+Datalist.propTypes = {
+  /**
+   * The activeItem is highlighted.
+   */
+  activeItem: propTypes.number,
+  /**
+   * Optional unique HTML attribute id.
+   */
+  id: propTypes.string,
+  /**
+   * Optional unique HTML attribute id prefix for each item in the list.
+   * The final value of each items' id is `{itemPrefixId}{activeItem}`
+   */
+  itemPrefixId: propTypes.string,
+
+  /**
+   * The list of items to render. This can be a simple list of
+   * strings or a list of objects when used with listFormatter.
+   */
+  list: propTypes.array.isRequired,
+  /**
+   * An optional formatter to render each item inside an <li> tag
+   * This is useful if the list is an array of objects rather than
+   * just strings.
+   */
+  listFormatter: propTypes.func,
+  /**
+   * Callback when an item is clicked with the mouse.
+   *  (item, index) => {}
+   */
+  onSelectItem: propTypes.func.isRequired,
+  /**
+   * Is the list open or closed?
+   */
+  open: propTypes.bool,
+};
+
+module.exports = Datalist;
---
+++ src/sidebar/components/test/datalist-test.js
@@ -0,0 +1,164 @@
+import { mount } from 'enzyme';
+import { createElement } from 'preact';
+
+import Datalist from '../datalist';
+import { $imports } from '../datalist';
+
+import mockImportedComponents from './mock-imported-components';
+
+describe('Datalist', function() {
+  let fakeList;
+  let fakeOnSelectItem;
+  let fakeListFormatter;
+  function createComponent(props) {
+    return mount(
+      <Datalist
+        list={fakeList}
+        open={true}
+        onSelectItem={fakeOnSelectItem}
+        {...props}
+      />
+    );
+  }
+
+  beforeEach(function() {
+    fakeList = ['tag1', 'tag2'];
+    fakeOnSelectItem = sinon.stub();
+    fakeListFormatter = sinon.stub();
+    $imports.$mock(mockImportedComponents());
+  });
+
+  afterEach(() => {
+    $imports.$restore();
+  });
+
+  it('does not render the list when open is false', () => {
+    const wrapper = createComponent({ open: false });
+    assert.isFalse(wrapper.find('.datalist__items').exists());
+  });
+
+  it('does not render the list when list is empty', () => {
+    const wrapper = createComponent({ list: [] });
+    assert.isFalse(wrapper.find('.datalist__items').exists());
+  });
+
+  it('sets unique keys to the <li> items', () => {
+    const wrapper = createComponent();
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(0)
+        .key(),
+      'datalist-0'
+    );
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(1)
+        .key(),
+      'datalist-1'
+    );
+  });
+
+  it('renders the items in order of the list prop', () => {
+    const wrapper = createComponent();
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(0)
+        .text(),
+      'tag1'
+    );
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(1)
+        .text(),
+      'tag2'
+    );
+  });
+
+  it('does not apply the `is-selected` class to items that are not selected', () => {
+    const wrapper = createComponent();
+    assert.isFalse(wrapper.find('li.is-selected').exists());
+  });
+
+  it('applies `is-selected` class only to the <li> at the matching index', () => {
+    const wrapper = createComponent({ activeItem: 0 });
+    assert.isTrue(
+      wrapper
+        .find('li')
+        .at(0)
+        .hasClass('is-selected')
+    );
+    assert.isFalse(
+      wrapper
+        .find('li')
+        .at(1)
+        .hasClass('is-selected')
+    );
+  });
+
+  it('calls onSelect when an <li> is clicked with the corresponding item', () => {
+    const wrapper = createComponent({ activeItem: 0 });
+    wrapper
+      .find('li')
+      .at(0)
+      .simulate('click');
+    assert.calledWith(fakeOnSelectItem, 'tag1');
+  });
+
+  it('calls listFormatter when building the <li> items if present', () => {
+    createComponent({ listFormatter: fakeListFormatter });
+    assert.calledWith(fakeListFormatter, 'tag1', 0);
+    assert.calledWith(fakeListFormatter, 'tag2', 1);
+  });
+
+  it('adds the `id` attribute to <ul> only if its present', () => {
+    let wrapper = createComponent();
+    assert.isNotOk(wrapper.find('ul').prop('id'));
+    wrapper = createComponent({ id: 'datalist-id' });
+    assert.equal(wrapper.find('ul').prop('id'), 'datalist-id');
+  });
+
+  it('creates unique ids on the <li> tags with the `itemPrefixId` only if its present', () => {
+    let wrapper = createComponent();
+    assert.isNotOk(
+      wrapper
+        .find('li')
+        .at(0)
+        .prop('id')
+    );
+    wrapper = createComponent({ itemPrefixId: 'item-prefix-id-' });
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(0)
+        .prop('id'),
+      'item-prefix-id-0'
+    );
+    assert.equal(
+      wrapper
+        .find('li')
+        .at(1)
+        .prop('id'),
+      'item-prefix-id-1'
+    );
+  });
+
+  it('sets the `aria-selected` attribute on the active index ', () => {
+    const wrapper = createComponent({ activeItem: 0 });
+    assert.isTrue(
+      wrapper
+        .find('li')
+        .at(0)
+        .prop('aria-selected')
+    );
+    assert.isFalse(
+      wrapper
+        .find('li')
+        .at(1)
+        .prop('aria-selected')
+    );
+  });
+});
