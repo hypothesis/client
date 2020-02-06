f39b081054841c61d5e18c0c98ea4a5eb30c14c9
Kyle Keating <kkeating@hypothes.is>
2020-02-06 11:29:31 -0800

    Add `disabled` prop to the Button component

--- src/sidebar/components/test/button-test.js
+++ src/sidebar/components/test/button-test.js
@@ -91,6 +91,16 @@ describe('Button', () => {
     assert.isTrue(wrapper.find('button').hasClass('button--primary'));
   });
 
+  it('disables the button when `disabled` prop is true', () => {
+    const wrapper = createComponent({ disabled: true });
+    assert.isTrue(wrapper.find('button[disabled=true]').exists());
+  });
+
+  it('shall not disable the button by default', () => {
+    const wrapper = createComponent();
+    assert.isTrue(wrapper.find('button[disabled=false]').exists());
+  });
+
   it(
     'should pass a11y checks',
     checkAccessibility({
