9b6db0e2548f486c928291e33b4217a9ef593230
Lyza Danger Gardner <lyza@lyza.com>
2020-07-28 11:29:08 -0400

    Only force threads to visible if they are not already visible

--- src/sidebar/services/threads.js
+++ src/sidebar/services/threads.js
@@ -1,15 +1,24 @@
+/**
+ * @typedef {import('../util/build-thread').Thread} Thread
+ */
+
 // @ngInject
 export default function threadsService(store) {
   /**
    * Make this thread and all of its children "visible". This has the effect of
    * "unhiding" a thread which is currently hidden by an applied search filter
-   * (as well as its child threads).
+   * (as well as its child threads). Only threads that are not currently visible
+   * will be forced visible.
+   *
+   * @param {Thread} thread
    */
   function forceVisible(thread) {
     thread.children.forEach(child => {
       forceVisible(child);
     });
-    store.setForcedVisible(thread.id, true);
+    if (!thread.visible) {
+      store.setForcedVisible(thread.id, true);
+    }
   }
 
   return {
