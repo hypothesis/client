344204ff72b196d3d4405113412f80714cb3ffe7
Lyza Danger Gardner <lyza@lyza.com>
2020-08-06 11:53:38 -0400

    Update dev server document configuration and add some templating

--- scripts/gulp/dev-server.js
+++ scripts/gulp/dev-server.js
@@ -3,19 +3,74 @@
 const fs = require('fs');
 const log = require('fancy-log');
 const urlParser = require('url');
+const Mustache = require('mustache');
 
 const { createServer, useSsl } = require('./create-server');
 
-function readmeText() {
-  return fs.readFileSync('./README.md', 'utf-8');
+const DOCUMENT_PATH = './test-documents/';
+const DOCUMENT_PATTERN = /\.html\.mustache/;
+
+/**
+ * Render some content in an HTML document with `<pre>` tags
+ *
+ * @param {string} filename - source to inject between `<pre>` tags
+ * @return {string}
+ */
+function preformattedContent(filename) {
+  const textContent = fs.readFileSync(filename, 'utf-8');
+  const template = fs.readFileSync(
+    `${DOCUMENT_PATH}preformatted.template.mustache`,
+    'utf-8'
+  );
+  return Mustache.render(template, { textContent });
 }
 
-function licenseText() {
-  return fs.readFileSync('./LICENSE', 'utf-8');
+/**
+ * Generate `<script>` content for client configuration and injection
+ *
+ * @param {string} clientUrl
+ * @return {string}
+ */
+function renderConfig(clientUrl) {
+  const scriptTemplate = fs.readFileSync(
+    `${DOCUMENT_PATH}client-config.js.mustache`,
+    'utf-8'
+  );
+  return Mustache.render(scriptTemplate, { clientUrl });
 }
 
-function codeOfConductText() {
-  return fs.readFileSync('./CODE_OF_CONDUCT', 'utf-8');
+/**
+ * Read in the file at `filename` and render it as a template, injecting
+ * script source for the embedded client.
+ *
+ * @param {string} filename
+ * @param {string} clientUrl
+ * @param [{Object}] context - optional extra view context
+ * @return {string} Rendered HTML template with injected script
+ */
+function injectClientScript(filename, clientUrl, context = {}) {
+  const documentTemplate = fs.readFileSync(filename, 'utf-8');
+  const scriptContent = renderConfig(clientUrl);
+  context = { ...context, hypothesisScript: scriptContent };
+  return Mustache.render(documentTemplate, context);
+}
+
+/**
+ * Provide a "route" for all HTML documents in the test-documents directory
+ *
+ * @return {Object<string, string>} - Routes, mapping route (path) to
+ *                                    filepath of HTML document
+ */
+function buildDocumentRoutes() {
+  const documentRoutes = {};
+  const documentPaths = fs
+    .readdirSync(DOCUMENT_PATH)
+    .filter(filename => filename.match(DOCUMENT_PATTERN));
+  documentPaths.forEach(filename => {
+    const shortName = filename.replace(DOCUMENT_PATTERN, '');
+    documentRoutes[`/document/${shortName}`] = `${DOCUMENT_PATH}${filename}`;
+  });
+  return documentRoutes;
 }
 
 /**
@@ -32,118 +87,30 @@ function codeOfConductText() {
  * @constructor
  */
 function DevServer(port, config) {
+  const documentRoutes = buildDocumentRoutes();
+  const preformattedRoutes = {
+    '/document/code_of_conduct': './CODE_OF_CONDUCT',
+    '/document/license': './LICENSE',
+  };
+
   function listen() {
     const app = function (req, response) {
       const url = urlParser.parse(req.url);
       let content;
 
-      if (url.pathname === '/document/license') {
-        content = `
-          <!DOCTYPE html>
-          <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Hypothesis in-line frame document - License</title>
-          </head>
-          <body>
-            <pre style="margin: 20px;">${licenseText()}</pre>
-          </body>
-          </html>
-        `;
-      } else if (url.pathname === '/document/code_of_conduct') {
-        content = `
-          <!DOCTYPE html>
-          <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Hypothesis in-line frame document - Code of conduct</title>
-          </head>
-          <body>
-            <pre style="margin: 20px;">${codeOfConductText()}</pre>
-          </body>
-          </html>
-        `;
+      if (documentRoutes[url.pathname]) {
+        content = injectClientScript(
+          documentRoutes[url.pathname],
+          config.clientUrl
+        );
+      } else if (preformattedRoutes[url.pathname]) {
+        content = preformattedContent(preformattedRoutes[url.pathname]);
       } else {
-        content = `
-          <!DOCTYPE html>
-          <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Hypothesis Client Test</title>
-          </head>
-          <body>
-            <div data-hypothesis-trigger style="margin: 75px 0 0 75px;">
-              Number of annotations:
-              <span data-hypothesis-annotation-count>...</span>
-            </div>
-            <div style="margin: 10px 0 0 75px;">
-              <button id="add-test" style="padding: 0.6em; font-size: 0.75em">Toggle 2nd Frame</button>
-            </div>
-            <div style="margin: 10px 0 0 75px;">
-              <iframe enable-annotation id="iframe1" src="/document/license" style="width: 50%;height: 300px;"></iframe>
-            </div>
-            <div id="iframe2-container" style="margin: 10px 0 0 75px;">
-            </div>
-            <pre style="margin: 20px 75px 75px 75px;">${readmeText()}</pre>
-            <script>
-            var appHost = document.location.hostname;
-
-            window.hypothesisConfig = function () {
-              // See https://h.readthedocs.io/projects/client/en/latest/publishers/config/
-              return {
-                // enableExperimentalNewNoteButton: true,
-                // showHighlights: 'always',
-                // theme: 'clean',
-
-                // Example focused user mode
-                // focus: {
-                //   user: {
-                //     username: 'foo',
-                //     authority: 'lms',
-                //     displayName: 'Foo Bar',
-                //   }
-                // },
-                // Example services config
-                // services: [{
-                //   apiUrl: 'http://localhost:5000/api',
-                //   authority: 'partner.org',
-                //   allowLeavingGroups: false,
-                //   groups: ['a-group-id', 'another-group-id'],
-                // }],
-
-                // Open the sidebar when the page loads
-                openSidebar: true,
-              };
-            };
-
-            var embedScript = document.createElement('script');
-            embedScript.src = '${
-              config.clientUrl
-            }'.replace('{current_host}', document.location.hostname);
-            document.body.appendChild(embedScript);
-
-            var iframeIsAdded = false;
-            var addIframeBtn = document.querySelector('#add-test');
-            if(addIframeBtn){
-              addIframeBtn.addEventListener('click', function() {
-                if (!iframeIsAdded) {
-                  var iframe1 = document.querySelector('#iframe1');
-                  var iframeNew = iframe1.cloneNode();
-                  iframeNew.src = "/document/code_of_conduct";
-                  iframeNew.id = "iframe2";
-                  iframeIsAdded = true;
-                  document.querySelector('#iframe2-container').appendChild(iframeNew);
-                } else {
-                  var iframe2 = document.querySelector('#iframe2');
-                  iframe2.parentNode.removeChild(iframe2);
-                  iframeIsAdded = false;
-                }
-              });
-            }
-            </script>
-          </body>
-          </html>
-        `;
+        content = injectClientScript(
+          `${DOCUMENT_PATH}index.template.mustache`,
+          config.clientUrl,
+          { readme: fs.readFileSync('./README.md', 'utf-8') }
+        );
       }
       response.end(content);
     };
