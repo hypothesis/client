6e9f8571730d5d2ad8fe8a628e2ea0c1c408cd39
Robert Knight <robertknight@gmail.com>
2020-08-27 11:38:01 +0100

    Convert src/annotator/sidebar.coffee to JS
    Convert this module, but not its tests, to JS. This is a pretty
    straightforward conversion with a few minor improvements:
    
     - The initialization of `options` was rewritten in a way that is
       consistent with `PdfSidebar` and avoids a non-idiomatic way of
       passing initialization parameters from derived to base class
    
     - Some comments that were hard to parse were rewritten or removed if
       they stated something obvious
    
     - Private methods and fields related to gesture state were prefixed
       with an underscore. Some of the sidebar tests did and still do access
       these, which is not ideal.

---
+++ src/annotator/sidebar.js
@@ -0,0 +1,326 @@
+import $ from 'jquery';
+import Hammer from 'hammerjs';
+
+import annotationCounts from './annotation-counts';
+import sidebarTrigger from './sidebar-trigger';
+import events from '../shared/bridge-events';
+import features from './features';
+
+import { ToolbarController } from './toolbar';
+
+// Minimum width to which the frame can be resized.
+const MIN_RESIZE = 280;
+
+// TODO - Convert this to an ES import once `host.coffee` is converted.
+// @ts-expect-error
+const Host = require('./host');
+
+const defaultConfig = {
+  Document: {},
+  TextSelection: {},
+  BucketBar: {
+    container: '.annotator-frame',
+  },
+};
+
+export default class Sidebar extends Host {
+  constructor(element, config) {
+    if (config.theme === 'clean' || config.externalContainerSelector) {
+      delete config.pluginClasses.BucketBar;
+    }
+
+    super(element, { ...defaultConfig, ...config });
+
+    if (
+      config.openSidebar ||
+      config.annotations ||
+      config.query ||
+      config.group
+    ) {
+      this.on('panelReady', () => this.show());
+    }
+
+    if (this.plugins.BucketBar) {
+      this.plugins.BucketBar.element.on('click', () => this.show());
+    }
+
+    // Set up the toolbar on the left edge of the sidebar.
+    const toolbarContainer = document.createElement('div');
+    this.toolbar = new ToolbarController(toolbarContainer, {
+      createAnnotation: () => this.createAnnotation(),
+      setSidebarOpen: open => (open ? this.show() : this.hide()),
+      setHighlightsVisible: show => this.setAllVisibleHighlights(show),
+    });
+    this.toolbar.useMinimalControls = config.theme === 'clean';
+
+    if (this.frame) {
+      // If using our own container frame for the sidebar, add the toolbar to it.
+      this.frame.prepend(toolbarContainer);
+      this.toolbarWidth = this.toolbar.getWidth();
+    } else {
+      // If using a host-page provided container for the sidebar, the toolbar is
+      // not shown.
+      this.toolbarWidth = 0;
+    }
+
+    this._gestureState = {
+      initial: /** @type {number|null} */ (null),
+      final: /** @type {number|null} */ (null),
+    };
+    this._setupGestures();
+    this.hide();
+
+    // Publisher-provided callback functions
+    const [serviceConfig] = config.services || [];
+    if (serviceConfig) {
+      this.onLoginRequest = serviceConfig.onLoginRequest;
+      this.onLogoutRequest = serviceConfig.onLogoutRequest;
+      this.onSignupRequest = serviceConfig.onSignupRequest;
+      this.onProfileRequest = serviceConfig.onProfileRequest;
+      this.onHelpRequest = serviceConfig.onHelpRequest;
+    }
+
+    this.onLayoutChange = config.onLayoutChange;
+
+    // Initial layout notification
+    this._notifyOfLayoutChange(false);
+    this._setupSidebarEvents();
+  }
+
+  _setupSidebarEvents() {
+    annotationCounts(document.body, this.crossframe);
+    sidebarTrigger(document.body, () => this.show());
+    features.init(this.crossframe);
+
+    this.crossframe.on('showSidebar', () => this.show());
+    this.crossframe.on('hideSidebar', () => this.hide());
+
+    const eventHandlers = [
+      [events.LOGIN_REQUESTED, this.onLoginRequest],
+      [events.LOGOUT_REQUESTED, this.onLogoutRequest],
+      [events.SIGNUP_REQUESTED, this.onSignupRequest],
+      [events.PROFILE_REQUESTED, this.onProfileRequest],
+      [events.HELP_REQUESTED, this.onHelpRequest],
+    ];
+    eventHandlers.forEach(([event, handler]) => {
+      if (handler) {
+        this.crossframe.on(event, () => handler());
+      }
+    });
+  }
+
+  _resetGestureState() {
+    this._gestureState = { initial: null, final: null };
+  }
+
+  _setupGestures() {
+    const toggle = $(this.toolbar.sidebarToggleButton);
+
+    if (toggle[0]) {
+      // Prevent any default gestures on the handle.
+      toggle.on('touchmove', e => e.preventDefault());
+
+      // Set up Hammer instance and handlers.
+      const manager = new Hammer.Manager(toggle[0])
+        // eslint-disable-next-line no-restricted-properties
+        .on('panstart panend panleft panright', this._onPan.bind(this))
+        // eslint-disable-next-line no-restricted-properties
+        .on('swipeleft swiperight', this._onSwipe.bind(this));
+
+      // Set up gesture recognition.
+      const pan = manager.add(
+        new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL })
+      );
+      const swipe = manager.add(
+        new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL })
+      );
+      swipe.recognizeWith(pan);
+    }
+  }
+
+  // Schedule any changes needed to update the sidebar layout.
+  _updateLayout() {
+    // Only schedule one frame at a time.
+    if (this.renderFrame) {
+      return;
+    }
+
+    // Schedule a frame.
+    this.renderFrame = requestAnimationFrame(() => {
+      this.renderFrame = null;
+
+      if (this._gestureState.final !== this._gestureState.initial) {
+        const margin = /** @type {number} */ (this._gestureState.final);
+        const width = -margin;
+        this.frame.css('margin-left', `${margin}px`);
+        if (width >= MIN_RESIZE) {
+          this.frame.css('width', `${width}px`);
+        }
+        this._notifyOfLayoutChange();
+      }
+    });
+  }
+
+  /**
+   * Notify integrator when sidebar is opened, closed or resized.
+   *
+   * @param {boolean} [expanded] -
+   *   `true` or `false` if the sidebar is being directly opened or closed, as
+   *   opposed to being resized via the sidebar's drag handles
+   */
+  _notifyOfLayoutChange(expanded) {
+    if (!this.onLayoutChange) {
+      return;
+    }
+
+    // The sidebar structure is:
+    //
+    // [ Toolbar    ][                                   ]
+    // [ ---------- ][ Sidebar iframe container (@frame) ]
+    // [ Bucket Bar ][                                   ]
+    //
+    // The sidebar iframe is hidden or shown by adjusting the left margin of
+    // its container.
+
+    const toolbarWidth = this.toolbarWidth || 0;
+    const frame = this.frame || this.externalFrame;
+    const rect = frame[0].getBoundingClientRect();
+    const computedStyle = window.getComputedStyle(frame[0]);
+    const width = parseInt(computedStyle.width);
+    const leftMargin = parseInt(computedStyle.marginLeft);
+
+    // The width of the sidebar that is visible on screen, including the
+    // toolbar, which is always visible.
+    let frameVisibleWidth = toolbarWidth;
+
+    if (typeof expanded === 'boolean') {
+      if (expanded) {
+        frameVisibleWidth += width;
+      }
+    } else {
+      if (leftMargin < MIN_RESIZE) {
+        frameVisibleWidth -= leftMargin;
+      } else {
+        frameVisibleWidth += width;
+      }
+
+      // Infer expanded state based on whether at least part of the sidebar
+      // frame is visible.
+      expanded = frameVisibleWidth > toolbarWidth;
+    }
+
+    this.onLayoutChange({
+      expanded,
+      width: expanded ? frameVisibleWidth : toolbarWidth,
+      height: rect.height,
+    });
+  }
+
+  _onPan(event) {
+    if (!this.frame) {
+      return;
+    }
+
+    switch (event.type) {
+      case 'panstart':
+        this._resetGestureState();
+
+        // Disable animated transition of sidebar position
+        this.frame.addClass('annotator-no-transition');
+
+        // Disable pointer events on the iframe.
+        this.frame.css('pointer-events', 'none');
+
+        this._gestureState.initial = parseInt(
+          getComputedStyle(this.frame[0]).marginLeft
+        );
+
+        break;
+      case 'panend':
+        this.frame.removeClass('annotator-no-transition');
+
+        // Re-enable pointer events on the iframe.
+        this.frame.css('pointer-events', '');
+
+        // Snap open or closed.
+        if (
+          this._gestureState.final === null ||
+          this._gestureState.final <= -MIN_RESIZE
+        ) {
+          this.show();
+        } else {
+          this.hide();
+        }
+        this._resetGestureState();
+        break;
+      case 'panleft':
+      case 'panright': {
+        if (typeof this._gestureState.initial !== 'number') {
+          return;
+        }
+
+        const margin = this._gestureState.initial;
+        const delta = event.deltaX;
+        this._gestureState.final = Math.min(Math.round(margin + delta), 0);
+        this._updateLayout();
+        break;
+      }
+    }
+  }
+
+  _onSwipe(event) {
+    switch (event.type) {
+      case 'swipeleft':
+        this.show();
+        break;
+      case 'swiperight':
+        this.hide();
+        break;
+    }
+  }
+
+  show() {
+    this.crossframe.call('sidebarOpened');
+
+    if (this.frame) {
+      this.frame.css('margin-left', `${-1 * this.frame.width()}px`);
+      this.frame.removeClass('annotator-collapsed');
+    }
+
+    this.toolbar.sidebarOpen = true;
+
+    if (this.options.showHighlights === 'whenSidebarOpen') {
+      this.setVisibleHighlights(true);
+    }
+
+    this._notifyOfLayoutChange(true);
+  }
+
+  hide() {
+    if (this.frame) {
+      this.frame.css('margin-left', '');
+      this.frame.addClass('annotator-collapsed');
+    }
+
+    this.toolbar.sidebarOpen = false;
+
+    if (this.options.showHighlights === 'whenSidebarOpen') {
+      this.setVisibleHighlights(false);
+    }
+
+    this._notifyOfLayoutChange(false);
+  }
+
+  isOpen() {
+    if (this.frame) {
+      return !this.frame.hasClass('annotator-collapsed');
+    } else {
+      // Assume an external frame is always open.
+      return true;
+    }
+  }
+
+  setAllVisibleHighlights(shouldShowHighlights) {
+    this.crossframe.call('setVisibleHighlights', shouldShowHighlights);
+  }
+}
