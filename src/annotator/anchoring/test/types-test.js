43f23cc5ee122530e48bbf55a8ed64a5b2a5a957
Kyle Keating <kkeating@hypothes.is>
2020-09-28 13:36:53 -0700

    Convert types.coffee to js
    - Convert types to js
    - Add missing test coverage in types module

--- src/annotator/anchoring/test/types-test.js
+++ src/annotator/anchoring/test/types-test.js
@@ -1,15 +1,17 @@
-import * as types from '../types';
-
-const TextQuoteAnchor = types.TextQuoteAnchor;
-const TextPositionAnchor = types.TextPositionAnchor;
+import {
+  RangeAnchor,
+  TextPositionAnchor,
+  TextQuoteAnchor,
+  $imports,
+} from '../types';
 
 // These are primarily basic API tests for the anchoring classes. Tests for
 // anchoring a variety of HTML and PDF content exist in `html-test` and
 // `pdf-test`.
-describe('types', function () {
+describe('annotator/anchoring/types', () => {
   let container;
 
-  before(function () {
+  beforeEach(() => {
     container = document.createElement('div');
     container.innerHTML = [
       'Four score and seven years ago our fathers brought forth on this continent,',
@@ -19,45 +21,338 @@ describe('types', function () {
     document.body.appendChild(container);
   });
 
-  after(function () {
+  afterEach(() => {
     container.remove();
   });
 
-  describe('TextQuoteAnchor', function () {
-    describe('#toRange', function () {
-      it('returns a valid DOM Range', function () {
+  describe('RangeAnchor', () => {
+    let fakeSniff;
+    let fakeSerializedRange;
+    let fakeNormalize;
+    let fakeSerialize;
+
+    beforeEach(() => {
+      fakeSerializedRange = sinon.stub();
+      fakeSerialize = sinon.stub();
+      fakeNormalize = sinon.stub().returns({
+        serialize: fakeSerialize,
+      });
+      fakeSniff = sinon.stub().returns({
+        normalize: fakeNormalize,
+      });
+      $imports.$mock({
+        './range': {
+          sniff: fakeSniff,
+          SerializedRange: fakeSerializedRange,
+        },
+      });
+    });
+
+    afterEach(() => {
+      $imports.$restore();
+    });
+
+    describe('#fromRange', () => {
+      it('returns a RangeAnchor instance', () => {
+        const anchor = RangeAnchor.fromRange(container, new Range());
+        assert.calledOnce(fakeNormalize);
+        assert.instanceOf(anchor, RangeAnchor);
+        assert.deepEqual(anchor.range, fakeNormalize());
+      });
+    });
+
+    describe('#fromSelector', () => {
+      it('returns a RangeAnchor instance', () => {
+        const anchor = RangeAnchor.fromSelector(container, {
+          type: 'RangeSelector',
+          startContainer: '/div[1]',
+          startOffset: 0,
+          endContainer: '/div[1]',
+          endOffset: 1,
+        });
+        assert.calledOnce(fakeSerializedRange);
+        assert.instanceOf(anchor, RangeAnchor);
+      });
+    });
+
+    describe('#toRange', () => {
+      it('returns a normalized range result', () => {
+        fakeNormalize.returns({
+          toRange: sinon.stub().returns('normalized range'),
+        });
+        const range = new RangeAnchor(container, new Range());
+        assert.equal(range.toRange(), 'normalized range');
+      });
+    });
+
+    describe('#toSelector', () => {
+      beforeEach(() => {
+        fakeSerialize.returns({
+          start: '/div[1]',
+          startOffset: 0,
+          end: '/div[1]',
+          endOffset: 1,
+        });
+      });
+
+      function rangeSelectorResult() {
+        return {
+          type: 'RangeSelector',
+          startContainer: '/div[1]',
+          startOffset: 0,
+          endContainer: '/div[1]',
+          endOffset: 1,
+        };
+      }
+
+      it('returns a RangeSelector', () => {
+        const range = new RangeAnchor(container, new Range());
+        assert.deepEqual(range.toSelector({}), rangeSelectorResult());
+      });
+
+      it('returns a RangeSelector if options are missing', () => {
+        const range = new RangeAnchor(container, new Range());
+        assert.deepEqual(range.toSelector(), rangeSelectorResult());
+      });
+    });
+  });
+
+  describe('TextPositionAnchor', () => {
+    let fakePosFromRange;
+    let fakePosToRange;
+
+    beforeEach(() => {
+      fakePosFromRange = sinon.stub();
+      fakePosToRange = sinon.stub();
+      $imports.$mock({
+        'dom-anchor-text-position': {
+          fromRange: fakePosFromRange,
+          toRange: fakePosToRange,
+        },
+      });
+    });
+
+    afterEach(() => {
+      $imports.$restore();
+    });
+
+    function createTextPositionAnchor() {
+      return new TextPositionAnchor(container, 0, 0);
+    }
+
+    describe('#fromRange', () => {
+      it('returns a TextPositionAnchor instance', () => {
+        fakePosFromRange.returns({
+          start: 0,
+          end: 1,
+        });
+        const anchor = TextPositionAnchor.fromRange(container, new Range());
+        assert.calledOnce(fakePosFromRange);
+        assert.instanceOf(anchor, TextPositionAnchor);
+      });
+    });
+
+    describe('#fromSelector', () => {
+      it('returns a TextPositionAnchor instance', () => {
+        const anchor = TextPositionAnchor.fromSelector(container, {
+          start: 0,
+          end: 0,
+        });
+        assert.instanceOf(anchor, TextPositionAnchor);
+      });
+    });
+
+    describe('#toSelector', () => {
+      it('returns a TextPositionSelector', () => {
+        const anchor = createTextPositionAnchor();
+        assert.deepEqual(anchor.toSelector(), {
+          type: 'TextPositionSelector',
+          start: 0,
+          end: 0,
+        });
+      });
+    });
+
+    describe('#toRange', () => {
+      it('returns a range object', () => {
+        const anchor = createTextPositionAnchor();
+        fakePosToRange.returns('fake range');
+        assert.equal(anchor.toRange(), 'fake range');
+        assert.calledWith(fakePosToRange, container, { start: 0, end: 0 });
+      });
+    });
+
+    describe('integration tests', () => {
+      beforeEach(() => {
+        // restore dom-anchor-text-position to test third party lib integration
+        $imports.$restore({
+          'dom-anchor-text-position': true,
+        });
+      });
+
+      it('can convert a Range to TextPositionSelector and back to a Range', () => {
+        const range = document.createRange();
+        range.setStart(container.firstChild, 0);
+        range.setEnd(container.firstChild, 4);
+        const anchor = TextPositionAnchor.fromRange(container, range);
+        assert.deepEqual(anchor.toSelector(), {
+          type: 'TextPositionSelector',
+          start: 0,
+          end: 4,
+        });
+        const newRange = anchor.toRange();
+        assert.deepEqual(newRange, range);
+        assert.equal(newRange.toString(), 'Four');
+      });
+    });
+  });
+
+  describe('TextQuoteAnchor', () => {
+    let fakeQuoteToRange;
+    let fakeQuoteFromRange;
+    let fakeToTextPosition;
+
+    beforeEach(() => {
+      fakeQuoteToRange = sinon.stub();
+      fakeQuoteFromRange = sinon.stub();
+      fakeToTextPosition = sinon.stub();
+      $imports.$mock({
+        'dom-anchor-text-quote': {
+          fromRange: fakeQuoteFromRange,
+          toRange: fakeQuoteToRange,
+          toTextPosition: fakeToTextPosition,
+        },
+      });
+    });
+
+    afterEach(() => {
+      $imports.$restore();
+    });
+
+    function createTextQuoteAnchor() {
+      return new TextQuoteAnchor(container, 'Liberty', {
+        prefix: 'a new nation, conceived in ',
+        suffix: ', and dedicated to the proposition that',
+      });
+    }
+
+    describe('#fromRange', () => {
+      it('returns a TextQuoteAnchor instance', () => {
+        fakeQuoteFromRange.returns({
+          prefix: 'Four score and ',
+          suffix: 'brought forth on this continent',
+        });
+        const anchor = TextQuoteAnchor.fromRange(container, new Range());
+        assert.called(fakeQuoteFromRange);
+        assert.instanceOf(anchor, TextQuoteAnchor);
+      });
+    });
+
+    describe('#fromSelector', () => {
+      it('returns a TextQuoteAnchor instance', () => {
+        const anchor = TextQuoteAnchor.fromSelector(container, {
+          type: 'TextQuoteSelector',
+          exact: 'Liberty',
+          prefix: 'a new nation, conceived in ',
+          suffix: ', and dedicated to the proposition that',
+        });
+        assert.instanceOf(anchor, TextQuoteAnchor);
+      });
+    });
+
+    describe('#toSelector', () => {
+      it('returns a TextQuoteSelector', () => {
+        const anchor = createTextQuoteAnchor();
+        assert.deepEqual(anchor.toSelector(), {
+          type: 'TextQuoteSelector',
+          exact: 'Liberty',
+          prefix: 'a new nation, conceived in ',
+          suffix: ', and dedicated to the proposition that',
+        });
+      });
+    });
+
+    describe('#toRange', () => {
+      it('returns a valid DOM Range', () => {
+        $imports.$restore({
+          'dom-anchor-text-quote': {
+            toRange: true,
+          },
+        });
         const quoteAnchor = new TextQuoteAnchor(container, 'Liberty');
         const range = quoteAnchor.toRange();
         assert.instanceOf(range, Range);
         assert.equal(range.toString(), 'Liberty');
       });
 
-      it('throws if the quote is not found', function () {
+      it('throws if the quote is not found', () => {
+        $imports.$restore({
+          'dom-anchor-text-quote': {
+            toRange: true,
+          },
+        });
         const quoteAnchor = new TextQuoteAnchor(
           container,
           'five score and nine years ago'
         );
-        assert.throws(function () {
+        assert.throws(() => {
           quoteAnchor.toRange();
         });
       });
     });
 
-    describe('#toPositionAnchor', function () {
-      it('returns a TextPositionAnchor', function () {
+    describe('#toPositionAnchor', () => {
+      it('returns a TextPositionAnchor instance', () => {
+        $imports.$restore({
+          'dom-anchor-text-quote': {
+            toTextPosition: true,
+          },
+        });
         const quoteAnchor = new TextQuoteAnchor(container, 'Liberty');
         const pos = quoteAnchor.toPositionAnchor();
         assert.instanceOf(pos, TextPositionAnchor);
       });
 
-      it('throws if the quote is not found', function () {
+      it('throws if the quote is not found', () => {
+        $imports.$restore({
+          'dom-anchor-text-quote': {
+            toTextPosition: true,
+          },
+        });
         const quoteAnchor = new TextQuoteAnchor(
           container,
           'some are more equal than others'
         );
-        assert.throws(function () {
+        assert.throws(() => {
           quoteAnchor.toPositionAnchor();
         });
+      });
+    });
+
+    describe('integration tests', () => {
+      beforeEach(() => {
+        // restore dom-anchor-text-quote to test third party lib integration
+        $imports.$restore({
+          'dom-anchor-text-quote': true,
+        });
+      });
+
+      it('can convert a Range to TextQuoteSelector and back to a Range', () => {
+        const range = document.createRange();
+        range.setStart(container.firstChild, 0);
+        range.setEnd(container.firstChild, 4);
+        const anchor = TextQuoteAnchor.fromRange(container, range);
+        assert.deepEqual(anchor.toSelector(), {
+          type: 'TextQuoteSelector',
+          prefix: '',
+          suffix: ' score and seven years ago our f',
+          exact: 'Four',
+        });
+        const newRange = anchor.toRange();
+        // startContainer of anchor.toRange() points to <div>
+        // rather than the text node "Four score ..."
+        // assert.equal(anchor.toRange(), range);
+        assert.equal(newRange.toString(), 'Four');
       });
     });
   });
---
+++ src/annotator/anchoring/types.js
@@ -0,0 +1,214 @@
+/**
+ * This module exports a set of classes for converting between DOM `Range`
+ * objects and different types of selectors. It is mostly a thin wrapper around a
+ * set of anchoring libraries. It serves two main purposes:
+ *
+ *  1. Providing a consistent interface across different types of anchors.
+ *  2. Insulating the rest of the code from API changes in the underlying anchoring
+ *     libraries.
+ */
+import {
+  fromRange as posFromRange,
+  toRange as posToRange,
+} from 'dom-anchor-text-position';
+import {
+  fromRange as quoteFromRange,
+  toRange as quoteToRange,
+  toTextPosition,
+} from 'dom-anchor-text-quote';
+
+import { SerializedRange, sniff } from './range';
+
+/**
+ * @typedef {import("./range").BrowserRange} BrowserRange}
+ * @typedef {import("./range").NormalizedRange} NormalizedRange}
+ * @typedef {Range|BrowserRange|NormalizedRange|SerializedRange} AnyRangeType
+ *
+ * @typedef {import('../../types/api').RangeSelector} RangeSelector
+ * @typedef {import('../../types/api').TextPositionSelector} TextPositionSelector
+ * @typedef {import('../../types/api').TextQuoteSelector} TextQuoteSelector
+ *
+ * @typedef TextContentNode
+ * @prop {string} textContent
+ */
+
+/**
+ * Converts between `RangeSelector` selectors and `Range` objects.
+ */
+export class RangeAnchor {
+  /**
+   * @param {Node} root - A root element from which to anchor.
+   * @param {AnyRangeType} range -  A range describing the anchor.
+   */
+  constructor(root, range) {
+    this.root = root;
+    this.range = sniff(range).normalize(this.root);
+  }
+
+  /**
+   * @param {Node} root -  A root element from which to anchor.
+   * @param {AnyRangeType} range -  A range describing the anchor.
+   */
+  static fromRange(root, range) {
+    return new RangeAnchor(root, range);
+  }
+
+  /**
+   * Create an anchor from a serialized `RangeSelector` selector.
+   *
+   * @param {Node} root -  A root element from which to anchor.
+   * @param {RangeSelector} selector
+   */
+  static fromSelector(root, selector) {
+    const data = {
+      start: selector.startContainer,
+      startOffset: selector.startOffset,
+      end: selector.endContainer,
+      endOffset: selector.endOffset,
+    };
+    const range = new SerializedRange(data);
+    return new RangeAnchor(root, range);
+  }
+
+  toRange() {
+    return this.range.toRange();
+  }
+
+  /**
+   * @param {Object} [options]
+   *   @param {string} [options.ignoreSelector]
+   * @return {RangeSelector}
+   */
+  toSelector(options = {}) {
+    const range = this.range.serialize(this.root, options.ignoreSelector);
+    return {
+      type: 'RangeSelector',
+      startContainer: range.start,
+      startOffset: range.startOffset,
+      endContainer: range.end,
+      endOffset: range.endOffset,
+    };
+  }
+}
+
+/**
+ * Converts between `TextPositionSelector` selectors and `Range` objects.
+ */
+export class TextPositionAnchor {
+  /**
+   * @param {Node|TextContentNode} root
+   * @param {number} start
+   * @param {number} end
+   */
+  constructor(root, start, end) {
+    this.root = root;
+    this.start = start;
+    this.end = end;
+  }
+
+  /**
+   * @param {Node} root
+   * @param {Range} range
+   */
+  static fromRange(root, range) {
+    const selector = posFromRange(root, range);
+    return TextPositionAnchor.fromSelector(root, selector);
+  }
+  /**
+   * @param {Node} root
+   * @param {TextPositionSelector} selector
+   */
+  static fromSelector(root, selector) {
+    return new TextPositionAnchor(root, selector.start, selector.end);
+  }
+
+  /**
+   * @return {TextPositionSelector}
+   */
+  toSelector() {
+    return {
+      type: 'TextPositionSelector',
+      start: this.start,
+      end: this.end,
+    };
+  }
+
+  toRange() {
+    return posToRange(this.root, { start: this.start, end: this.end });
+  }
+}
+
+/**
+ * Converts between TextQuoteSelector selectors and Range objects.
+ */
+export class TextQuoteAnchor {
+  /**
+   * @param {Node|TextContentNode} root - A root element from which to anchor.
+   * @param {string} exact
+   * @param {Object} context
+   *   @param {string} [context.prefix]
+   *   @param {string} [context.suffix]
+   */
+  constructor(root, exact, context = {}) {
+    this.root = root;
+    this.exact = exact;
+    this.context = context;
+  }
+  /**
+   * @param {Node} root
+   * @param {Range} range
+   */
+  static fromRange(root, range) {
+    const selector = quoteFromRange(root, range);
+    return TextQuoteAnchor.fromSelector(root, selector);
+  }
+
+  /**
+   * @param {Node|TextContentNode} root
+   * @param {TextQuoteSelector} selector
+   */
+  static fromSelector(root, selector) {
+    const { prefix, suffix } = selector;
+    return new TextQuoteAnchor(root, selector.exact, { prefix, suffix });
+  }
+
+  /**
+   * @return {TextQuoteSelector}
+   */
+  toSelector() {
+    return {
+      type: 'TextQuoteSelector',
+      exact: this.exact,
+      prefix: this.context.prefix,
+      suffix: this.context.suffix,
+    };
+  }
+
+  /**
+   * @param {Object} [options]
+   *   @param {number} [options.hint] -
+   *     Offset hint to disambiguate matches
+   *     https://github.com/tilgovi/dom-anchor-text-quote#totextpositionroot-selector-options
+   */
+  toRange(options = {}) {
+    const range = quoteToRange(this.root, this.toSelector(), options);
+    if (range === null) {
+      throw new Error('Quote not found');
+    }
+    return range;
+  }
+
+  /**
+   * @param {Object} [options]
+   *   @param {number} [options.hint] -
+   *     Offset hint to disambiguate matches
+   *     https://github.com/tilgovi/dom-anchor-text-quote#totextpositionroot-selector-options
+   */
+  toPositionAnchor(options = {}) {
+    const anchor = toTextPosition(this.root, this.toSelector(), options);
+    if (anchor === null) {
+      throw new Error('Quote not found');
+    }
+    return new TextPositionAnchor(this.root, anchor.start, anchor.end);
+  }
+}
