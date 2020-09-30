a06fa1a254161d0617cf0dcd2399804e25314cf0
Robert Knight <robertknight@gmail.com>
2020-09-30 16:51:57 +0100

    Fix range selector anchoring in XHTML documents
    In HTML documents that have been served with an XML mime type,
    `document.evaluate` handles element names differently. In HTML documents
    the XPath segments do not require prefixes on element names. In XHTML
    documents however they do. Since the client always generates the same
    un-prefixed XPaths regardless of document type, evaluation always
    failed. There was a fallback path but it was only executed if
    `document.evaluate` threw an error, not if it returned `null` as in this
    case.
    
    This commit resolves the issue by first attempting to evaluate the XPath
    using custom logic that only handles simple XPaths and then using
    `document.evaluate` only for more complex XPaths. The _simple XPath_
    logic behaves the same in HTML and XML documents and ignores namespaces.
    As a result it works with the XPaths that the client generates
    regardless of document type and also regardless of whether the XPath
    references only HTML elements or elements from other namespaces (eg.
    MathML or SVG).
    
    We could change the way that the client generates XPaths in future in
    XML documents or SVG/MathML content within HTML documents, but the
    client would still need to handle `RangeSelector` selectors on existing
    annotations.
    
    For more details, see https://github.com/hypothesis/client/pull/2590#issuecomment-702132563
    
    Fixes #2592

--- src/annotator/anchoring/test/xpath-test.js
+++ src/annotator/anchoring/test/xpath-test.js
@@ -12,52 +12,107 @@ describe('annotator/anchoring/xpath', () => {
             <li id="li-1">text 1</li>
             <li id="li-2">text 2</li>
             <li id="li-3">text 3</li>
+            <custom-element>text 4</custom-element>
           </ul>
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <msqrt><mrow><mi>x</mi><mo>+</mo><mn>1</mn></mrow></msqrt>
+          </math>
+          <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
+            <text x="20" y="35">Hello</text>
+            <text x="40" y="35">world</text>
+          </svg>
         </span>`;
 
     beforeEach(() => {
       container = document.createElement('div');
       container.innerHTML = html;
       document.body.appendChild(container);
+
+      sinon.spy(document, 'evaluate');
     });
 
     afterEach(() => {
+      document.evaluate.restore();
+
       container.remove();
     });
 
     [
+      // Single element path
       {
         xpath: '/h1[1]',
         nodeName: 'H1',
       },
+      // Multi-element path
       {
-        xpath: '/p[1]/a[1]/text()[1]',
-        nodeName: '#text',
+        xpath: '/p[1]/a[1]',
+        nodeName: 'A',
       },
       {
         xpath: '/span[1]/ul[1]/li[2]',
         nodeName: 'LI',
       },
+      // Upper-case element names
       {
         xpath: '/SPAN[1]/UL[1]/LI[2]',
         nodeName: 'LI',
       },
+      // Element path with implicit `[1]` index
       {
-        xpath: '/SPAN[1]/UL[1]/LI[2]/text()',
-        nodeName: '#text',
+        xpath: '/h1',
+        nodeName: 'H1',
+      },
+      // Custom element
+      {
+        xpath: '/span/ul/custom-element',
+        nodeName: 'CUSTOM-ELEMENT',
+      },
+      // Embedded MathML
+      {
+        xpath: '/span/math/msqrt/mrow/mi',
+        nodeName: 'mi',
+      },
+      {
+        xpath: '/SPAN/MATH/MSQRT/MROW/MI',
+        nodeName: 'mi',
+      },
+      // Embedded SVG
+      {
+        xpath: '/span[1]/svg[1]/text[2]',
+        nodeName: 'text',
       },
     ].forEach(test => {
-      it('it returns the node associated with the XPath', () => {
+      it('evaluates simple XPaths without using `document.evaluate`', () => {
         const result = nodeFromXPath(test.xpath, container);
-        assert.equal(result.nodeName, test.nodeName);
+        assert.notCalled(document.evaluate);
+        assert.equal(result?.nodeName, test.nodeName);
       });
+    });
 
-      it('it returns the node associated with the XPath when `document.evaluate` throws an error', () => {
-        const result = nodeFromXPath(test.xpath, container);
-        sinon.stub(document, 'evaluate').throws(new Error());
-        const resultFallback = nodeFromXPath(test.xpath, container);
-        assert.equal(result, resultFallback);
-        document.evaluate.restore();
+    ['/missing/element', '/span[0]'].forEach(xpath => {
+      it('returns `null` if simple XPath evaluation fails', () => {
+        const result = nodeFromXPath(xpath, container);
+        assert.notCalled(document.evaluate);
+        assert.strictEqual(result, null);
+      });
+    });
+
+    [
+      ['/*[local-name()="h1"]', 'H1'],
+      ['/span[-1]', null],
+    ].forEach(([xpath, expectedNodeName]) => {
+      it('uses `document.evaluate` for non-simple XPaths', () => {
+        const result = nodeFromXPath(xpath, container);
+        assert.calledOnce(document.evaluate);
+        assert.strictEqual(result?.nodeName ?? result, expectedNodeName);
+      });
+    });
+
+    ['not-a-valid-xpath'].forEach(xpath => {
+      it('throws if XPath is invalid', () => {
+        assert.throws(() => {
+          nodeFromXPath(xpath, container);
+        }, /The string '.*' is not a valid XPath expression/);
       });
     });
   });
