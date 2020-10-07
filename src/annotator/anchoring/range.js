hypothesis/client

commit a554058161cdf20e3f706388c37f4de90d5c0e17

Remove jQuery usage from range.js
As part of the removal of jQuery from the annotator, remove usage from
`range.js`:

 - Replace `parents()` method with a small helper function

 - Rewrite `getTextNodes` function to accept and return DOM nodes rather
   than jQuery collections

 - Use `Node.contains` instead of jQuery's `contains` method. Note that there
   is a semantic difference that `contains(nodeA, nodeA)` returns
   `false` whereas `nodeA.contains(nodeA)` returns `true`. This enabled
   simplifying a condition.

src/annotator/anchoring/range.js
@@ -1,11 +1,26 @@
-import $ from 'jquery';
-
 import { xpathFromNode, nodeFromXPath } from './xpath';
 import {
   getFirstTextNodeNotBefore,
   getLastTextNodeUpTo,
   getTextNodes,
 } from './xpath-util';
+
+/**
+ * Return ancestors of `element`, optionally filtered by a CSS `selector`.
+ *
+ * @param {Node} node
+ * @param {string} [selector]
+ */
+function parents(node, selector) {
+  const parents = [];
+  while (node.parentElement) {
+    if (!selector || node.parentElement.matches(selector)) {
+      parents.push(node.parentElement);
+    }
+    node = node.parentElement;
+  }
+  return parents;
+}
 
 /**
  * Creates a wrapper around a range object obtained from a DOMSelection.
@@ -209,9 +224,8 @@ export class NormalizedRange {
    * Returns updated self or null.
    */
   limit(bounds) {
-    const nodes = $.grep(
-      this.textNodes(),
-      node => node.parentNode === bounds || $.contains(bounds, node.parentNode)
+    const nodes = this.textNodes().filter(node =>
+      bounds.contains(node.parentNode)
     );
     if (!nodes.length) {
       return null;
@@ -220,10 +234,10 @@ export class NormalizedRange {
     this.start = nodes[0];
     this.end = nodes[nodes.length - 1];
 
-    const startParents = $(this.start).parents();
+    const startParents = parents(this.start);
 
-    for (let parent of $(this.end).parents()) {
-      if (startParents.index(parent) !== -1) {
+    for (let parent of parents(this.end)) {
+      if (startParents.indexOf(parent) !== -1) {
         this.commonAncestor = parent;
         break;
       }
@@ -245,19 +259,19 @@ export class NormalizedRange {
     const serialization = (node, isEnd) => {
       let origParent;
       if (ignoreSelector) {
-        origParent = $(node).parents(`:not(${ignoreSelector})`).eq(0);
+        origParent = parents(node, `:not(${ignoreSelector})`)[0];
       } else {
-        origParent = $(node).parent();
+        origParent = node.parentElement;
       }
-      const xpath = xpathFromNode(origParent[0], root ?? document);
+      const xpath = xpathFromNode(origParent, root ?? document);
       const textNodes = getTextNodes(origParent);
       // Calculate real offset as the combined length of all the
       // preceding textNode siblings. We include the length of the
       // node if it's the end node.
-      const nodes = textNodes.slice(0, textNodes.index(node));
+      const nodes = textNodes.slice(0, textNodes.indexOf(node));
       let offset = 0;
       for (let n of nodes) {
-        offset += n.nodeValue.length;
+        offset += n.nodeValue?.length ?? 0;
       }
 
       if (isEnd) {
@@ -300,11 +314,11 @@ export class NormalizedRange {
    * Returns an Array of TextNode instances.
    */
   textNodes() {
-    const textNodes = getTextNodes($(this.commonAncestor));
-    const start = textNodes.index(this.start);
-    const end = textNodes.index(this.end);
+    const textNodes = getTextNodes(this.commonAncestor);
+    const start = textNodes.indexOf(this.start);
+    const end = textNodes.indexOf(this.end);
     // Return the textNodes that fall between the start and end indexes.
-    return $.makeArray(textNodes.slice(start, +end + 1 || undefined));
+    return textNodes.slice(start, +end + 1 || undefined);
   }
 
   /**
@@ -382,7 +396,10 @@ export class SerializedRange {
         targetOffset--;
       }
 
-      for (let tn of getTextNodes($(node))) {
+      for (let tn of getTextNodes(node)) {
+        if (tn.nodeValue === null) {
+          continue;
+        }
         if (length + tn.nodeValue.length > targetOffset) {
           range[p + 'Container'] = tn;
           range[p + 'Offset'] = this[p + 'Offset'] - length;
@@ -423,17 +440,15 @@ export class SerializedRange {
     // Node.compareDocumentPosition() to decide when to set the
     // commonAncestorContainer and bail out.
 
-    const contains = (a, b) => a.compareDocumentPosition(b) & 16;
-    $(range.startContainer)
-      .parents()
-      .each(function () {
-        if (contains(this, range.endContainer)) {
-          range.commonAncestorContainer = this;
-          // bail out of loop
-          return false;
-        }
-        return true;
-      });
+    const contains = (a, b) =>
+      a.compareDocumentPosition(b) & Node.DOCUMENT_POSITION_CONTAINED_BY;
+
+    for (let parent of parents(range.startContainer)) {
+      if (contains(parent, range.endContainer)) {
+        range.commonAncestorContainer = parent;
+        break;
+      }
+    }
 
     return new BrowserRange(range).normalize();
   }

src/annotator/anchoring/test/xpath-util-test.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 import {
   findChild,
   getTextNodes,
@@ -61,8 +59,7 @@ describe('annotator/anchoring/xpath-util', () => {
   describe('getTextNodes', () => {
     let container;
 
-    // Helper (expects a jquery array)
-    const nodeValues = nodes => nodes.map((i, n) => n.nodeValue);
+    const nodeValues = nodes => nodes.map(n => n.nodeValue);
 
     beforeEach(() => {
       container = document.createElement('div');
@@ -75,26 +72,22 @@ describe('annotator/anchoring/xpath-util', () => {
 
     it('finds basic text nodes', () => {
       container.innerHTML = `<span>text 1</span><span>text 2</span>`;
-      const result = getTextNodes($(container));
-      assert.deepEqual(nodeValues(result).toArray(), ['text 1', 'text 2']);
+      const result = getTextNodes(container);
+      assert.deepEqual(nodeValues(result), ['text 1', 'text 2']);
     });
 
     it('finds basic text nodes and whitespace', () => {
       container.innerHTML = `<span>text 1</span>
         <span>text 2</span>`;
-      const result = getTextNodes($(container));
-      assert.deepEqual(nodeValues(result).toArray(), [
-        'text 1',
-        '\n        ',
-        'text 2',
-      ]);
+      const result = getTextNodes(container);
+      assert.deepEqual(nodeValues(result), ['text 1', '\n        ', 'text 2']);
     });
 
     it('finds basic text nodes and whitespace but ignores comments', () => {
       container.innerHTML = `<span>text 1</span>
         <!--span>text 2</span-->`;
-      const result = getTextNodes($(container));
-      assert.deepEqual(nodeValues(result).toArray(), ['text 1', '\n        ']);
+      const result = getTextNodes(container);
+      assert.deepEqual(nodeValues(result), ['text 1', '\n        ']);
     });
   });
 

src/annotator/anchoring/xpath-util.js
@@ -85,45 +85,21 @@ export function xpathFromNode(node, root) {
 }
 
 /**
- * Flatten a nested array structure.
- * TODO: use Array.prototype.flat and polyfill
+ * Return all text node descendants of `element`.
+ *
+ * @param {Element} element
+ * @return {Text[]}
  */
-function flatten(array) {
-  const flatten = ary => {
-    let flat = [];
-    ary.forEach(el => {
-      if (el && Array.isArray(el)) {
-        flat = flat.concat(flatten(el));
-      } else {
-        flat = flat.concat(el);
-      }
-    });
-    return flat;
-  };
-  return flatten(array);
-}
-
-/**
- * Finds all text nodes within the elements in the current collection.
- * Returns a new jQuery collection of text nodes.
- */
-export function getTextNodes(jq) {
-  const getTextNodes = node => {
-    if (node && node.nodeType !== Node.TEXT_NODE) {
-      const nodes = [];
-      if (node.nodeType !== Node.COMMENT_NODE) {
-        [...node.childNodes].forEach(child => {
-          nodes.push(getTextNodes(child));
-        });
-      }
-      return nodes;
-    } else {
-      return node;
+export function getTextNodes(element) {
+  const nodes = [];
+  for (let node of Array.from(element.childNodes)) {
+    if (node instanceof Text) {
+      nodes.push(node);
+    } else if (node instanceof Element) {
+      nodes.push(...getTextNodes(node));
     }
-  };
-  return jq.map((index, node) => {
-    return flatten(getTextNodes(node));
-  });
+  }
+  return nodes;
 }
 
 /**

commit babbb4011a88751974cf4579505289d8bc37241f

Remove logic to ignore Hypothesis UI when generating range selectors
Remove the logic that was intended to ignore Hypothesis UI when
generating range selectors.

We don't need this any more because all Hypothesis UI elements in the
host page which contain text have `user-select` set to prevent text
selection. This affects the bucket bar, vertical toolbar and adder.

In addition, the logic did not serve its intended purpose as it only applied to
range selector generation. If the user did somehow succeed in selecting text in
the bucket bar for example the client would still generate quote and position
selectors that referred to that content. What the client would need to
do instead is modify the range from which the annotation's selectors
were generated.

As a side effect this fixes a regression introduced in a554058 which caused
range selector generation to fail in Chrome and Firefox due to use of
`:not(<inner selector>)` selector where `<inner selector>` is a
list of selectors (the `IGNORE_SELECTOR` value from `guest.js`).

src/annotator/anchoring/range.js
@@ -6,17 +6,14 @@ import {
 } from './xpath-util';
 
 /**
- * Return ancestors of `element`, optionally filtered by a CSS `selector`.
+ * Return ancestors of `node`.
  *
  * @param {Node} node
- * @param {string} [selector]
  */
-function parents(node, selector) {
+function parents(node) {
   const parents = [];
   while (node.parentElement) {
-    if (!selector || node.parentElement.matches(selector)) {
-      parents.push(node.parentElement);
-    }
+    parents.push(node.parentElement);
     node = node.parentElement;
   }
   return parents;
@@ -169,13 +166,11 @@ export class BrowserRange {
    * Creates a range suitable for storage.
    *
    * root           - A root Element from which to anchor the serialization.
-   * ignoreSelector - A selector String of elements to ignore. For example
-   *                  elements injected by the annotator.
    *
    * Returns an instance of SerializedRange.
    */
-  serialize(root, ignoreSelector) {
-    return this.normalize().serialize(root, ignoreSelector);
+  serialize(root) {
+    return this.normalize().serialize(root);
   }
 }
 
@@ -250,19 +245,12 @@ export class NormalizedRange {
    * character offset), which can be easily stored in a database.
    *
    * root -           The root Element relative to which XPaths should be calculated
-   * ignoreSelector - A selector String of elements to ignore. For example
-   *                  elements injected by the annotator.
    *
    * Returns an instance of SerializedRange.
    */
-  serialize(root, ignoreSelector) {
+  serialize(root) {
     const serialization = (node, isEnd) => {
-      let origParent;
-      if (ignoreSelector) {
-        origParent = parents(node, `:not(${ignoreSelector})`)[0];
-      } else {
-        origParent = node.parentElement;
-      }
+      const origParent = node.parentElement;
       const xpath = xpathFromNode(origParent, root ?? document);
       const textNodes = getTextNodes(origParent);
       // Calculate real offset as the combined length of all the
@@ -433,13 +421,11 @@ export class SerializedRange {
    * Creates a range suitable for storage.
    *
    * root           - A root Element from which to anchor the serialization.
-   * ignoreSelector - A selector String of elements to ignore. For example
-   *                  elements injected by the annotator.
    *
    * Returns an instance of SerializedRange.
    */
-  serialize(root, ignoreSelector) {
-    return this.normalize(root).serialize(root, ignoreSelector);
+  serialize(root) {
+    return this.normalize(root).serialize(root);
   }
 
   // Returns the range as an Object literal.

src/annotator/anchoring/types.js
@@ -75,12 +75,10 @@ export class RangeAnchor {
   }
 
   /**
-   * @param {Object} [options]
-   *   @param {string} [options.ignoreSelector]
    * @return {RangeSelector}
    */
-  toSelector(options = {}) {
-    const range = this.range.serialize(this.root, options.ignoreSelector);
+  toSelector() {
+    const range = this.range.serialize(this.root);
     return {
       type: 'RangeSelector',
       startContainer: range.start,

src/annotator/guest.js
@@ -63,13 +63,6 @@ function annotationsAt(node) {
   return /** @type {AnnotationData[]} */ (items);
 }
 
-// A selector which matches elements added to the DOM by Hypothesis (eg. for
-// highlights and annotation UI).
-//
-// We can simplify this once all classes are converted from an "annotator-"
-// prefix to a "hypothesis-" prefix.
-const IGNORE_SELECTOR = '[class^="annotator-"],[class^="hypothesis-"]';
-
 export default class Guest extends Delegator {
   constructor(element, config, anchoring = htmlAnchoring) {
     const defaultConfig = {
@@ -355,11 +348,8 @@ export default class Guest extends Delegator {
       }
 
       // Find a target using the anchoring module.
-      const options = {
-        ignoreSelector: IGNORE_SELECTOR,
-      };
       return this.anchoring
-        .anchor(root, target.selector, options)
+        .anchor(root, target.selector)
         .then(range => ({
           annotation,
           target,
@@ -477,11 +467,8 @@ export default class Guest extends Delegator {
     this.selectedRanges = null;
 
     const getSelectors = range => {
-      const options = {
-        ignoreSelector: IGNORE_SELECTOR,
-      };
       // Returns an array of selectors for the passed range.
-      return this.anchoring.describe(root, range, options);
+      return this.anchoring.describe(root, range);
     };
 
     const setDocumentInfo = info => {

src/annotator/anchoring/test/range-test.js
@@ -271,17 +271,6 @@ describe('annotator/anchoring/range', () => {
           endOffset: 1,
         });
       });
-
-      it('converts BrowserRange to SerializedRange instance with `ignoreSelector` condition', () => {
-        const browserRange = createBrowserRange();
-        const result = browserRange.serialize(container, 'p');
-        assert.deepEqual(result, {
-          start: '/section[1]', // /p[1] selector in xpath ignored
-          startOffset: 0,
-          end: '/section[1]/span[1]', // /p[1] selector in xpath ignored
-          endOffset: 1,
-        });
-      });
     });
   });
 
@@ -373,19 +362,6 @@ describe('annotator/anchoring/range', () => {
         assert.deepEqual(serializedRange, {
           start: '/html[1]/body[1]/div[1]/section[1]/p[1]',
           end: '/html[1]/body[1]/div[1]/section[1]/span[1]/p[1]',
-          startOffset: 0,
-          endOffset: 6,
-        });
-      });
-
-      it('serialize the range with `ignoreSelector` condition', () => {
-        const serializedRange = createNormalizedRange().serialize(
-          container,
-          '#p-3'
-        );
-        assert.deepEqual(serializedRange, {
-          start: '/section[1]/p[1]',
-          end: '/section[1]/span[1]',
           startOffset: 0,
           endOffset: 6,
         });
@@ -510,15 +486,6 @@ describe('annotator/anchoring/range', () => {
         assert.isTrue(result instanceof SerializedRange);
         // The copied instance shall be identical to the initial.
         assert.deepEqual(result, serializedRange);
-      });
-
-      it('converts a SerializedRange to a new SerializedRange instance with `ignoreSelector` condition', () => {
-        const serializedRange = createSerializedRange();
-        const result = serializedRange.serialize(container, '#p-3');
-        // End xpath shall ignore the provided selector and not
-        // be identical to the initial end xpath.
-        assert.notEqual(serializedRange.end, result.end);
-        assert.equal(result.end, '/section[1]/span[1]');
       });
     });
   });