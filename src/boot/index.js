e1f314b3cb7997289901ca059844c39639ac00ff
Robert Knight <robertknight@gmail.com>
2020-10-10 09:07:20 +0100

    Gracefully abort startup in old browsers
    Run a series of feature tests at the very start of the boot process and
    abort with a console warning if they fail.
    
    We display a console warning rather than a louder warning to avoid being
    disruptive on sites maintained by publishers which embed Hypothesis and
    which still want to support older browsers. On those sites the
    Hypothesis annotation feature will simply be missing.

--- src/boot/index.js
+++ src/boot/index.js
@@ -13,14 +13,24 @@ import { jsonConfigsFrom } from '../shared/settings';
 
 import boot from './boot';
 import processUrlTemplate from './url-template';
+import { isBrowserSupported } from './browser-check';
 
-const settings = jsonConfigsFrom(document);
-
-boot(document, {
-  assetRoot: processUrlTemplate(settings.assetRoot || '__ASSET_ROOT__'),
-  // @ts-ignore - `__MANIFEST__` is injected by the build script
-  manifest: __MANIFEST__,
-  sidebarAppUrl: processUrlTemplate(
-    settings.sidebarAppUrl || '__SIDEBAR_APP_URL__'
-  ),
-});
+if (isBrowserSupported()) {
+  const settings = jsonConfigsFrom(document);
+  boot(document, {
+    assetRoot: processUrlTemplate(settings.assetRoot || '__ASSET_ROOT__'),
+    // @ts-ignore - `__MANIFEST__` is injected by the build script
+    manifest: __MANIFEST__,
+    sidebarAppUrl: processUrlTemplate(
+      settings.sidebarAppUrl || '__SIDEBAR_APP_URL__'
+    ),
+  });
+} else {
+  // Show a "quiet" warning to avoid being disruptive on non-Hypothesis sites
+  // that embed the client.
+  //
+  // In Via or when using the bookmarklet we could show something louder.
+  console.warn(
+    'The Hypothesis annotation tool is not supported in this browser. See https://web.hypothes.is/help/which-browsers-are-supported-by-hypothesis/.'
+  );
+}
