5445c2a347cd57b763853981e0917a04d91ae41e
Robert Knight <robertknight@gmail.com>
2020-11-09 08:22:11 +0000

    Add `TextPosition` and `TextRange` classes
    Add a `TextRange` class which represents a region of the document as an
    immutable start/end pair of (element, text position) points. Compared to
    a DOM `Range`, this representation of a document region is more robust to
    changes in the DOM structure of the region that don't affect the text
    content, such as wrapping parts of the range in elements to insert
    accessible highlights.

---
+++ src/annotator/anchoring/text-range.js
@@ -0,0 +1,169 @@
+/**
+ * Return the total length of the text of all previous siblings of `node`.
+ *
+ * @param {Node} node
+ */
+function previousSiblingTextLength(node) {
+  let sibling = node.previousSibling;
+  let length = 0;
+  while (sibling) {
+    length += sibling.textContent?.length ?? 0;
+    sibling = sibling.previousSibling;
+  }
+  return length;
+}
+
+/**
+ * Represents an offset within the text content of an element.
+ *
+ * This position can be resolved to a specific descendant node in the current
+ * DOM subtree of the element using the `resolve` method.
+ */
+export class TextPosition {
+  /**
+   * Construct a `TextPosition` that refers to the text position `offset` within
+   * the text content of `element`.
+   *
+   * @param {Element} element
+   * @param {number} offset
+   */
+  constructor(element, offset) {
+    if (offset < 0) {
+      throw new Error('Offset is invalid');
+    }
+
+    this.element = element;
+
+    /** Character offset from the start of the element's `textContent`. */
+    this.offset = offset;
+  }
+
+  /**
+   * Resolve the position to a specific text node and offset within that node.
+   *
+   * Throws if `this.offset` exceeds the length of the element's text or if
+   * the element has no text. Offsets at the boundary between two nodes are
+   * resolved to the start of the node that begins at the boundary.
+   *
+   * @return {{ node: Text, offset: number }}
+   * @throws {RangeError}
+   */
+  resolve() {
+    const root = this.element;
+    const nodeIter = /** @type {Document} */ (root.ownerDocument).createNodeIterator(
+      root,
+      NodeFilter.SHOW_TEXT
+    );
+
+    let currentNode;
+    let textNode;
+    let length = 0;
+
+    while ((currentNode = nodeIter.nextNode())) {
+      textNode = /** @type {Text} */ (currentNode);
+      if (length + textNode.data.length > this.offset) {
+        return { node: textNode, offset: this.offset - length };
+      }
+      length += textNode.data.length;
+    }
+
+    if (textNode && length === this.offset) {
+      return { node: textNode, offset: textNode.data.length };
+    }
+
+    throw new RangeError('Offset exceeds text length');
+  }
+
+  /**
+   * Construct a `TextPosition` representing the range start or end point (node, offset).
+   *
+   * @param {Node} node
+   * @param {number} offset
+   * @return {TextPosition}
+   */
+  static fromPoint(node, offset) {
+    switch (node.nodeType) {
+      case Node.TEXT_NODE: {
+        if (offset < 0 || offset > /** @type {Text} */ (node).data.length) {
+          throw new Error('Text node offset is out of range');
+        }
+
+        if (!node.parentElement) {
+          throw new Error('Text node has no parent');
+        }
+
+        // Get the offset to the start of the parent element.
+        const textOffset = previousSiblingTextLength(node) + offset;
+
+        return new TextPosition(node.parentElement, textOffset);
+      }
+      case Node.ELEMENT_NODE: {
+        if (offset < 0 || offset > node.childNodes.length) {
+          throw new Error('Child node offset is out of range');
+        }
+
+        // Get the text length before the `offset`th child of element.
+        let textOffset = 0;
+        for (let i = 0; i < offset; i++) {
+          textOffset += node.childNodes[i].textContent?.length ?? 0;
+        }
+
+        return new TextPosition(/** @type {Element} */ (node), textOffset);
+      }
+      default:
+        throw new Error('Point is not in an element or text node');
+    }
+  }
+}
+
+/**
+ * Represents a region of a document as a (start, end) pair of `TextPosition` points.
+ *
+ * Representing a range in this way allows for changes in the DOM content of the
+ * range which don't affect its text content, without affecting the text content
+ * of the range itself.
+ */
+export class TextRange {
+  /**
+   * Construct an immutable `TextRange` from a `start` and `end` point.
+   *
+   * @param {TextPosition} start
+   * @param {TextPosition} end
+   */
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  /**
+   * Resolve the `TextRange` to a DOM range.
+   *
+   * May throw if the `start` or `end` positions cannot be resolved to a range.
+   *
+   * @return {Range}
+   */
+  toRange() {
+    const { node: startNode, offset: startOffset } = this.start.resolve();
+    const { node: endNode, offset: endOffset } = this.end.resolve();
+
+    const range = new Range();
+    range.setStart(startNode, startOffset);
+    range.setEnd(endNode, endOffset);
+    return range;
+  }
+
+  /**
+   * Convert an exiting DOM `Range` to a `TextRange`
+   *
+   * @param {Range} range
+   * @return {TextRange}
+   */
+  static fromRange(range) {
+    const start = TextPosition.fromPoint(
+      range.startContainer,
+      range.startOffset
+    );
+    const end = TextPosition.fromPoint(range.endContainer, range.endOffset);
+    return new TextRange(start, end);
+  }
+}
