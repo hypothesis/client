564a3aa41460d42d1049847d9b8fe63ae83beb20
Robert Knight <robertknight@gmail.com>
2020-11-20 16:18:43 +0000

    Rewrite a comment
    Try to explain the issue with `textContent` in a better way.

--- src/annotator/anchoring/text-range.js
+++ src/annotator/anchoring/text-range.js
@@ -1,15 +1,15 @@
 /**
  * Return the combined length of text nodes contained in `node`.
  *
- * This is different than `node.textContent` if called on a comment or processing
- * instruction directly.
- *
  * @param {Node} node
  */
 function nodeTextLength(node) {
   switch (node.nodeType) {
     case Node.ELEMENT_NODE:
     case Node.TEXT_NODE:
+      // nb. `textContent` excludes text in comments and processing instructions
+      // when called on a parent element, so we don't need to subtract that here.
+
       return /** @type {string} */ (node.textContent).length;
     default:
       return 0;
