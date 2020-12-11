35f83f39f86d46769a1faabdbdd0ca3d565be1e0
Lyza Danger Gardner <lyza@lyza.com>
2020-12-11 09:53:48 -0500

    Restructure filters store for extensibility and clarity
    Restructure the filters store module with the following goals in mind:
    
    * Simplify the structure of user-focus mode
    * Make the filtering structure for user-focus filters consistent with
      other filters
    * Add some actions and selectors for setting and getting filters,
      clearing the way to start adding some additional filtering
    * Be explicit about filter precedence
    
    Only one change here that affects the store's API and that is adjusted
    for in `useRootThread`.

--- src/sidebar/components/hooks/use-root-thread.js
+++ src/sidebar/components/hooks/use-root-thread.js
@@ -17,22 +17,16 @@ export default function useRootThread() {
   const query = store.filterQuery();
   const route = store.route();
   const selectionState = store.selectionState();
-  const userFilter = store.userFilter();
+  const filters = store.getFilterValues();
 
   const threadState = useMemo(() => {
     /** @type {Object.<string,string>} */
-    const filters = {};
-    if (userFilter) {
-      filters.user = userFilter;
-    }
     return {
       annotations,
-      filters,
-      query,
       route,
       selection: { ...selectionState, filterQuery: query, filters },
     };
-  }, [annotations, query, route, selectionState, userFilter]);
+  }, [annotations, query, route, selectionState, filters]);
 
   return threadAnnotations(threadState);
 }
