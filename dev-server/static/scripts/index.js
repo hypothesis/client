d09c67946153f54ee010e7780c8e5307dbe72e00
Eduardo Sanz García <esanzgar@gmail.com>
2021-01-19 17:37:34 +0100

    Fix JS browser compatibility on dev
    Because the code in `dev-server/static/scripts/util.js` is not
    transpiled by Babel, it needs to be compatible with all the browsers
    versions in `browserslist` (`package.json`).
    
    Optional chaining `(?.)` is for example not support in Chrome versions <
    80.
    
    This is only an issue when testing `localhost:3000` on these supported,
    but older browser versions.

--- dev-server/static/scripts/index.js
+++ dev-server/static/scripts/index.js
@@ -1,18 +1,25 @@
+ /**
+  * Because the code in this file is not transpiled by Babel, it must be compatible
+  * with all the supported browsers version (see `browserlist` in `package.json`)
+  * without transpilation. Do not include latest EcmaScript features as these
+  * will cause exceptions while working on dev (`localhost:3000`) on slightly
+  * older, yet supported browser versions.
+  */
+ 
 // Code for controls on the dev server homepage.
 
-import { activeClientUrl, loadClient, unloadClient } from './util.js';
+(function () {
+  let toggleClientButton = document.querySelector('.js-toggle-client');
+  toggleClientButton.textContent = 'Unload client';
+  let clientUrl = activeClientUrl;
 
-const toggleClientButton = document.querySelector('.js-toggle-client');
-toggleClientButton.textContent = 'Unload client';
-const clientUrl = activeClientUrl;
-
-toggleClientButton.onclick = () => {
-  if (activeClientUrl) {
-    unloadClient();
-    toggleClientButton.textContent = 'Load client';
-  } else {
-    loadClient(clientUrl);
-    toggleClientButton.textContent = 'Unload client';
-  }
-};
-
+  toggleClientButton.onclick = () => {
+    if (activeClientUrl) {
+      unloadClient();
+      toggleClientButton.textContent = 'Load client';
+    } else {
+      loadClient(clientUrl);
+      toggleClientButton.textContent = 'Unload client';
+    }
+  };
+})();
