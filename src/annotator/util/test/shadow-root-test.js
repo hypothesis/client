4a979326d8cb85bc3a309c2a34e8e590ab7f07c2
Eduardo Sanz García <esanzgar@gmail.com>
2021-02-05 16:54:31 +0100

    Apply applyFocusVisiblePolyfill to shadow DOM elements
    Follow instructions from:
    https://github.com/WICG/focus-visible#shadow-dom

--- src/annotator/util/test/shadow-root-test.js
+++ src/annotator/util/test/shadow-root-test.js
@@ -1,10 +1,21 @@
 import { createShadowRoot } from '../shadow-root';
 
 describe('annotator/util/shadow-root', () => {
+  let applyFocusVisiblePolyfill;
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    applyFocusVisiblePolyfill = window.applyFocusVisiblePolyfill;
+    window.applyFocusVisiblePolyfill = sinon.stub();
+  });
+
+  afterEach(() => {
+    container.remove();
+    window.applyFocusVisiblePolyfill = applyFocusVisiblePolyfill;
+  });
   describe('createShadowRoot', () => {
     it('attaches a shadow root to the container', () => {
-      const container = document.createElement('div');
-
       const shadowRoot = createShadowRoot(container);
 
       assert.ok(shadowRoot);
@@ -12,17 +23,13 @@ describe('annotator/util/shadow-root', () => {
     });
 
     it('does not attach a shadow root if Shadow DOM is unavailable', () => {
-      const container = document.createElement('div');
       container.attachShadow = null;
-
       const shadowRoot = createShadowRoot(container);
 
       assert.equal(shadowRoot, container);
     });
 
     it('injects stylesheets into the shadow root', () => {
-      const container = document.createElement('div');
-
       createShadowRoot(container);
 
       const styleEl = container.shadowRoot.querySelector('style');
@@ -30,9 +37,13 @@ describe('annotator/util/shadow-root', () => {
       assert.match(styleEl.textContent, /@import ".*annotator\.css.*"/);
     });
 
-    it('does not inject stylesheets into the shadow root if style is not found', () => {
-      const container = document.createElement('div');
+    it('applies the applyFocusVisiblePolyfill if exists', () => {
+      const shadowRoot = createShadowRoot(container);
 
+      assert.calledWith(window.applyFocusVisiblePolyfill, shadowRoot);
+    });
+
+    it('does not inject stylesheets into the shadow root if style is not found', () => {
       const link = document.querySelector(
         'link[rel="stylesheet"][href*="/build/styles/annotator.css"]'
       );
