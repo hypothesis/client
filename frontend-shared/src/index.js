e3467f5d3c0f28d888fa55f5cf4428c3551c2491
Kyle Keating <kkeating@hypothes.is>
2021-03-01 14:47:07 -0800

    Remove frontend-share subfolder
    - Remove github actions related to frontend-shared
    - Remove scripts and gulp commands related to frontend-shared
    - frontend-shared is now managed from its own repository.

--- frontend-shared/src/index.js
+++
@@ -1,8 +0,0 @@
-// Components
-export { SvgIcon, registerIcons } from './components/SvgIcon';
-
-// Hooks
-export { useElementShouldClose } from './hooks/use-element-should-close';
-
-// Utilities
-export { normalizeKeyName } from './browser-compatibility-utils';
