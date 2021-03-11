1b71a6525a2b9b442667fb1bcc672331e262a501
Eduardo Sanz García <esanzgar@gmail.com>
2021-03-11 13:52:05 +0100

    Convert Notebook into a react component
    Notebook class creates the container element and instantiates the preact
    component NotebookModal.
    
    NotebookModal listens to the openNotebook event sent through the
    eventBus and hides and shows the modal accordingly.
    
    The creation of the iframe is a costly operation: it involves several
    backend calls and the rendering of it in the DOM. Therefore, we delay
    the creation of the iframe element until the NotebookModal listens for
    the `openNotebook` event.
    
    The useEffect's cleanup of the NotebookModal componente has significant
    effects because it resets the overflow CSS property of document.body.
    
    As pointed by @robertknight, `render(null, container)` destroys the
    component triggering the useEffect's cleanup function.

--- src/annotator/notebook.js
+++ src/annotator/notebook.js
@@ -1,34 +1,6 @@
-import { createSidebarConfig } from './config/sidebar';
 import { createShadowRoot } from './util/shadow-root';
 import { render } from 'preact';
-
-// FIXME: use the button from the frontend shared package once this is stable.
-import Button from '../sidebar/components/Button';
-
-/**
- * Create the iframe that will load the notebook application.
- *
- * @return {HTMLIFrameElement}
- */
-function createNotebookFrame(config, groupId) {
-  const notebookConfig = createSidebarConfig(config);
-  // Explicity set the "focused" group
-  notebookConfig.group = groupId;
-  const configParam =
-    'config=' + encodeURIComponent(JSON.stringify(notebookConfig));
-  const notebookAppSrc = config.notebookAppUrl + '#' + configParam;
-
-  const notebookFrame = document.createElement('iframe');
-
-  // Enable media in annotations to be shown fullscreen
-  notebookFrame.setAttribute('allowfullscreen', '');
-
-  notebookFrame.src = notebookAppSrc;
-  notebookFrame.title = 'Hypothesis annotation notebook';
-  notebookFrame.className = 'notebook-inner';
-
-  return notebookFrame;
-}
+import NotebookModal from './components/NotebookModal';
 
 export default class Notebook {
   /**
@@ -38,110 +10,22 @@ export default class Notebook {
    * @param {Record<string, any>} config
    */
   constructor(element, eventBus, config = {}) {
-    this.element = element;
-    this._emitter = eventBus.createEmitter();
-    this.options = config;
-    this.frame = null;
-
-    /** @type {null|string} */
-    this._groupId = null;
-    /** @type {null|string} */
-    this._prevGroupId = null;
-
     /**
      * Un-styled shadow host for the notebook content.
-     *
      * This isolates the notebook from the page's styles.
      */
     this._outerContainer = document.createElement('hypothesis-notebook');
-    this.element.appendChild(this._outerContainer);
+    element.appendChild(this._outerContainer);
+    this.shadowRoot = createShadowRoot(this._outerContainer);
 
-    /**
-     * Lazily-initialized container for the notebook iframe. This is only created
-     * when the notebook is actually used.
-     *
-     * @type {HTMLElement|null}
-     */
-    this.container = null;
-
-    this._emitter.subscribe('openNotebook', groupId => {
-      this._groupId = groupId;
-      this.open();
-    });
-  }
-
-  _update() {
-    const container = this._initContainer();
-
-    // Create a new iFrame if we don't have one at all yet, or if the
-    // groupId has changed since last use
-    const needIframe =
-      !this.frame || !this._prevGroupId || this._prevGroupId !== this._groupId;
-    this._prevGroupId = this._groupId;
-
-    if (needIframe) {
-      this.frame?.remove();
-      this.frame = createNotebookFrame(this.options, this._groupId);
-      container.appendChild(this.frame);
-    }
-  }
-
-  open() {
-    const container = this._initContainer();
-    this._update();
-    container.classList.add('is-open');
-    container.style.display = '';
-
-    // The overflow CSS property is set to hidden to prevent scrolling of the guest page,
-    // while the notebook is shown as modal. It is restored on the close method.
-    // I believe this hack only works if this.element points to document.body of the guest page.
-    this.originalOverflowStyle = this.element.style.overflow;
-    this.element.style.overflow = 'hidden';
-  }
-
-  close() {
-    if (this.container) {
-      this.container.classList.remove('is-open');
-      this.container.style.display = 'none';
-    }
-    this.element.style.overflow = /** @type {string} */ (this
-      .originalOverflowStyle);
+    render(
+      <NotebookModal eventBus={eventBus} config={config} />,
+      this.shadowRoot
+    );
   }
 
   destroy() {
+    render(null, this.shadowRoot);
     this._outerContainer.remove();
-    this._emitter.destroy();
-  }
-
-  _initContainer() {
-    if (this.container) {
-      return this.container;
-    }
-
-    const shadowRoot = createShadowRoot(this._outerContainer);
-    this.container = document.createElement('div');
-    this.container.style.display = 'none';
-    this.container.className = 'notebook-outer';
-    shadowRoot.appendChild(this.container);
-
-    const onClose = () => {
-      this.close();
-      this._emitter.publish('closeNotebook');
-    };
-
-    render(
-      <div className="Notebook__controller-bar">
-        <Button
-          icon="cancel"
-          className="Notebook__close-button"
-          buttonText="Close"
-          title="Close the Notebook"
-          onClick={onClose}
-        />
-      </div>,
-      this.container
-    );
-
-    return this.container;
   }
 }
--- src/annotator/test/notebook-test.js
+++ src/annotator/test/notebook-test.js
@@ -1,15 +1,18 @@
-import Notebook from '../notebook';
+import { useEffect } from 'preact/hooks';
+import { act } from 'preact/test-utils';
+
+import Notebook, { $imports } from '../notebook';
 import { EventBus } from '../util/emitter';
 
 describe('Notebook', () => {
   // `Notebook` instances created by current test
   let notebooks;
+  let container;
+  let cleanUpCallback;
 
   const createNotebook = (config = {}) => {
-    config = { notebookAppUrl: '/base/annotator/test/empty.html', ...config };
-    const element = document.createElement('div');
     const eventBus = new EventBus();
-    const notebook = new Notebook(element, eventBus, config);
+    const notebook = new Notebook(container, eventBus, config);
 
     notebooks.push(notebook);
 
@@ -18,140 +21,54 @@ describe('Notebook', () => {
 
   beforeEach(() => {
     notebooks = [];
+    container = document.createElement('div');
+    cleanUpCallback = sinon.stub();
+
+    const FakeNotebookModal = () => {
+      useEffect(() => {
+        return () => {
+          cleanUpCallback();
+        };
+      }, []);
+      return <div id="notebook-modal" />;
+    };
+
+    $imports.$mock({
+      './components/NotebookModal': { default: FakeNotebookModal },
+    });
   });
 
   afterEach(() => {
     notebooks.forEach(n => n.destroy());
+    $imports.$restore();
   });
 
-  describe('notebook container frame', () => {
-    it('is not created until the notebook is shown', () => {
+  describe('notebook container', () => {
+    it('creates the container', () => {
+      assert.isFalse(container.hasChildNodes());
       const notebook = createNotebook();
-      assert.isNull(notebook.container);
-
-      notebook.open();
-      assert.isNotNull(notebook.container);
+      const shadowRoot = notebook._outerContainer.shadowRoot;
+      assert.isNotNull(shadowRoot);
+      assert.isNotNull(shadowRoot.querySelector('#notebook-modal'));
     });
 
-    it('is not created if `hide` is called before notebook is first shown', () => {
+    it('removes the container', () => {
       const notebook = createNotebook();
-      notebook.close();
-      assert.isNull(notebook.container);
-    });
-
-    it('displays when opened', () => {
-      const notebook = createNotebook();
-
-      notebook.open();
-
-      assert.equal(notebook.container.style.display, '');
-      assert.isTrue(notebook.container.classList.contains('is-open'));
-    });
-
-    it('hides when closed', () => {
-      const notebook = createNotebook();
-
-      notebook.open();
-      notebook.close();
-
-      assert.equal(notebook.container.style.display, 'none');
-      assert.isFalse(notebook.container.classList.contains('is-open'));
-    });
-  });
-
-  describe('creating the notebook iframe', () => {
-    it('creates the iframe when the notebook is opened for the first time', () => {
-      const notebook = createNotebook();
-
-      assert.isNull(notebook.frame);
-
-      notebook.open();
-
-      assert.isTrue(notebook.frame instanceof Element);
-    });
-
-    it('sets the iframe source to the configured `notebookAppUrl`', () => {
-      const notebook = createNotebook({
-        notebookAppUrl: 'http://www.example.com/foo/bar',
-      });
-
-      notebook.open();
-
-      // The rest of the config gets added as a hash to the end of the src,
-      // so split that off and look at the string before it
-      assert.equal(
-        notebook.frame.src.split('#')[0],
-        'http://www.example.com/foo/bar'
-      );
-    });
-
-    it('does not create a new iframe if opened again with same group ID', () => {
-      const notebook = createNotebook();
-      notebook._groupId = 'mygroup';
-
-      // The first opening will create a new iFrame
-      notebook._emitter.publish('openNotebook', 'myGroup');
-      const removeSpy = sinon.spy(notebook.frame, 'remove');
-      // Open it again — the group hasn't changed so the iframe won't be
-      // replaced
-      notebook._emitter.publish('openNotebook', 'myGroup');
-
-      assert.notCalled(removeSpy);
-    });
-
-    it('does not create a new iframe if shown again with same group ID', () => {
-      const notebook = createNotebook();
-      notebook._groupId = 'mygroup';
-
-      // First open: creates an iframe
-      notebook._emitter.publish('openNotebook', 'myGroup');
-      const removeSpy = sinon.spy(notebook.frame, 'remove');
-
-      // Open again with another group
-      notebook._emitter.publish('openNotebook', 'anotherGroup');
-
-      // Open again, which will remove the first iframe and create a new one
-      notebook.open();
-      assert.calledOnce(removeSpy);
-    });
-  });
-
-  describe('responding to user input', () => {
-    it('closes the notebook when close button clicked', () => {
-      const notebook = createNotebook();
-
-      notebook.open();
-
-      const button = notebook.container.getElementsByClassName(
-        'Notebook__close-button'
-      )[0];
-      button.click();
-      assert.equal(notebook.container.style.display, 'none');
-    });
-  });
-
-  describe('responding to events', () => {
-    it('opens on `openNotebook`', () => {
-      const notebook = createNotebook();
-
-      notebook._emitter.publish('openNotebook');
-
-      assert.equal(notebook.container.style.display, '');
-    });
-  });
-
-  describe('destruction', () => {
-    it('should remove the frame', () => {
-      const notebook = createNotebook();
-      const hostDocument = notebook.element;
-
-      // Make sure the frame is created
-      notebook.open();
-      assert.isNotNull(hostDocument.querySelector('hypothesis-notebook'));
-
       notebook.destroy();
+      assert.isFalse(container.hasChildNodes());
+    });
 
-      assert.isNull(hostDocument.querySelector('hypothesis-notebook'));
+    it('calls the clean up function of the NotebookModal when the container is removed', () => {
+      // Necessary to run the useEffect for first time and register the cleanup function
+      let notebook;
+      act(() => {
+        notebook = createNotebook();
+      });
+      // Necessary to run the cleanup function of the useEffect
+      act(() => {
+        notebook.destroy();
+      });
+      assert.called(cleanUpCallback);
     });
   });
 });
