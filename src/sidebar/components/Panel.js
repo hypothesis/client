93234f3451438115f214b64bc4f8eae6f01db3e8
Lyza Danger Gardner <lyza@lyza.com>
2021-03-16 14:13:33 -0400

    Add basic `Panel` component
    Add a basic Panel component to be used by `SidebarPanel` and for
    other basic panel-y needs.

---
+++ src/sidebar/components/Panel.js
@@ -0,0 +1,39 @@
+import { SvgIcon } from '@hypothesis/frontend-shared';
+
+import Button from './Button';
+
+/**
+ * @typedef PanelProps
+ * @prop {import("preact").ComponentChildren} children
+ * @prop {string} [icon] - Name of optional icon to render in header
+ * @prop {() => any} [onClose] - handler for closing the panel; if provided,
+ *   will render a close button that invokes this onClick
+ * @prop {string} title
+ */
+
+/**
+ * Render a "panel"-like interface
+ *
+ * @param {PanelProps} props
+ */
+export default function Panel({ children, icon, onClose, title }) {
+  const withCloseButton = typeof onClose === 'function';
+  return (
+    <div className="Panel">
+      <div className="Panel__header">
+        {icon && (
+          <div className="Panel__header-icon">
+            <SvgIcon name={icon} title={title} />
+          </div>
+        )}
+        <h2 className="Panel__title u-stretch">{title}</h2>
+        {withCloseButton && (
+          <div>
+            <Button icon="cancel" buttonText="Close" onClick={onClose} />
+          </div>
+        )}
+      </div>
+      <div className="Panel__content">{children}</div>
+    </div>
+  );
+}
