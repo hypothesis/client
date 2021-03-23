9881844bacfa54207f480204e7c9dca65bd88047
Eduardo Sanz García <esanzgar@gmail.com>
2021-03-23 14:20:13 +0100

    Remove unnecessary container element

--- src/annotator/components/test/Toolbar-test.js
+++ src/annotator/components/test/Toolbar-test.js
@@ -9,19 +9,17 @@ const noop = () => {};
 describe('Toolbar', () => {
   const createToolbar = props =>
     mount(
-      <div>
-        <Toolbar
-          closeSidebar={noop}
-          createAnnotation={noop}
-          toggleHighlights={noop}
-          toggleSidebar={noop}
-          isSidebarOpen={false}
-          showHighlights={false}
-          newAnnotationType="note"
-          useMinimalControls={false}
-          {...props}
-        />
-      </div>
+      <Toolbar
+        closeSidebar={noop}
+        createAnnotation={noop}
+        toggleHighlights={noop}
+        toggleSidebar={noop}
+        isSidebarOpen={false}
+        showHighlights={false}
+        newAnnotationType="note"
+        useMinimalControls={false}
+        {...props}
+      />
     );
 
   const findButton = (wrapper, label) =>
