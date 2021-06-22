60d3c7591b697320db07bc25f536e755fdfd977d
Eduardo Sanz García <esanzgar@gmail.com>
2021-06-22 18:38:03 +0200

    Enable `MessageChannel` communication in the `host`
    This involves to modify the `cross-frame` service.

--- src/annotator/cross-frame.js
+++ src/annotator/cross-frame.js
@@ -1,9 +1,16 @@
-import AnnotationSync from './annotation-sync';
 import Bridge from '../shared/bridge';
-import Discovery from '../shared/discovery';
-import * as frameUtil from './util/frame-util';
-import FrameObserver from './frame-observer';
 
+import AnnotationSync from './annotation-sync';
+import { PortFinder } from './communicator';
+import FrameObserver from './frame-observer';
+import * as frameUtil from './util/frame-util';
+
+/**
+ * Generate a pseudo uuid to distinguish `guest` frames
+ */
+function generateToken() {
+  return Math.random().toString().replace(/\D/g, '');
+}
 /**
  * @typedef {import('../types/annotator').AnnotationData} AnnotationData
  * @typedef {import('../types/annotator').Destroyable} Destroyable
@@ -23,15 +30,17 @@ import FrameObserver from './frame-observer';
 export class CrossFrame {
   /**
    * @param {Element} element
+   * @param {MessagePort|null} hostPort -- enables to communicate between the
+   * `host` and `sidebar` frames. If `null`, it indicates that this is a `guest`
+   * frame.
    * @param {object} options
    *   @param {Record<string, any>} options.config,
    *   @param {boolean} [options.server]
    *   @param {(event: string, ...args: any[]) => void} options.on
    *   @param {(event: string, ...args: any[]) => void } options.emit
    */
-  constructor(element, options) {
-    const { config, server, on, emit } = options;
-    const discovery = new Discovery(window, { server });
+  constructor(element, hostPort, options) {
+    const { config, on, emit } = options;
     const bridge = new Bridge();
     const annotationSync = new AnnotationSync(bridge, { on, emit });
     const frameObserver = new FrameObserver(element);
@@ -46,7 +55,7 @@ export class CrossFrame {
       }
 
       frameUtil.isLoaded(frame, () => {
-        const subFrameIdentifier = discovery.generateToken();
+        const subFrameIdentifier = generateToken();
         frameIdentifiers.set(frame, subFrameIdentifier);
         const injectedConfig = {
           ...config,
@@ -63,10 +72,6 @@ export class CrossFrame {
       frameIdentifiers.delete(frame);
     };
 
-    // Initiate connection to the sidebar.
-    const onDiscoveryCallback = (source, origin, token) =>
-      bridge.createChannel(source, origin, token);
-    discovery.startDiscovery(onDiscoveryCallback);
     frameObserver.observe(injectIntoFrame, iframeUnloaded);
 
     /**
@@ -74,7 +79,6 @@ export class CrossFrame {
      */
     this.destroy = () => {
       bridge.destroy();
-      discovery.stopDiscovery();
       frameObserver.disconnect();
     };
 
@@ -89,7 +93,7 @@ export class CrossFrame {
      * Subscribe to an event from the sidebar.
      *
      * @param {string} event
-     * @param {Function} callback
+     * @param {(...args: any[]) => void} callback
      */
     this.on = (event, callback) => bridge.on(event, callback);
 
@@ -105,8 +109,35 @@ export class CrossFrame {
      * Register a callback to be invoked once the connection to the sidebar
      * is set up.
      *
-     * @param {Function} callback
+     * @param {(...args: any[]) => void} callback
      */
     this.onConnect = callback => bridge.onConnect(callback);
+
+    /**
+     * Connect to the `sidebar` frame by either (1) using the `host` port in the
+     * `hostToSidebar` channel or (2) discovering the `guest` port of the
+     * `guestToSidebar` channel.
+     */
+    this.connectWithSidebar = () => {
+      if (hostPort) {
+        // Because `hostPort` is not `null` we know that this is a `host` frame.
+        // In this case, use the `host` port of the `hostToSidebar` channel to
+        // communicate with the `sidebar` frame.
+        bridge.createChannelFromPort(hostPort, 'sidebar');
+        return;
+      }
+
+      // This is a `guest` frame. Discover the `guest` port on the `guestToSidebar`
+      // channel using the parent window
+      const portFinder = new PortFinder();
+      portFinder
+        .discover({
+          channel: 'guestToSidebar',
+          hostFrame: window.parent,
+          port: 'guest',
+          subFrameIdentifier: config.subFrameIdentifier,
+        })
+        .then(port => bridge.createChannelFromPort(port, 'sidebar'));
+    };
   }
 }
