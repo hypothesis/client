hypothesis/client

commit ede3280fadec2221728b080790b8ce215f3e2f3c

Remove unnecessary TS suppression for `URLSearchParams.entries`
This method is available after including the `dom.iterable` typings.
Also the use of `entries()` is unnecessary because the result is the
same as directly iterating over the `URLSearchParams` instance.

Also add an example for `stringify`.

src/sidebar/util/query-string.js
@@ -4,6 +4,9 @@
  * The returned string does not have a leading "?" and the parameters are
  * sorted by name.
  *
+ * @example
+ *   stringify({ foo: 'bar', meep: 'one two' }) // Returns "foo=bar&meep=one+two"
+ *
  * @param {Record<string, string>} params
  * @return {string}
  */
@@ -26,8 +29,7 @@ export function parse(query) {
   const params = new URLSearchParams(query);
   /** @type {Record<string, string>} */
   const result = {};
-  // @ts-expect-error - `URLSearchParams.entries` is missing.
-  for (let [key, value] of params.entries()) {
+  for (let [key, value] of params) {
     result[key] = value;
   }
   return result;

commit 0231eaa5d107bf56ea63db8a50086a30ddf524d6

Add test case for multi-valued parameters in API requests
Check that multi-valued parameters are handled as expected when making
API requests. The test uses the example of a `search` API call with
multiple URIs, as that's the most important example.

Add a test case that would have caught the regression introduced by
https://github.com/hypothesis/client/pull/3582.

src/sidebar/services/test/api-test.js
@@ -156,6 +156,23 @@ describe('APIService', () => {
     return api.search({ uri: 'http://foobar.com/?foo=bar;baz=qux' });
   });
 
+  // Test that covers the most critical use case of multiple values being
+  // provided for an API parameter. Other API calls accept multiple values
+  // as well though.
+  it('repeats query parameters when multiple values are provided', () => {
+    const pdfURL = 'https://example.com/test.pdf';
+    const fingerprintURL = 'urn:x-pdf:foobar';
+
+    expectCall(
+      'get',
+      `search?uri=${encodeURIComponent(pdfURL)}&uri=${encodeURIComponent(
+        fingerprintURL
+      )}`
+    );
+
+    return api.search({ uri: [pdfURL, fingerprintURL] });
+  });
+
   it("fetches the user's profile", () => {
     const profile = { userid: 'acct:user@publisher.org' };
     expectCall('get', 'profile?authority=publisher.org', 200, profile);