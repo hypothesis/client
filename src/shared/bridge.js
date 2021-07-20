efc316ddd2ed92f761da900eb2c11b4ff1efd828
Robert Knight <robertknight@gmail.com>
2021-07-20 15:49:06 +0100

    Remove the need for some test-only code in Bridge
    The need for this "testing only" code can be removed by improving the way
    that we simulate the `connect` message being handled in tests.

--- src/shared/bridge.js
+++ src/shared/bridge.js
@@ -118,7 +118,6 @@ export default class Bridge {
     const connect = (_token, cb) => {
       if (_token === token) {
         cb();
-        ready(); // This is necessary for testing only.
       }
     };
 
