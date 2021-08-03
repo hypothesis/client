b48d61868a45f845ce36ee1a913a65a68f9633b3
Robert Knight <robertknight@gmail.com>
2021-08-03 15:32:09 +0100

    Omit query params with `undefined` values from API calls
    The previous version stated that it aimed to match the behavior of the
    `query-string` package but it was actually tested against Node's
    `querystring` package, which has slightly different behavior for this
    edge case. It omits parameters with `undefined` values and renders empty
    parameters for `null` values.
    
    The notebook view was calling `api.search({ uri: undefined, ... })` and
    this translated to `/api/search?uri=&...` which caused an error.
    
    Omit parameters instead in this case.

--- src/sidebar/services/api.js
+++ src/sidebar/services/api.js
@@ -128,15 +128,18 @@ function createAPICall(
           params
         );
 
-        /** @param {Param?} param */
-        const serialize = param => param?.toString() ?? '';
-
         const apiURL = new URL(url);
         for (let [key, value] of Object.entries(queryParams)) {
-          if (Array.isArray(value)) {
-            value.forEach(v => apiURL.searchParams.append(key, serialize(v)));
-          } else {
-            apiURL.searchParams.append(key, serialize(value));
+          if (!Array.isArray(value)) {
+            value = [value];
+          }
+          for (let item of value) {
+            // eslint-disable-next-line eqeqeq
+            if (item == null) {
+              // Skip all parameters with nullish values.
+              continue;
+            }
+            apiURL.searchParams.append(key, item.toString());
           }
         }
 
