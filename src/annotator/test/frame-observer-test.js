d0e56e8ad0bb167eb160f85722340dec253ef4aa
Eduardo Sanz García <esanzgar@gmail.com>
2021-08-13 12:32:20 +0200

    Add unit testing for the `FrameObserver` class
    The `FrameObserver` unit test now exercise the whole class, not just the
    `findFrames` function.
    
    On a future commit, we will inline the `findFrames functionality.

--- src/annotator/test/frame-observer-test.js
+++ src/annotator/test/frame-observer-test.js
@@ -1,42 +1,81 @@
-import { findFrames } from '../frame-observer';
+import FrameObserver, { DEBOUNCE_WAIT } from '../frame-observer';
 
-describe('findFrames', () => {
+describe('FrameObserver', () => {
   let container;
+  let frameObserver;
+  let onFrameAdded;
+  let onFrameRemoved;
 
-  const _addFrameToContainer = () => {
+  function waitForFrameObserver() {
+    return new Promise(resolve => setTimeout(resolve, DEBOUNCE_WAIT + 10));
+  }
+
+  function createAnnotatableIFrame(
+    attribute = 'enable-annotation',
+    value = ''
+  ) {
     const frame = document.createElement('iframe');
-    frame.setAttribute('enable-annotation', '');
+    frame.setAttribute(attribute, value);
     container.appendChild(frame);
     return frame;
-  };
+  }
 
   beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container);
+    onFrameAdded = sinon.stub();
+    onFrameRemoved = sinon.stub();
+    frameObserver = new FrameObserver(container, onFrameAdded, onFrameRemoved);
   });
 
   afterEach(() => {
     container.remove();
+    frameObserver.disconnect();
   });
 
-  it('should return valid frames', () => {
-    let foundFrames = findFrames(container);
+  it('triggers onFrameAdded when an annotatable iframe is added', async () => {
+    let frame = createAnnotatableIFrame();
+    await waitForFrameObserver();
 
-    assert.lengthOf(foundFrames, 0);
+    assert.calledWith(onFrameAdded, frame);
 
-    const frame1 = _addFrameToContainer();
-    const frame2 = _addFrameToContainer();
-    foundFrames = findFrames(container);
+    frame = createAnnotatableIFrame('enable-annotation', 'yes');
+    await waitForFrameObserver();
 
-    assert.deepEqual([...foundFrames], [frame1, frame2]);
+    assert.calledWith(onFrameAdded, frame);
+
+    frame = createAnnotatableIFrame('enable-annotation', 'true');
+    await waitForFrameObserver();
+
+    assert.calledWith(onFrameAdded, frame);
+
+    frame = createAnnotatableIFrame('enable-annotation', '1');
+    await waitForFrameObserver();
+
+    assert.calledWith(onFrameAdded, frame);
+
+    frame = createAnnotatableIFrame('enable-annotation', 'false'); // the actual value of the attribute is irrelevant
+    await waitForFrameObserver();
+
+    assert.calledWith(onFrameAdded, frame);
   });
 
-  it('should not return frames that have not opted into annotation', () => {
-    const frame = _addFrameToContainer();
+  it("doesn't trigger onFrameAdded when non-annotatable iframes are added", async () => {
+    createAnnotatableIFrame('dummy-attribute');
+    await waitForFrameObserver();
 
-    frame.removeAttribute('enable-annotation');
-    const foundFrames = findFrames(container);
-
-    assert.lengthOf(foundFrames, 0);
+    assert.notCalled(onFrameAdded);
   });
+
+  // This test doesn't work. Surprisingly, `isAccessible` returns `true` even
+  // thought the iframe is from a different domain. My suspicion is that the
+  // iframe is accessed at a time where the loading has not yet started and it
+  // is therefore accessible.
+  // it("doesn't triggers onFrameAdded when annotatable iframe is from a different domain", async () => {
+  //   const frame = createAnnotatableIFrame('src', 'https://example.com');
+  //   frame.setAttribute('enable-annotation', '');
+  //   await waitForFrameObserver();
+
+  //   assert.notCalled(onFrameAdded);
+  // });
 });
