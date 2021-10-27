192011b90e0f9dce340a1d0b116caaf62836e9af
Lyza Danger Gardner <lyza@lyza.com>
2021-10-27 15:44:03 -0400

    Update to use shared icons where possible

--- src/sidebar/icons.js
+++ src/sidebar/icons.js
@@ -1,100 +1,105 @@
 // @ts-nocheck - TS doesn't understand SVG imports.
 
-import addIcon from '../images/icons/add.svg';
+import {
+  arrowLeft,
+  arrowRight,
+  cancel,
+  caretRight,
+  caretUp,
+  ccStd,
+  ccZero,
+  check,
+  copy,
+  edit,
+  editorLatex,
+  editorQuote,
+  editorTextBold,
+  editorTextItalic,
+  email,
+  external,
+  flag,
+  flagFilled,
+  globe,
+  groups,
+  help,
+  highlight,
+  image,
+  leave,
+  link,
+  listOrdered,
+  listUnordered,
+  logo,
+  lock,
+  plus,
+  profile,
+  refresh,
+  restricted,
+  search,
+  share,
+  socialFacebook,
+  socialTwitter,
+  trash,
+} from '@hypothesis/frontend-shared/lib/icons';
+
+// The following icons differ here from the shared-package versions
 import annotateIcon from '../images/icons/annotate.svg';
-import arrowLeftIcon from '../images/icons/arrow-left.svg';
-import arrowRightIcon from '../images/icons/arrow-right.svg';
-import cancelIcon from '../images/icons/cancel.svg';
-import caretRightIcon from '../images/icons/caret-right.svg';
-import ccStdIcon from '../images/icons/cc-std.svg';
-import ccZeroIcon from '../images/icons/cc-zero.svg';
-import collapseMenuIcon from '../images/icons/collapse-menu.svg';
-import collapsedIcon from '../images/icons/collapsed.svg';
-import copyIcon from '../images/icons/copy.svg';
-import cursorIcon from '../images/icons/cursor.svg';
-import editIcon from '../images/icons/edit.svg';
-import emailIcon from '../images/icons/email.svg';
-import expandMenuIcon from '../images/icons/expand-menu.svg';
-import externalIcon from '../images/icons/external.svg';
-import facebookIcon from '../images/icons/facebook.svg';
-import flagIcon from '../images/icons/flag.svg';
-import flagActiveIcon from '../images/icons/flag--active.svg';
-import formatBoldIcon from '../images/icons/format-bold.svg';
-import formatFunctionsIcon from '../images/icons/format-functions.svg';
-import formatItalicIcon from '../images/icons/format-italic.svg';
-import formatListNumberedIcon from '../images/icons/format-list-numbered.svg';
-import formatListUnorderedIcon from '../images/icons/format-list-unordered.svg';
-import formatQuoteIcon from '../images/icons/format-quote.svg';
-import groupsIcon from '../images/icons/groups.svg';
-import helpIcon from '../images/icons/help.svg';
-import highlightIcon from '../images/icons/highlight.svg';
-import imageIcon from '../images/icons/image.svg';
-import leaveIcon from '../images/icons/leave.svg';
-import linkIcon from '../images/icons/link.svg';
-import lockIcon from '../images/icons/lock.svg';
-import logoIcon from '../images/icons/logo.svg';
-import pointerIcon from '../images/icons/pointer.svg';
-import profileIcon from '../images/icons/profile.svg';
-import publicIcon from '../images/icons/public.svg';
-import refreshIcon from '../images/icons/refresh.svg';
-import restrictedIcon from '../images/icons/restricted.svg';
 import replyIcon from '../images/icons/reply.svg';
-import searchIcon from '../images/icons/search.svg';
-import shareIcon from '../images/icons/share.svg';
-import checkIcon from '../images/icons/check.svg';
 import sortIcon from '../images/icons/sort.svg';
-import trashIcon from '../images/icons/trash.svg';
-import twitterIcon from '../images/icons/twitter.svg';
+
+// This is very similar to caretDown but is larger
+import expandMenuIcon from '../images/icons/expand-menu.svg';
+
+// Not in shared package
+import pointerIcon from '../images/icons/pointer.svg';
 
 /**
  * Set of icons used by the sidebar application via the `SvgIcon`
  * component.
  */
 export default {
-  add: addIcon,
+  add: plus,
   annotate: annotateIcon,
-  'arrow-left': arrowLeftIcon,
-  'arrow-right': arrowRightIcon,
-  cancel: cancelIcon,
-  'caret-right': caretRightIcon,
-  'cc-std': ccStdIcon,
-  'cc-zero': ccZeroIcon,
-  'collapse-menu': collapseMenuIcon,
-  collapsed: collapsedIcon,
-  copy: copyIcon,
-  cursor: cursorIcon,
-  edit: editIcon,
-  email: emailIcon,
+  'arrow-left': arrowLeft,
+  'arrow-right': arrowRight,
+  cancel,
+  'caret-right': caretRight,
+  'cc-std': ccStd,
+  'cc-zero': ccZero,
+  'collapse-menu': caretUp,
+  collapsed: caretRight,
+  copy,
+  edit,
+  email,
   'expand-menu': expandMenuIcon,
-  error: cancelIcon,
-  external: externalIcon,
-  facebook: facebookIcon,
-  flag: flagIcon,
-  'flag--active': flagActiveIcon,
-  'format-bold': formatBoldIcon,
-  'format-functions': formatFunctionsIcon,
-  'format-italic': formatItalicIcon,
-  'format-list-numbered': formatListNumberedIcon,
-  'format-list-unordered': formatListUnorderedIcon,
-  'format-quote': formatQuoteIcon,
-  groups: groupsIcon,
-  help: helpIcon,
-  highlight: highlightIcon,
-  image: imageIcon,
-  leave: leaveIcon,
-  link: linkIcon,
-  lock: lockIcon,
-  logo: logoIcon,
+  error: cancel,
+  external,
+  facebook: socialFacebook,
+  flag,
+  'flag--active': flagFilled,
+  'format-bold': editorTextBold,
+  'format-functions': editorLatex,
+  'format-italic': editorTextItalic,
+  'format-list-numbered': listOrdered,
+  'format-list-unordered': listUnordered,
+  'format-quote': editorQuote,
+  groups,
+  help,
+  highlight,
+  image,
+  leave,
+  link,
+  lock,
+  logo,
   pointer: pointerIcon,
-  profile: profileIcon,
-  public: publicIcon,
-  refresh: refreshIcon,
-  restricted: restrictedIcon,
+  profile,
+  public: globe,
+  refresh,
+  restricted,
   reply: replyIcon,
-  search: searchIcon,
-  share: shareIcon,
-  success: checkIcon,
+  search,
+  share,
+  success: check,
   sort: sortIcon,
-  trash: trashIcon,
-  twitter: twitterIcon,
+  trash,
+  twitter: socialTwitter,
 };
