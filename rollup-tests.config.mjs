44310d042edbe1630e94770c53a822b2ef6e0da8
Robert Knight <robertknight@gmail.com>
2021-10-27 11:12:06 +0100

    Remove obsolete JSX runtime aliases
    This is no longer needed with the current version of
    @hypothesis/frontend-shared.

--- rollup-tests.config.mjs
+++ rollup-tests.config.mjs
@@ -1,6 +1,5 @@
 import { readFileSync } from 'fs';
 
-import alias from '@rollup/plugin-alias';
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
@@ -29,16 +28,6 @@ export default {
       // Node modules that are not available in the browser.
       crypto: '',
       util: '',
-    }),
-    alias({
-      entries: [
-        {
-          // This is needed because of Babel configuration used by
-          // @hypothesis/frontend-shared. It can be removed once that is fixed.
-          find: 'preact/compat/jsx-dev-runtime',
-          replacement: 'preact/jsx-dev-runtime',
-        },
-      ],
     }),
     replace({
       preventAssignment: true,
