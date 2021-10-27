1ab5574306a32d8ca123a7e7e30937f3ccffc1f8
Lyza Danger Gardner <lyza@lyza.com>
2021-10-27 15:44:13 -0400

    Update to use shared icons where possible

--- src/annotator/icons.js
+++ src/annotator/icons.js
@@ -1,15 +1,20 @@
 // @ts-nocheck
 
+import {
+  cancel,
+  caretLeft,
+  caretRight,
+  caution,
+  hide,
+  highlight,
+  note,
+  show,
+} from '@hypothesis/frontend-shared/lib/icons';
+
+// Different variant than shared icon
 import annotateIcon from '../images/icons/annotate.svg';
-import cancelIcon from '../images/icons/cancel.svg';
-import cautionIcon from '../images/icons/caution.svg';
-import caretLeftIcon from '../images/icons/caret-left.svg';
-import caretRightIcon from '../images/icons/caret-right.svg';
-import hideIcon from '../images/icons/hide.svg';
-import highlightIcon from '../images/icons/highlight.svg';
-import noteIcon from '../images/icons/note.svg';
+// Not in shared icon set
 import pointerIcon from '../images/icons/pointer.svg';
-import showIcon from '../images/icons/show.svg';
 
 /**
  * Set of icons used by the annotator part of the application via the `SvgIcon`
@@ -17,13 +22,13 @@ import showIcon from '../images/icons/show.svg';
  */
 export default {
   annotate: annotateIcon,
-  cancel: cancelIcon,
-  caution: cautionIcon,
-  'caret-left': caretLeftIcon,
-  'caret-right': caretRightIcon,
-  hide: hideIcon,
-  highlight: highlightIcon,
-  note: noteIcon,
+  cancel,
+  caution,
+  'caret-left': caretLeft,
+  'caret-right': caretRight,
+  hide,
+  highlight,
+  note,
   pointer: pointerIcon,
-  show: showIcon,
+  show,
 };
