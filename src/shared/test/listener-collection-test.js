2eba3b24daf511027f9bf68dac5c0e43ffc5707a
Eduardo Sanz García <esanzgar@gmail.com>
2021-11-09 10:09:08 +0100

    Add a remove method to ListenerCollection
    This new method allows to unregister individual event listeners.

--- src/shared/test/listener-collection-test.js
+++ src/shared/test/listener-collection-test.js
@@ -11,23 +11,41 @@ describe('ListenerCollection', () => {
     listeners.removeAll();
   });
 
-  it('registers and triggers event listener', () => {
-    const listener = sinon.stub();
-    listeners.add(window, 'resize', listener);
+  describe('#add', () => {
+    it('registers and triggers event listener', () => {
+      const listener = sinon.stub();
+      listeners.add(window, 'resize', listener);
 
-    window.dispatchEvent(new Event('resize'));
-    assert.calledOnce(listener);
+      window.dispatchEvent(new Event('resize'));
+      assert.calledOnce(listener);
+    });
   });
 
-  it('unregisters event listeners', () => {
-    const listener1 = sinon.stub();
-    const listener2 = sinon.stub();
-    listeners.add(window, 'resize', listener1);
-    listeners.add(window, 'resize', listener2);
-    listeners.removeAll();
+  describe('#remove', () => {
+    it('unregisters some event listeners', () => {
+      const listener1 = sinon.stub();
+      const listener2 = sinon.stub();
+      listeners.add(window, 'resize', listener1);
+      const listenerId = listeners.add(window, 'resize', listener2);
+      listeners.remove(listenerId);
 
-    window.dispatchEvent(new Event('resize'));
-    assert.notCalled(listener1);
-    assert.notCalled(listener2);
+      window.dispatchEvent(new Event('resize'));
+      assert.calledOnce(listener1);
+      assert.notCalled(listener2);
+    });
+  });
+
+  describe('#removeAll', () => {
+    it('unregisters all event listeners', () => {
+      const listener1 = sinon.stub();
+      const listener2 = sinon.stub();
+      listeners.add(window, 'resize', listener1);
+      listeners.add(window, 'resize', listener2);
+      listeners.removeAll();
+
+      window.dispatchEvent(new Event('resize'));
+      assert.notCalled(listener1);
+      assert.notCalled(listener2);
+    });
   });
 });
