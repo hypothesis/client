41105c91e6a030cc3e52c33d1a61a68066ea91ba
Lyza Danger Gardner <lyza@lyza.com>
2022-02-02 15:47:52 -0500

    Modernize styling of `AdderToolbar`
    Convert the `AdderToolbar` component to tailwind utility-first styles.
    
    This component has some styling complexity that requires the continued
    use of some external styles for now:
    
    * It serves as the root element in a shadow DOM and has a couple of
      reset rules not serviceable by tailwind utility classes
    * The hover behavior of the adder toolbar buttons is too complex to
      implement with utility styles alone.
    
    Visual differences before/after are negligible. The adder is 1px taller
    due to using standardized spacing.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,10 +10,20 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        adderPopUp: 'adderPopUp 0.08s ease-in forwards',
+        adderPopDown: 'adderPopDown 0.08s ease-in forwards',
+      },
+      boxShadow: {
+        adderToolbar: '0px 2px 10px 0px rgba(0, 0, 0, 0.25)',
+      },
       colors: {
         slate: {
           // TODO remove when available from upstream preset
           1: '#e3e3e5',
+        },
+        'color-text': {
+          inverted: '#f2f2f2',
         },
       },
       fontFamily: {
@@ -31,9 +41,41 @@ export default {
       fontSize: {
         tiny: ['10px'],
         sm: ['11px', '1.4'],
+        'annotator-sm': ['12px'],
         base: ['13px', '1.4'],
         lg: ['14px'],
         xl: ['16px'],
+        annotator: {
+          sm: ['12px'],
+        },
+      },
+      keyframes: {
+        adderPopDown: {
+          '0%': {
+            opacity: '0.05',
+            transform: 'scale(0.8) translateY(10px)',
+          },
+          '20%': {
+            opacity: '0.7',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1) translateY(0px)',
+          },
+        },
+        adderPopUp: {
+          '0%': {
+            opacity: '0.05',
+            transform: 'scale(0.8) translateY(-10px)',
+          },
+          '20%': {
+            opacity: '0.7',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1) translateY(0px)',
+          },
+        },
       },
     },
   },
