6d50b1b5465c2f1c83d6bb3f673e8266b5396c85
Lyza Danger Gardner <lyza@lyza.com>
2022-02-25 15:28:42 -0500

    Transition `WarningBanner` to tailwind; update to shared components

--- src/annotator/components/WarningBanner.js
+++ src/annotator/components/WarningBanner.js
@@ -1,4 +1,5 @@
-import { SvgIcon } from '@hypothesis/frontend-shared';
+import { Icon, Link } from '@hypothesis/frontend-shared';
+import classnames from 'classnames';
 
 /**
  * A banner shown at the top of the PDF viewer if the PDF cannot be annotated
@@ -6,20 +7,26 @@ import { SvgIcon } from '@hypothesis/frontend-shared';
  */
 export default function WarningBanner() {
   return (
-    <div className="WarningBanner WarningBanner--notice">
-      <div className="WarningBanner__type">
-        <SvgIcon name="caution" className="WarningBanner__icon" />
-      </div>
-      <div className="WarningBanner__message">
-        <strong>This PDF does not contain selectable text:</strong>{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://web.hypothes.is/help/how-to-ocr-optimize-pdfs/"
-        >
-          Learn how to fix this
-        </a>{' '}
-        in order to annotate with Hypothesis.
+    <div className="bg-white">
+      <div
+        className={classnames(
+          'flex items-center gap-x-2',
+          'border border-yellow-notice bg-yellow-notice/10 text-annotator-base'
+        )}
+      >
+        <div className="bg-yellow-notice text-white p-2">
+          <Icon name="caution" classes="text-annotator-xl" />
+        </div>
+        <div>
+          <strong>This PDF does not contain selectable text:</strong>{' '}
+          <Link
+            target="_blank"
+            href="https://web.hypothes.is/help/how-to-ocr-optimize-pdfs/"
+          >
+            Learn how to fix this
+          </Link>{' '}
+          in order to annotate with Hypothesis.
+        </div>
       </div>
     </div>
   );
