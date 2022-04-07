cf10411974b4e9d63aa359ac2aba2e15054cb68e
Robert Knight <robertknight@gmail.com>
2022-04-07 22:48:43 +0100

    Add types to links store module

--- src/sidebar/store/modules/links.js
+++ src/sidebar/store/modules/links.js
@@ -1,10 +1,15 @@
-import { actionTypes } from '../util';
 import { replaceURLParams } from '../../util/url';
-import { createStoreModule } from '../create-store';
+import { createStoreModule, makeAction } from '../create-store';
 
 const initialState = /** @type {Record<string, string>|null} */ (null);
 
+/** @typedef {typeof initialState} State */
+
 const reducers = {
+  /**
+   * @param {State} state
+   * @param {{ links: Record<string, string> }} action
+   */
   UPDATE_LINKS(state, action) {
     return {
       ...action.links,
@@ -12,18 +17,13 @@ const reducers = {
   },
 };
 
-const actions = actionTypes(reducers);
-
 /**
  * Update the link map
  *
  * @param {Record<string, string>} links - Link map fetched from the `/api/links` endpoint
  */
 function updateLinks(links) {
-  return {
-    type: actions.UPDATE_LINKS,
-    links,
-  };
+  return makeAction(reducers, 'UPDATE_LINKS', { links });
 }
 
 /**
@@ -31,9 +31,9 @@ function updateLinks(links) {
  *
  * Returns an empty string if links have not been fetched yet.
  *
+ * @param {State} state
  * @param {string} linkName
  * @param {Record<string, string>} [params]
- * @return {string}
  */
 function getLink(state, linkName, params = {}) {
   if (!state) {
