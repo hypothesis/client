0874831a33dc82839e8671f5fe351fa7cbcd375a
Lyza Danger Gardner <lyza@lyza.com>
2022-04-25 13:14:20 -0400

    Convert several simpler sidebar components to Tailwind
    - Convert `LaunchErrorPanel`
    - Convert `SidebarContentError`
    - Convert `SortMenu`

--- src/sidebar/components/LaunchErrorPanel.js
+++ src/sidebar/components/LaunchErrorPanel.js
@@ -1,4 +1,5 @@
 import { Panel } from '@hypothesis/frontend-shared';
+import classnames from 'classnames';
 
 /**
  * @typedef LaunchErrorPanelProps
@@ -15,7 +16,13 @@ import { Panel } from '@hypothesis/frontend-shared';
  */
 export default function LaunchErrorPanel({ error }) {
   return (
-    <div className="LaunchErrorPanel">
+    <div
+      className={classnames(
+        // The large top-margin is to ensure the panel clears the close button
+        // in the Notebook
+        'm-2 mt-12'
+      )}
+    >
       <Panel title="Unable to start Hypothesis">{error.message}</Panel>
     </div>
   );
