6de3558a8bc0d6b627fc68a5005c9d899f80412f
Lyza Danger Gardner <lyza@lyza.com>
2022-05-10 12:51:37 -0400

    Remove local patterns CSS and Buttons patterns page

--- dev-server/ui-playground/index.js
+++ dev-server/ui-playground/index.js
@@ -1,16 +1,8 @@
 import { startApp } from '@hypothesis/frontend-shared/lib/pattern-library';
-import ButtonPatterns from './components/ButtonPatterns';
 
 import { sidebarIcons } from '../../src/sidebar/icons';
 
 /** @type {import('@hypothesis/frontend-shared/lib/pattern-library').PlaygroundRoute[]} */
-const extraRoutes = [
-  {
-    route: '/buttons',
-    title: 'Buttons',
-    component: ButtonPatterns,
-    group: 'components',
-  },
-];
+const extraRoutes = [];
 
 startApp({ baseURL: '/ui-playground', extraRoutes, icons: sidebarIcons });
