231f9e3dd8261b9de0bd0e8c7f87ff51a7dac50b
Lyza Danger Gardner <lyza@lyza.com>
2022-08-23 12:20:46 -0400

    Remove out-of-date button patterns from local pattern library

--- dev-server/ui-playground/index.js
+++ dev-server/ui-playground/index.js
@@ -1,8 +1,3 @@
 import { startApp } from '@hypothesis/frontend-shared/lib/pattern-library';
 
-import { sidebarIcons } from '../../src/sidebar/icons';
-
-/** @type {import('@hypothesis/frontend-shared/lib/pattern-library').PlaygroundRoute[]} */
-const extraRoutes = [];
-
-startApp({ baseURL: '/ui-playground', extraRoutes, icons: sidebarIcons });
+startApp({ baseURL: '/ui-playground' });
