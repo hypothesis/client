ea1d84f87879d19218f5969e5b81797303d9ec82
Robert Knight <robertknight@gmail.com>
2022-09-23 16:02:32 +0100

    Update bump-version.mjs for compatibility with semver v7
    The export of this CommonJS module is not a function any more. We
    currently don't have an explicit dependency on the semver package but
    get it as a transitive dependency of other packages. We should probably
    change that.

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -6,6 +6,6 @@ import semver from 'semver';
 //   node bump-version.mjs minor 1.2.3  # Outputs 1.3.0
 const partStr = process.argv[2];
 const versionStr = process.argv[3];
-const version = semver(versionStr);
+const version = semver.parse(versionStr);
 version.inc(partStr);
 console.log(version.toString());
