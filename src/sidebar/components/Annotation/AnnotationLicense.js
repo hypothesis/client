608965f5f5145c7e1a7687689f5616a33da91d55
Lyza Danger Gardner <lyza@lyza.com>
2022-10-12 13:39:50 -0400

    Update shared components in `AnnotationLicense`
    Visual change: Link hover is `brand` instead of `brand-dark` to match
    conventional color combinations.

--- src/sidebar/components/Annotation/AnnotationLicense.js
+++ src/sidebar/components/Annotation/AnnotationLicense.js
@@ -1,4 +1,8 @@
-import { Icon, Link } from '@hypothesis/frontend-shared';
+import {
+  Link,
+  CcStdIcon,
+  CcZeroIcon,
+} from '@hypothesis/frontend-shared/lib/next';
 
 /**
  * Render information about CC licensing
@@ -7,13 +11,14 @@ export default function AnnotationLicense() {
   return (
     <div className="pt-2 border-t text-sm leading-none">
       <Link
-        classes="flex items-center text-color-text-light"
+        classes="flex items-center"
+        color="text-light"
         href="http://creativecommons.org/publicdomain/zero/1.0/"
         target="_blank"
         title="View more information about the Creative Commons Public Domain dedication"
       >
-        <Icon name="cc-std" classes="text-tiny" />
-        <Icon name="cc-zero" classes="ml-px text-tiny" />
+        <CcStdIcon className="w-em h-em text-tiny" />
+        <CcZeroIcon className="w-em h-em ml-px text-tiny" />
         <div className="ml-1">
           Annotations can be freely reused by anyone for any purpose.
         </div>
