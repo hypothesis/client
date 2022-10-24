cc9de92d81f088f52c82d2a04e3ebfd1de5dfe69
Robert Knight <robertknight@gmail.com>
2022-10-24 15:25:46 +0100

    Enable Prettier for code other than PDF.js in dev-server/static/scripts/
    We only want to exclude the vendored PDF.js code from Prettier formatting. The
    other small scripts in dev-server/static/scripts/ should be formatted.

--- dev-server/static/scripts/index.js
+++ dev-server/static/scripts/index.js
@@ -1,10 +1,10 @@
- /**
-  * Because the code in this file is not transpiled by Babel, it must be compatible
-  * with all the supported browsers version (see `browserlist` in `package.json`)
-  * without transpilation. Do not include latest EcmaScript features as these
-  * will cause exceptions while working on dev (`localhost:3000`) on slightly
-  * older, yet supported browser versions.
-  */
+/**
+ * Because the code in this file is not transpiled by Babel, it must be compatible
+ * with all the supported browsers version (see `browserlist` in `package.json`)
+ * without transpilation. Do not include latest EcmaScript features as these
+ * will cause exceptions while working on dev (`localhost:3000`) on slightly
+ * older, yet supported browser versions.
+ */
 
 // Code for controls on the dev server homepage.
 
@@ -24,11 +24,18 @@
   };
 
   // Set up links whose URLs should have a randomly generated suffix.
-  const randomizedLinks = Array.from(document.querySelectorAll('.js-randomize-url'));
+  const randomizedLinks = Array.from(
+    document.querySelectorAll('.js-randomize-url')
+  );
   for (let link of randomizedLinks) {
     const randomizeUrl = () => {
-      const randomHexString = Math.random().toString().slice(2 /* strip "0." prefix */, 6);
-      link.href = link.href.replace(/(\/rand-.*)?$/, `/rand-${randomHexString}`);
+      const randomHexString = Math.random()
+        .toString()
+        .slice(2 /* strip "0." prefix */, 6);
+      link.href = link.href.replace(
+        /(\/rand-.*)?$/,
+        `/rand-${randomHexString}`
+      );
     };
     randomizeUrl();
     link.addEventListener('click', randomizeUrl);
