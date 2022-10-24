a02defc859ad818e873dede1a36e78840da28b3e
Robert Knight <robertknight@gmail.com>
2022-10-24 15:25:46 +0100

    Enable Prettier for code other than PDF.js in dev-server/static/scripts/
    We only want to exclude the vendored PDF.js code from Prettier formatting. The
    other small scripts in dev-server/static/scripts/ should be formatted.

--- dev-server/static/scripts/util.js
+++ dev-server/static/scripts/util.js
@@ -1,10 +1,10 @@
- /**
-  * Because the code in this file is not transpiled by Babel, it must be compatible
-  * with all the supported browsers version (see `browserlist` in `package.json`)
-  * without transpilation. Do not include latest EcmaScript features as these
-  * will cause exceptions while working on dev (`localhost:3000`) on slightly
-  * older, yet supported browser versions.
-  */
+/**
+ * Because the code in this file is not transpiled by Babel, it must be compatible
+ * with all the supported browsers version (see `browserlist` in `package.json`)
+ * without transpilation. Do not include latest EcmaScript features as these
+ * will cause exceptions while working on dev (`localhost:3000`) on slightly
+ * older, yet supported browser versions.
+ */
 
 /** @type {string|null} */
 let activeClientUrl;
@@ -27,7 +27,9 @@ function loadClient(clientUrl) {
  * This uses the same method as the browser extension does to deactivate the client.
  */
 function unloadClient() {
-  let annotatorLink = document.querySelector('link[type="application/annotator+html"]');
+  let annotatorLink = document.querySelector(
+    'link[type="application/annotator+html"]'
+  );
 
   if (annotatorLink) {
     annotatorLink.dispatchEvent(new Event('destroy'));
