f26adc20178ed012d2b8c4d4ab6a5cb891cf975a
Lyza Danger Gardner <lyza@lyza.com>
2022-11-09 09:20:55 -0500

    Position highlight controls relative to content container offset
    Fixed positioning is always relative to the viewport, so set the top
    position of the fixed-position highlight controls to take the integration's
    content container's vertical offset from the viewport into account.
    
    Translation: ensure that highlight controls are not obscured by PDFjs'
    top menu/controls bar.

--- src/annotator/highlight-clusters.tsx
+++ src/annotator/highlight-clusters.tsx
@@ -77,7 +77,7 @@ export class HighlightClusterController implements Destroyable {
     // For now, the controls are fixed at top-left of screen. This is temporary.
     Object.assign(this._outerContainer.style, {
       position: 'fixed',
-      top: '4px',
+      top: `${this._element.offsetTop + 4}px`,
       left: '4px',
     });
 
