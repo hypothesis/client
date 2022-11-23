0e8393cd119ddf549cbef4845117646f4cb364e6
Robert Knight <robertknight@gmail.com>
2022-11-23 14:48:11 +0000

    Change ThreadList to use ReplaceWhenOffscreen

--- src/sidebar/components/ThreadList.tsx
+++ src/sidebar/components/ThreadList.tsx
@@ -1,33 +1,11 @@
-import { useEffect, useLayoutEffect, useMemo, useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import classnames from 'classnames';
-import debounce from 'lodash.debounce';
 
-import { ListenerCollection } from '../../shared/listener-collection';
 import type { Thread } from '../helpers/build-thread';
-import {
-  calculateVisibleThreads,
-  THREAD_DIMENSION_DEFAULTS,
-} from '../helpers/visible-threads';
 import { useSidebarStore } from '../store';
-import { getElementHeightWithMargins } from '../util/dom';
 
+import ReplaceWhenOffscreen from './ReplaceWhenOffscreen';
 import ThreadCard from './ThreadCard';
-
-// The precision of the `scrollPosition` value in pixels; values will be rounded
-// down to the nearest multiple of this scale value
-const SCROLL_PRECISION = 50;
-
-function getScrollContainer() {
-  const container = document.querySelector('.js-thread-list-scroll-root');
-  if (!container) {
-    throw new Error('Scroll container is missing');
-  }
-  return container;
-}
-
-function roundScrollPosition(pos: number) {
-  return Math.max(pos - (pos % SCROLL_PRECISION), 0);
-}
 
 export type ThreadListProps = {
   threads: Thread[];
@@ -43,64 +21,9 @@ export type ThreadListProps = {
  * embedded YouTube videos, rendered math and more.
  */
 export default function ThreadList({ threads }: ThreadListProps) {
-  // Client height of the scroll container.
-  const [scrollContainerHeight, setScrollContainerHeight] = useState(0);
-
-  // Scroll offset of scroll container, rounded to a multiple of `SCROLL_PRECISION`
-  // to avoid excessive re-renderings.
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  // Measure the initial size and offset of the scroll container once rendering
-  // is complete and attach listeners to observe future size or scroll offset changes.
-  useLayoutEffect(() => {
-    const listeners = new ListenerCollection();
-    const scrollContainer = getScrollContainer();
-
-    setScrollContainerHeight(scrollContainer.clientHeight);
-    setScrollPosition(roundScrollPosition(scrollContainer.scrollTop));
-
-    const updateScrollPosition = debounce(
-      () => {
-        setScrollContainerHeight(scrollContainer.clientHeight);
-        setScrollPosition(roundScrollPosition(scrollContainer.scrollTop));
-      },
-      10,
-      { maxWait: 100 }
-    );
-
-    listeners.add(scrollContainer, 'scroll', updateScrollPosition);
-
-    // We currently assume that the scroll container's size only changes with
-    // the window as a whole. A more general approach would involve using
-    // ResizeObserver via the `observeElementSize` utility.
-    listeners.add(window, 'resize', updateScrollPosition);
-
-    return () => {
-      listeners.removeAll();
-      updateScrollPosition.cancel();
-    };
-  }, []);
-
-  // Map of thread ID to measured height of thread.
-  const [threadHeights, setThreadHeights] = useState(() => new Map());
-
   // ID of thread to scroll to after the next render. If the thread is not
   // present, the value persists until it can be "consumed".
   const [scrollToId, setScrollToId] = useState<string | null>(null);
-
-  const topLevelThreads = threads;
-
-  const { offscreenLowerHeight, offscreenUpperHeight, visibleThreads } =
-    useMemo(
-      () =>
-        calculateVisibleThreads(
-          topLevelThreads,
-          threadHeights,
-          scrollPosition,
-          scrollContainerHeight
-        ),
-      [topLevelThreads, threadHeights, scrollPosition, scrollContainerHeight]
-    );
 
   const store = useSidebarStore();
 
@@ -134,68 +57,15 @@ export default function ThreadList({ threads }: ThreadListProps) {
       return;
     }
 
-    const threadIndex = topLevelThreads.findIndex(t => t.id === scrollToId);
-    if (threadIndex === -1) {
-      // Thread is not currently present. The `scrollToId` will be consumed
-      // when this thread appears.
-      return;
+    const element = document.getElementById(scrollToId);
+    if (element) {
+      element.scrollIntoView();
     }
-
-    // Clear `scrollToId` so we don't scroll again after the next render.
-    setScrollToId(null);
-
-    const getThreadHeight = (thread: Thread) =>
-      threadHeights.get(thread.id) || THREAD_DIMENSION_DEFAULTS.defaultHeight;
-
-    const yOffset = topLevelThreads
-      .slice(0, threadIndex)
-      .reduce((total, thread) => total + getThreadHeight(thread), 0);
-
-    const scrollContainer = getScrollContainer();
-    scrollContainer.scrollTop = yOffset;
-  }, [scrollToId, topLevelThreads, threadHeights]);
-
-  // When the set of visible threads changes, recalculate the real rendered
-  // heights of thread cards and update `threadHeights` state if there are changes.
-  useEffect(() => {
-    setThreadHeights(prevHeights => {
-      const changedHeights = new Map();
-      for (const { id } of visibleThreads) {
-        const threadElement = document.getElementById(id)!;
-
-        if (!threadElement) {
-          // This could happen if the `ThreadList` DOM is not connected to the document.
-          //
-          // Errors earlier in the render can also potentially cause this (see
-          // https://github.com/hypothesis/client/pull/3665#issuecomment-895857072),
-          // although we don't in general try to make all effects robust to that
-          // as it is a problem that needs to be handled elsewhere.
-          console.warn(
-            'ThreadList could not measure thread. Element not found.'
-          );
-          return prevHeights;
-        }
-
-        const height = getElementHeightWithMargins(threadElement);
-        if (height !== prevHeights.get(id)) {
-          changedHeights.set(id, height);
-        }
-      }
-
-      // Skip update if no heights changed from previous measured values
-      // (or defaults).
-      if (changedHeights.size === 0) {
-        return prevHeights;
-      }
-
-      return new Map([...prevHeights, ...changedHeights]);
-    });
-  }, [visibleThreads]);
+  }, [scrollToId]);
 
   return (
     <div>
-      <div style={{ height: offscreenUpperHeight }} />
-      {visibleThreads.map(child => (
+      {threads.map(child => (
         <div
           className={classnames(
             // The goal is to space out each annotation card vertically. Typically
@@ -209,10 +79,11 @@ export default function ThreadList({ threads }: ThreadListProps) {
           id={child.id}
           key={child.id}
         >
-          <ThreadCard thread={child} />
+          <ReplaceWhenOffscreen defaultHeight={200}>
+            <ThreadCard thread={child} />
+          </ReplaceWhenOffscreen>
         </div>
       ))}
-      <div style={{ height: offscreenLowerHeight }} />
     </div>
   );
 }
