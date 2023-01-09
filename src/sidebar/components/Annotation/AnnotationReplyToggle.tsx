e1e8272f4f0ade4e255ac46ec98dbf0179cf8aa6
Lyza Danger Gardner <lyza@lyza.com>
2023-01-09 12:12:01 -0500

    Convert AnnotationReplyToggle to TS

--- src/sidebar/components/Annotation/AnnotationReplyToggle.tsx
+++ src/sidebar/components/Annotation/AnnotationReplyToggle.tsx
@@ -1,23 +1,19 @@
 import { LinkButton } from '@hypothesis/frontend-shared';
 
-/**
- * @typedef AnnotationReplyToggleProps
- * @prop {() => void} onToggleReplies
- * @prop {number} replyCount
- * @prop {boolean} threadIsCollapsed
- */
-
+export type AnnotationReplyToggleProps = {
+  onToggleReplies: () => void;
+  replyCount: number;
+  threadIsCollapsed: boolean;
+};
 /**
  * Render a thread-card control to toggle (expand or collapse) all of this
  * thread's children.
- *
- * @param {AnnotationReplyToggleProps} props
  */
 function AnnotationReplyToggle({
   onToggleReplies,
   replyCount,
   threadIsCollapsed,
-}) {
+}: AnnotationReplyToggleProps) {
   const toggleAction = threadIsCollapsed ? 'Show replies' : 'Hide replies';
   const toggleText = `${toggleAction} (${replyCount})`;
 
