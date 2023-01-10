12401303527307774da81586bcbc560a00f304f1
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-01-10 09:43:37 +0100

    Migrated AnnotationUser to TS

--- src/sidebar/components/Annotation/AnnotationUser.tsx
+++ src/sidebar/components/Annotation/AnnotationUser.tsx
@@ -1,18 +1,15 @@
 import { LinkBase } from '@hypothesis/frontend-shared/lib/next';
 
-/**
- * @typedef AnnotationUserProps
- * @prop {string} [authorLink]
- * @prop {string} displayName
- */
+type AnnotationUserProps = {
+  authorLink?: string;
+  displayName: string;
+};
 
 /**
  * Display information about an annotation's user. Link to the user's
  * activity if `authorLink` is present.
- *
- * @param {AnnotationUserProps} props
  */
-function AnnotationUser({ authorLink, displayName }) {
+function AnnotationUser({ authorLink, displayName }: AnnotationUserProps) {
   const user = <h3 className="text-color-text font-bold">{displayName}</h3>;
 
   if (authorLink) {
