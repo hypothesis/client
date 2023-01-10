0a526ed41962e93a37c7a754a723fd94c8c3cea1
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-01-10 10:34:58 +0100

    Migrate MenuArrow to TS

--- src/sidebar/components/MenuArrow.tsx
+++ src/sidebar/components/MenuArrow.tsx
@@ -4,11 +4,10 @@ import {
 } from '@hypothesis/frontend-shared/lib/next';
 import classnames from 'classnames';
 
-/**
- * @typedef MenuArrowProps
- * @prop {string} [classes]
- * @prop {'up'|'down'} [direction]
- */
+type MenuArrowProps = {
+  classes?: string;
+  direction?: 'up' | 'down';
+};
 
 /**
  * Render a white-filled "pointer" arrow for use in menus and menu-like
@@ -16,11 +15,12 @@ import classnames from 'classnames';
  *
  * This will set up absolute positioning for this arrow, but the vertical and
  * horizontal positioning will need to be tuned in the component using the
- * arrow by adding additional utlity classes (`classes` prop here).
- *
- * @param {MenuArrowProps} props
+ * arrow by adding additional utility classes (`classes` prop here).
  */
-export default function MenuArrow({ classes, direction = 'up' }) {
+export default function MenuArrow({
+  classes,
+  direction = 'up',
+}: MenuArrowProps) {
   const Icon = direction === 'up' ? PointerUpIcon : PointerDownIcon;
   return (
     <Icon
