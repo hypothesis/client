69f1ca28d1ca8118661458afbaeaf208df59605d
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-01-13 09:25:37 +0100

    Add alert role to WarningBanner

--- src/annotator/components/WarningBanner.js
+++ src/annotator/components/WarningBanner.js
@@ -7,7 +7,7 @@ import classnames from 'classnames';
  */
 export default function WarningBanner() {
   return (
-    <div className="bg-white">
+    <div className="bg-white" role="alert">
       <div
         className={classnames(
           'flex items-center gap-x-2',
