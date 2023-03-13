4babd7f1a8e9b6bcbb0896bcd8b66784cd3f48f2
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-03-13 16:31:00 +0100

    Migrate session to TypeScript

--- src/sidebar/helpers/session.ts
+++ src/sidebar/helpers/session.ts
@@ -1,9 +1,6 @@
+import type { Profile } from '../../types/api';
+import type { SidebarSettings } from '../../types/config';
 import { serviceConfig } from '../config/service-config';
-
-/**
- * @typedef {import('../../types/config').SidebarSettings} SidebarSettings
- * @typedef {import('../../types/api').Profile} Profile
- */
 
 /**
  * The following things must all be true for the tutorial component to auto-display
@@ -18,12 +15,15 @@ import { serviceConfig } from '../config/service-config';
  *   dismissed by this user yet). This implies the presence of a profile, which
  *   in turn implies that there is an authenticated user.
  *
- * @param {boolean} isSidebar - is the app currently displayed in a sidebar?
- * @param {Profile} profile - User profile returned from the API
- * @param {SidebarSettings} settings
- * @return {boolean} - Tutorial panel should be displayed automatically
+ * @param isSidebar - is the app currently displayed in a sidebar?
+ * @param profile - User profile returned from the API
+ * @return Tutorial panel should be displayed automatically
  */
-export function shouldAutoDisplayTutorial(isSidebar, profile, settings) {
+export function shouldAutoDisplayTutorial(
+  isSidebar: boolean,
+  profile: Profile,
+  settings: SidebarSettings
+): boolean {
   const shouldShowBasedOnProfile =
     typeof profile.preferences === 'object' &&
     !!profile.preferences.show_sidebar_tutorial;
