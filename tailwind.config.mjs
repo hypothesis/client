d5f3335ae45b58b27387006321277a7553b75971
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-03-23 15:32:27 +0100

    Add src/shared/components path to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,7 @@ export default {
   content: [
     './src/sidebar/components/**/*.{js,ts,tsx}',
     './src/annotator/components/**/*.{js,ts,tsx}',
+    './src/shared/components/**/*.{js,ts,tsx}',
     './dev-server/ui-playground/components/**/*.{js,ts,tsx}',
     './node_modules/@hypothesis/frontend-shared/lib/**/*.{js,ts,tsx}',
     // This module references `sidebar-frame` and related classes
