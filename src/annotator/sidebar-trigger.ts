6974408d8e7cd5d452a44aa215a5168cb72157b5
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-03-28 14:36:16 +0200

    Migrate sidebar-trigger module to TypeScript

--- src/annotator/sidebar-trigger.ts
+++ src/annotator/sidebar-trigger.ts
@@ -4,10 +4,10 @@ const SIDEBAR_TRIGGER_BTN_ATTR = 'data-hypothesis-trigger';
  * Show the sidebar when user clicks on an element with the
  * trigger data attribute.
  *
- * @param {Element} rootEl - The DOM element which contains the trigger elements.
- * @param {() => void} showFn - Function which shows the sidebar.
+ * @param rootEl - The DOM element which contains the trigger elements.
+ * @param showFn - Function which shows the sidebar.
  */
-export function sidebarTrigger(rootEl, showFn) {
+export function sidebarTrigger(rootEl: Element, showFn: () => void) {
   const triggerElems = rootEl.querySelectorAll(
     '[' + SIDEBAR_TRIGGER_BTN_ATTR + ']'
   );
