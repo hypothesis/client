8de830816cbd806f1fbfe8bef965abc334180f75
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-04-21 12:51:20 +0200

    Migrate warn-once to TS

--- src/shared/warn-once.ts
+++ src/shared/warn-once.ts
@@ -1,5 +1,4 @@
-/** @type {Set<string>} */
-let shownWarnings = new Set();
+const shownWarnings = new Set<string>();
 
 /**
  * Log a warning if it has not already been reported.
@@ -7,12 +6,12 @@ let shownWarnings = new Set();
  * This is useful to avoid spamming the console if a warning is emitted in a
  * context that may be called frequently.
  *
- * @param {...unknown} args -
+ * @param args -
  *   Arguments to forward to `console.warn`. The arguments `toString()` values
  *   are concatenated into a string key which is used to determine if the warning
  *   has been logged before.
  */
-export function warnOnce(...args) {
+export function warnOnce(...args: unknown[]) {
   const key = args.join();
   if (shownWarnings.has(key)) {
     return;
