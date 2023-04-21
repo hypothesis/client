7e7bc6e41a76f9e552f51833d0b6e366a721bc93
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-04-21 12:46:23 +0200

    Migrate random to TS

--- src/shared/random.ts
+++ src/shared/random.ts
@@ -1,5 +1,4 @@
-/** @param {number} val */
-function byteToHex(val) {
+function byteToHex(val: number): string {
   const str = val.toString(16);
   return str.length === 1 ? '0' + str : str;
 }
@@ -7,10 +6,9 @@ function byteToHex(val) {
 /**
  * Generate a random hex string of `len` chars.
  *
- * @param {number} len - An even-numbered length string to generate.
- * @return {string}
+ * @param len - An even-numbered length string to generate.
  */
-export function generateHexString(len) {
+export function generateHexString(len: number): string {
   const bytes = new Uint8Array(len / 2);
   window.crypto.getRandomValues(bytes);
   return Array.from(bytes).map(byteToHex).join('');
