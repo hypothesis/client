47b7b17c5eeb74e54a915bbf4ee343d155e0bcb3
Robert Knight <robertknight@gmail.com>
2023-06-14 11:50:46 +0100

    Update wait-for-npm-release.sh after Yarn v3 update
     - Use `npm info` to get the latest published version. The previous yarn
       command now does something different.
    
     - Add log output to make it more obvious what is going on in CI logs
    
     - Add a 60s timeout, so the script doesn't end up running indefinitely
       in CI if something goes wrong

--- scripts/wait-for-npm-release.sh
+++ scripts/wait-for-npm-release.sh
@@ -6,9 +6,15 @@
 # This script is needed because a new release of a package is not always
 # immediately available after "npm publish" returns.
 #
-# Usage: wait-for-npm-release.sh [<dist-tag>]
+# Usage: wait-for-npm-release.sh [<dist-tag>] [<version>]
 #
 # <dist-tag> defaults to "latest".
+# <version> defaults to the "version" field from package.json.
+
+package_name=hypothesis
+timeout=60
+start_time=$(date +%s)
+end_time=$((start_time + timeout))
 
 if [ -z "$1" ]; then
   dist_tag=latest
@@ -16,12 +22,26 @@ else
   dist_tag=$1
 fi
 
-expected_version=$(node -p "require('./package.json').version")
-while [ true ]
+if [ -z "$2" ]; then
+  expected_version=$(node -p "require('./package.json').version")
+else
+  expected_version=$2
+fi
+
+echo "Waiting for $package_name@$expected_version to be published as \"$dist_tag\" on npm..."
+
+while true
 do
-  released_version=$(yarn info -s hypothesis dist-tags.$dist_tag)
+  released_version=$(npm info "$package_name" "dist-tags.$dist_tag")
   if [ "$released_version" = "$expected_version" ]; then
+    echo "Package version $expected_version found on npm."
     break
+  fi
+
+  current_time=$(date +%s)
+  if [ "$current_time" -ge "$end_time" ]; then
+    echo "Timeout reached. Package was not published after $timeout seconds."
+    exit 1
   fi
 
   sleep 1
