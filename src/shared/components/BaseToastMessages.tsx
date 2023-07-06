aced1fab1fe732b1c861f28fcf39ff022251f603
Lyza Danger Gardner <lyza@lyza.com>
2023-07-06 08:59:59 -0400

    Use `Callout` for toast-message UI and styling
    Replace custom use of `Card` with new shared `Callout` component for
    individual toast messages.

--- src/shared/components/BaseToastMessages.tsx
+++ src/shared/components/BaseToastMessages.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  Link,
-  CancelIcon,
-  CautionIcon,
-  CheckIcon,
-} from '@hypothesis/frontend-shared';
+import { Callout, Link } from '@hypothesis/frontend-shared';
 import classnames from 'classnames';
 
 export type ToastMessage = {
@@ -34,72 +28,40 @@ function ToastMessageItem({ message, onDismiss }: ToastMessageItemProps) {
       ? `${message.type.charAt(0).toUpperCase() + message.type.slice(1)}: `
       : '';
 
-  let Icon;
-  switch (message.type) {
-    case 'success':
-      Icon = CheckIcon;
-      break;
-    case 'error':
-      Icon = CancelIcon;
-      break;
-    case 'notice':
-    default:
-      Icon = CautionIcon;
-      break;
-  }
   /**
-   * a11y linting is disabled here: There is a click-to-remove handler on a
-   * non-interactive element. This allows sighted users to get the toast message
-   * out of their way if it interferes with interacting with the underlying
-   * components. This shouldn't pose the same irritation to users with screen-
-   * readers as the rendered toast messages shouldn't impede interacting with
-   * the underlying document.
+   * There is a click-to-remove handler on a non-interactive element. This
+   * allows sighted users to get the toast message out of their way if it
+   * interferes with interacting with the underlying components. This shouldn't
+   * pose the same irritation to users with screen- readers as the rendered
+   * toast messages shouldn't impede interacting with the underlying document.
    */
   return (
-    /* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
-    <Card
-      classes={classnames('flex', {
+    <Callout
+      classes={classnames({
         'sr-only': message.visuallyHidden,
-        'border-red-error': message.type === 'error',
-        'border-yellow-notice': message.type === 'notice',
-        'border-green-success': message.type === 'success',
       })}
+      status={message.type}
       onClick={() => onDismiss(message.id)}
+      variant="raised"
     >
-      <div
-        className={classnames('flex items-center p-3 text-white', {
-          'bg-red-error': message.type === 'error',
-          'bg-yellow-notice': message.type === 'notice',
-          'bg-green-success': message.type === 'success',
-        })}
-      >
-        <Icon
-          className={classnames(
-            // Adjust alignment of icon to appear more aligned with text
-            'mt-[2px]'
-          )}
-        />
-      </div>
-      <div className="grow p-3" data-testid="toast-message-text">
-        <strong>{prefix}</strong>
-        {message.message}
-        {message.moreInfoURL && (
-          <div className="text-right">
-            <Link
-              href={message.moreInfoURL}
-              onClick={
-                event =>
-                  event.stopPropagation() /* consume the event so that it does not dismiss the message */
-              }
-              target="_new"
-              underline="none"
-            >
-              More info
-            </Link>
-          </div>
-        )}
-      </div>
-    </Card>
+      <strong>{prefix}</strong>
+      {message.message}
+      {message.moreInfoURL && (
+        <div className="text-right">
+          <Link
+            href={message.moreInfoURL}
+            onClick={
+              event =>
+                event.stopPropagation() /* consume the event so that it does not dismiss the message */
+            }
+            target="_new"
+            underline="none"
+          >
+            More info
+          </Link>
+        </div>
+      )}
+    </Callout>
   );
 }
 
@@ -128,25 +90,22 @@ export default function BaseToastMessages({
       >
         {messages.map(message => (
           <li
-            className={classnames(
-              'relative w-full container hover:cursor-pointer',
-              {
-                // Add a bottom margin to visible messages only. Typically, we'd
-                // use a `space-y-2` class on the parent to space children.
-                // Doing that here could cause an undesired top margin on
-                // the first visible message in a list that contains (only)
-                // visually-hidden messages before it.
-                // See https://tailwindcss.com/docs/space#limitations
-                'mb-2': !message.visuallyHidden,
-                // Slide in from right in narrow viewports; fade in larger
-                // viewports to toast message isn't flying too far
-                'motion-safe:animate-slide-in-from-right lg:animate-fade-in':
-                  !message.isDismissed,
-                // Only ever fade in if motion-reduction is preferred
-                'motion-reduce:animate-fade-in': !message.isDismissed,
-                'animate-fade-out': message.isDismissed,
-              }
-            )}
+            className={classnames('relative w-full container', {
+              // Add a bottom margin to visible messages only. Typically, we'd
+              // use a `space-y-2` class on the parent to space children.
+              // Doing that here could cause an undesired top margin on
+              // the first visible message in a list that contains (only)
+              // visually-hidden messages before it.
+              // See https://tailwindcss.com/docs/space#limitations
+              'mb-2': !message.visuallyHidden,
+              // Slide in from right in narrow viewports; fade in larger
+              // viewports to toast message isn't flying too far
+              'motion-safe:animate-slide-in-from-right lg:animate-fade-in':
+                !message.isDismissed,
+              // Only ever fade in if motion-reduction is preferred
+              'motion-reduce:animate-fade-in': !message.isDismissed,
+              'animate-fade-out': message.isDismissed,
+            })}
             key={message.id}
           >
             <ToastMessageItem message={message} onDismiss={onMessageDismiss} />
