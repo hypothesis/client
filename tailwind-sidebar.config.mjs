e1765dad1de2754968d5228ddce0a3d9d2163b4c
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-07-18 12:34:08 +0200

    Fix toast message animations

--- tailwind-sidebar.config.mjs
+++ tailwind-sidebar.config.mjs
@@ -4,6 +4,7 @@ export default {
   presets: [tailwindConfig],
   content: [
     './src/sidebar/components/**/*.{js,ts,tsx}',
+    './src/shared/components/**/*.{js,ts,tsx}',
     './dev-server/ui-playground/components/**/*.{js,ts,tsx}',
     './node_modules/@hypothesis/frontend-shared/lib/**/*.{js,ts,tsx}',
   ],
