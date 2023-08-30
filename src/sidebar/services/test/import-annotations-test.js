ab8ec27df69b427a9c249d45c5c9ce82b6c91ba5
Robert Knight <robertknight@gmail.com>
2023-08-30 12:40:34 +0100

    Include some error details in toast shown when an import fails
    This will make triaging any user reports of issues easier. It would look better
    if we could present the details in a separate dialog, but this is a starting
    point.
    
    Also disable auto-dismiss for non-success statuses so the user is less likely to
    miss them.
    
    Part of https://github.com/hypothesis/client/issues/5741

--- src/sidebar/services/test/import-annotations-test.js
+++ src/sidebar/services/test/import-annotations-test.js
@@ -213,7 +213,7 @@ describe('ImportAnnotationsService', () => {
 
       assert.calledWith(
         fakeToastMessenger.notice,
-        '1 annotations imported, 1 imports failed',
+        '1 annotations imported, 1 imports failed (Oh no)',
       );
     });
 
@@ -223,7 +223,10 @@ describe('ImportAnnotationsService', () => {
 
       await svc.import([generateAnnotation(), generateAnnotation()]);
 
-      assert.calledWith(fakeToastMessenger.error, '2 imports failed');
+      assert.calledWith(
+        fakeToastMessenger.error,
+        '2 imports failed (Something went wrong)',
+      );
     });
 
     it('throws an error if called when user is logged out', async () => {
