45fe9d7f56d0d8de3b8e24e908dc59a19140d397
Robert Knight <robertknight@gmail.com>
2023-09-01 12:48:40 +0100

    Convert remaining modules in src/types to TypeScript

--- src/types/sidebar.ts
+++ src/types/sidebar.ts
@@ -4,16 +4,11 @@
 
 /**
  * Defined panel components available in the sidebar.
- *
- * @typedef {'help'|'loginPrompt'|'shareGroupAnnotations'} PanelName
  */
+export type PanelName = 'help' | 'loginPrompt' | 'shareGroupAnnotations';
 
 /**
  * The top-level tabs in the sidebar interface. Used to reference which tab
  * is currently selected (active/visible).
- *
- * @typedef {'annotation'|'note'|'orphan'} TabName
  */
-
-// Make TypeScript treat this file as a module.
-export const unused = {};
+export type TabName = 'annotation' | 'note' | 'orphan';
