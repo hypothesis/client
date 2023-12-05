8bf9a5e09634092b4eda3a15f913cbd1873cd38c
Robert Knight <robertknight@gmail.com>
2023-12-05 09:55:19 +0000

    Reduce `pageLabelInRange` nesting by using an early return

--- src/sidebar/util/page-range.ts
+++ src/sidebar/util/page-range.ts
@@ -8,29 +8,29 @@
  *   specify an empty range.
  */
 export function pageLabelInRange(label: string, range: string): boolean {
-  if (range.includes('-')) {
-    let [start, end] = range.split('-');
-    if (!start) {
-      start = label;
-    }
-    if (!end) {
-      end = label;
-    }
-    const [startInt, endInt, labelInt] = [
-      parseInt(start),
-      parseInt(end),
-      parseInt(label),
-    ];
-    if (
-      Number.isInteger(startInt) &&
-      Number.isInteger(endInt) &&
-      Number.isInteger(labelInt)
-    ) {
-      return labelInt >= startInt && labelInt <= endInt;
-    } else {
-      return false;
-    }
-  } else {
+  if (!range.includes('-')) {
     return label === range;
+  }
+
+  let [start, end] = range.split('-');
+  if (!start) {
+    start = label;
+  }
+  if (!end) {
+    end = label;
+  }
+  const [startInt, endInt, labelInt] = [
+    parseInt(start),
+    parseInt(end),
+    parseInt(label),
+  ];
+  if (
+    Number.isInteger(startInt) &&
+    Number.isInteger(endInt) &&
+    Number.isInteger(labelInt)
+  ) {
+    return labelInt >= startInt && labelInt <= endInt;
+  } else {
+    return false;
   }
 }
