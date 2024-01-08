4f1adf0abd9ecf95b9a05e100165e85289975b6c
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-01-08 16:32:51 +0100

    Update to chai 5 and remove karma-chai and karma-sinon

--- src/sidebar/test/bootstrap.js
+++ src/sidebar/test/bootstrap.js
@@ -1,14 +1,19 @@
+import { assert } from 'chai';
 import { configure } from 'enzyme';
 import { Adapter } from 'enzyme-adapter-preact-pure';
 import 'preact/debug';
+import sinon from 'sinon';
 
 import { patch } from '../../test-util/assert-methods';
 
-// Expose the sinon assertions.
+// Expose the sinon assertions and add extra assert methods.
 sinon.assert.expose(assert, { prefix: null });
-
-// Add extra assert methods
 patch(assert);
+
+// Expose these globally, to avoid dependency on outdated karma-chai and
+// karma-sinon plugins
+globalThis.assert = assert;
+globalThis.sinon = sinon;
 
 // Configure Enzyme for UI tests.
 configure({ adapter: new Adapter() });
