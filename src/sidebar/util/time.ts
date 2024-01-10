f192a5961636799bef896f0400308797d2e07831
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-01-10 11:30:15 +0100

    Create function to format dates in a standard sortable format

--- src/sidebar/util/time.ts
+++ src/sidebar/util/time.ts
@@ -262,3 +262,16 @@ export function formatDate(date: Date, Intl?: IntlType): string {
     Intl,
   );
 }
+
+/**
+ * Formats a date as `YYYY-MM-DD hh:mm`, using 24h and system timezone.
+ */
+export function formatDateTime(date: Date): string {
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const day = `${date.getDate()}`.padStart(2, '0');
+  const hours = `${date.getHours()}`.padStart(2, '0');
+  const minutes = `${date.getMinutes()}`.padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
