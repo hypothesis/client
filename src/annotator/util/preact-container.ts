a13024c2cd2bbdbf74edb92b3d4b0304a14c7019
Robert Knight <robertknight@gmail.com>
2024-02-01 12:29:53 +0000

    Add `PreactContainer` utility and refactor adder/bucket-bar to use it
    The various top-level Hypothesis UI elements in the annotator have a
    common structure where:
    
     - There is a `<hypothesis-{name}>` container element
    
     - The container has a shadow root with the annotator stylesheet loaded
       into it
    
     - Preact is used to render the contents of the element, into the shadow
       root.
    
     - There is a controller which wraps the container element and has
       methods to read/write the displayed data. When the data is changed,
       the Preact tree is re-rendered.
    
    Add a `PreactContainer` utility which implements common parts of this pattern,
    and refactor two of the top-level UI controls (bucket bar and adder) to use it.

---
+++ src/annotator/util/preact-container.ts
@@ -0,0 +1,55 @@
+import type { JSX } from 'preact';
+import { render } from 'preact';
+
+import type { Destroyable } from '../../types/annotator';
+import { createShadowRoot } from './shadow-root';
+
+/**
+ * Manages the root `<hypothesis-*>` container for a top-level Hypothesis UI
+ * element.
+ *
+ * This implements common functionality for these elements, such as:
+ *
+ *  - Creating the `<hypothesis-{name}>` element with a shadow root, and loading
+ *    stylesheets into it.
+ *  - Re-rendering the Preact component tree when {@link PreactContainer.render} is called.
+ *  - Unmounting the component and removing the container element when
+ *    {@link PreactContainer.destroy} is called
+ */
+export class PreactContainer implements Destroyable {
+  private _element: HTMLElement;
+  private _shadowRoot: ShadowRoot;
+  private _render: () => JSX.Element;
+
+  /**
+   * Create a new `<hypothesis-{name}>` container element.
+   *
+   * After constructing the container, {@link PreactContainer.render} should be
+   * called to perform the initial render.
+   *
+   * @param name - Suffix for the element
+   * @param render - Callback that renders the root JSX element for this container
+   */
+  constructor(name: string, render: () => JSX.Element) {
+    const tag = `hypothesis-${name}`;
+    this._element = document.createElement(tag);
+    this._shadowRoot = createShadowRoot(this._element);
+    this._render = render;
+  }
+
+  /** Unmount the Preact component and remove the container element from the DOM. */
+  destroy() {
+    render(null, this._shadowRoot);
+    this._element.remove();
+  }
+
+  /** Return a reference to the container element. */
+  get element(): HTMLElement {
+    return this._element;
+  }
+
+  /** Re-render the root Preact component. */
+  render() {
+    render(this._render(), this._shadowRoot);
+  }
+}
