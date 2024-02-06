b58ceba958c7d19d5d1eee0c44fe95f034cae44d
Robert Knight <robertknight@gmail.com>
2024-02-06 11:42:52 +0000

    Add `pageRangesOverlap` utility
    This will be used for testing whether an EPUB chapter's page range ovelaps a
    page range focus filter.

--- src/sidebar/util/test/page-range-test.js
+++ src/sidebar/util/test/page-range-test.js
@@ -1,4 +1,4 @@
-import { pageLabelInRange } from '../page-range';
+import { pageLabelInRange, pageRangesOverlap } from '../page-range';
 
 describe('pageLabelInRange', () => {
   [
@@ -91,6 +91,83 @@ describe('pageLabelInRange', () => {
   ].forEach(({ label, range, match }) => {
     it('returns true if the label is in the page range', () => {
       assert.equal(pageLabelInRange(label, range), match);
+    });
+  });
+});
+
+describe('pageRangesOverlap', () => {
+  [
+    // Matching single page
+    {
+      rangeA: '1',
+      rangeB: '1',
+      overlap: true,
+    },
+    // Non-matching single page
+    {
+      rangeA: '1',
+      rangeB: '2',
+      overlap: false,
+    },
+    // Overlapping numeric ranges
+    {
+      rangeA: '1-2',
+      rangeB: '2-4',
+      overlap: true,
+    },
+    {
+      rangeA: '2-4',
+      rangeB: '1-2',
+      overlap: true,
+    },
+    // Inverted numeric ranges. These are implicitly normalized.
+    {
+      rangeA: '2-1',
+      rangeB: '4-2',
+      overlap: true,
+    },
+    // Half-open ranges
+    {
+      rangeA: '1-',
+      rangeB: '-3',
+      overlap: true,
+    },
+    // Non-overlapping numeric ranges
+    {
+      rangeA: '1-2',
+      rangeB: '3-4',
+      overlap: false,
+    },
+    {
+      rangeA: '3-4',
+      rangeB: '1-2',
+      overlap: false,
+    },
+    // Relation is undefined if either of the ranges is non-numeric.
+    {
+      rangeA: 'ii',
+      rangeB: '3-4',
+      overlap: null,
+    },
+    {
+      rangeA: 'i-iii',
+      rangeB: 'iii-iv',
+      overlap: null,
+    },
+    {
+      rangeA: '1-ii',
+      rangeB: 'ii-3',
+      overlap: null,
+    },
+    // As a special case, matching non-numeric ranges overlap.
+    {
+      rangeA: 'ii',
+      rangeB: 'ii',
+      overlap: true,
+    },
+  ].forEach(({ rangeA, rangeB, overlap }) => {
+    it('returns true if the two ranges overlap', () => {
+      assert.strictEqual(pageRangesOverlap(rangeA, rangeB), overlap);
     });
   });
 });
