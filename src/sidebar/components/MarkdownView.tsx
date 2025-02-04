7bca966859b7bb9608ce2d9716de69653b8582b4
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-02-04 10:34:56 +0100

    Add basic popover for at-mentions

--- src/sidebar/components/MarkdownView.tsx
+++ src/sidebar/components/MarkdownView.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useRef } from 'preact/hooks';
+import { ListenerCollection, Popover } from '@hypothesis/frontend-shared';
+import { useEffect, useMemo, useRef, useState } from 'preact/hooks';
 
 import type { Mention } from '../../types/api';
 import { replaceLinksWithEmbeds } from '../media-embedder';
@@ -13,10 +14,16 @@ export type MarkdownViewProps = {
   mentions?: Mention[];
 };
 
-function replaceMentionTags(element: HTMLElement, mentions: Mention[] = []) {
+function replaceMentionTags(
+  element: HTMLElement,
+  mentions: Mention[] = [],
+): Array<[Mention, HTMLElement]> {
   const mentionLinks = element.querySelectorAll('a[data-hyp-mention]');
+  const foundMentions: Array<[Mention, HTMLElement]> = [];
+
   for (const mentionLink of mentionLinks) {
-    const mentionUserId = (mentionLink as HTMLElement).dataset.userid;
+    const htmlMentionLink = mentionLink as HTMLElement;
+    const mentionUserId = htmlMentionLink.dataset.userid;
     const mention =
       mentionUserId && mentions.find(m => m.userid === mentionUserId);
 
@@ -25,6 +32,8 @@ function replaceMentionTags(element: HTMLElement, mentions: Mention[] = []) {
       // the user profile
       mentionLink.setAttribute('href', mention.link);
       mentionLink.setAttribute('target', '_blank');
+
+      foundMentions.push([mention, htmlMentionLink]);
     } else {
       // If it doesn't, convert it to "plain text"
       const plainTextMention = document.createElement('span');
@@ -34,6 +43,8 @@ function replaceMentionTags(element: HTMLElement, mentions: Mention[] = []) {
       mentionLink.parentElement?.replaceChild(plainTextMention, mentionLink);
     }
   }
+
+  return foundMentions;
 }
 
 /**
@@ -52,6 +63,10 @@ export default function MarkdownView({
   );
   const content = useRef<HTMLDivElement | null>(null);
 
+  const hasMentions = !!mentions?.length;
+  const [activeMention, setActiveMention] = useState<Mention | null>(null);
+  const mentionsPopoverAnchorRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
     replaceLinksWithEmbeds(content.current!, {
       // Make embeds the full width of the sidebar, unless the sidebar has been
@@ -62,7 +77,21 @@ export default function MarkdownView({
   }, [markdown]);
 
   useEffect(() => {
-    replaceMentionTags(content.current!, mentions);
+    const listenerCollection = new ListenerCollection();
+    const foundMentions = replaceMentionTags(content.current!, mentions);
+
+    for (const [mention, element] of foundMentions) {
+      listenerCollection.add(element, 'mouseenter', () => {
+        setActiveMention(mention);
+        mentionsPopoverAnchorRef.current = element;
+      });
+      listenerCollection.add(element, 'mouseleave', () => {
+        setActiveMention(null);
+        mentionsPopoverAnchorRef.current = null;
+      });
+    }
+
+    return () => listenerCollection.removeAll();
   }, [html, mentions]);
 
   // NB: The following could be implemented by setting attribute props directly
@@ -72,7 +101,7 @@ export default function MarkdownView({
   // a review in the future.
   return (
     <div className="w-full break-anywhere cursor-text">
-      <StyledText>
+      <StyledText classes="relative">
         <div
           className={classes}
           data-testid="markdown-text"
@@ -80,6 +109,16 @@ export default function MarkdownView({
           dangerouslySetInnerHTML={{ __html: html }}
           style={style}
         />
+        {hasMentions && (
+          <Popover
+            open={!!activeMention}
+            onClose={() => setActiveMention(null)}
+            anchorElementRef={mentionsPopoverAnchorRef}
+            classes="p-2"
+          >
+            {activeMention?.display_name ?? activeMention?.username}
+          </Popover>
+        )}
       </StyledText>
     </div>
   );
