d54397cd6f4a7a0223800be72c99c1fd780e33d3
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-04-25 14:32:57 +0200

    Add vitest eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
+import vitest from '@vitest/eslint-plugin';
 import hypothesisBase from 'eslint-config-hypothesis/base';
 import hypothesisJSX from 'eslint-config-hypothesis/jsx';
 import hypothesisTS from 'eslint-config-hypothesis/ts';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 
-export default defineConfig([
+export default defineConfig(
   globalIgnores([
     '.tox/**/*',
     '.yalc/**/*',
@@ -53,4 +54,14 @@ export default defineConfig([
       },
     },
   },
-]);
+
+  // Tests
+  {
+    files: ['**/*-test.js'],
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
+  },
+);
