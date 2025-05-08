hypothesis/client

commit 076594f85f09f4bd0c41d9064af266fdd7ffe058

Use common container for sidebar buckets and toolbar

src/annotator/sidebar.tsx
@@ -164,6 +164,25 @@ export class Sidebar implements Destroyable {
     this.bucketBar = null;
     this.features = new FeatureFlags();
 
+    const iframeContainerId = 'sidebar-container';
+
+    // Set up the toolbar on the left edge of the sidebar.
+    const toolbarContainer = document.createElement('div');
+    toolbarContainer.setAttribute('data-testid', 'toolbar-container');
+    this.toolbar = new ToolbarController(toolbarContainer, {
+      sidebarContainerId: iframeContainerId,
+      createAnnotation: (tool: AnnotationTool) => {
+        if (this._guestRPC.length === 0) {
+          return;
+        }
+
+        const rpc = this._guestWithSelection ?? this._guestRPC[0];
+        rpc.call('createAnnotation', { tool });
+      },
+      setSidebarOpen: open => (open ? this.open() : this.close()),
+      setHighlightsVisible: show => this.setHighlightsVisible(show),
+    });
+
     if (config.externalContainerSelector) {
       this.externalFrame =
         document.querySelector(config.externalContainerSelector) ?? element;
@@ -172,10 +191,14 @@ export class Sidebar implements Destroyable {
       this.iframeContainer = document.createElement('div');
       this.iframeContainer.style.display = 'none';
       this.iframeContainer.className = 'sidebar-container';
-      this.iframeContainer.id = 'sidebar-container';
+      this.iframeContainer.id = iframeContainerId;
 
       if (config.theme === 'clean') {
         this.iframeContainer.classList.add('theme-clean');
+        // Append toolbar directly to the iframe container when clean theme is
+        // enabled
+        this.iframeContainer.append(toolbarContainer);
+        this.toolbar.useMinimalControls = true;
       } else {
         let bucketBarContainer: HTMLElement | undefined;
         if (config.bucketContainerSelector) {
@@ -200,11 +223,10 @@ export class Sidebar implements Destroyable {
           // See https://github.com/hypothesis/client/pull/2750.
           'absolute top-0 bottom-0 w-[23px] left-[-22px]',
 
-          // Make the bucket bar fill the container, with large padding on top
-          // so that buckets are below the toolbar, and small padding on the
+          // Make the bucket bar fill the container, with small padding on the
           // right to align the right edge of the buckets with the right edge
           // of toolbar icons.
-          'flex flex-col pt-[110px] pr-[5px]',
+          'flex flex-col pr-[5px]',
 
           // Use a grey background, with lower opacity with the sidebar is
           // collapsed, so the page content behind it can be read.
@@ -214,6 +236,11 @@ export class Sidebar implements Destroyable {
           // (eg. scroll bar thumbs) which are behind it.
           'pointer-events-none',
         );
+
+        // Allow pointer events in the toolbar
+        toolbarContainer.className = 'pointer-events-auto';
+        sidebarEdge.append(toolbarContainer);
+
         this.iframeContainer.append(sidebarEdge);
 
         if (!bucketBarContainer) {
@@ -256,31 +283,9 @@ export class Sidebar implements Destroyable {
 
     this._listeners = new ListenerCollection();
 
-    // Set up the toolbar on the left edge of the sidebar.
-    const toolbarContainer = document.createElement('div');
-    this.toolbar = new ToolbarController(toolbarContainer, {
-      sidebarContainerId: this.iframeContainer?.id,
-      createAnnotation: (tool: AnnotationTool) => {
-        if (this._guestRPC.length === 0) {
-          return;
-        }
-
-        const rpc = this._guestWithSelection ?? this._guestRPC[0];
-        rpc.call('createAnnotation', { tool });
-      },
-      setSidebarOpen: open => (open ? this.open() : this.close()),
-      setHighlightsVisible: show => this.setHighlightsVisible(show),
-    });
-
-    if (config.theme === 'clean') {
-      this.toolbar.useMinimalControls = true;
-    } else {
-      this.toolbar.useMinimalControls = false;
-    }
-
     if (this.iframeContainer) {
-      // If using our own container frame for the sidebar, add the toolbar to it.
-      this.iframeContainer.prepend(toolbarContainer);
+      // If using our own container frame for the sidebar, infer the width from
+      // it.
       this._toolbarWidth = this.toolbar.getWidth();
     } else {
       // If using a host-page provided container for the sidebar, the toolbar is

src/annotator/components/Toolbar.tsx
@@ -146,7 +146,19 @@ export default function Toolbar({
   return (
     <div
       className={classnames(
-        'absolute left-[-33px] w-[33px] z-2',
+        {
+          // For minimal controls, display the toolbar to the left, fully
+          // outside the sidebar
+          'absolute right-full': useMinimalControls,
+          // When the full toolbar is displayed, we position it relative to its
+          // container, so that it takes vertical space and pushes next elements
+          // down (eg. the buckets list).
+          // The toolbar is wider than its parent, so we need to adjust the
+          // right position so that the right edge of the toolbar aligns with
+          // the right edge of the parent.
+          'relative right-[11px]': !useMinimalControls,
+        },
+        'w-[33px] z-2',
         'text-px-base leading-none', // non-scaling sizing
       )}
     >

src/annotator/util/buckets.ts
@@ -191,7 +191,7 @@ export function computeBuckets(
   // Add an upper "navigation" bucket with offscreen-above anchors
   const above: Bucket = {
     anchors: aboveAnchors,
-    position: vMargin,
+    position: vMargin - 5,
   };
 
   // Add a lower "navigation" bucket with offscreen-below anchors

commit e23af314dbd2bc27f3144ca9807fc1a0b3043376

Fix sidebar closing after drag-resize
Fix an issue where the sidebar would close after clicking and dragging
the sidebar toggle button in the toolbar.

Prior to 076594f85f09f4bd0c41d9064af266fdd7ffe058 the toolbar button was
prevented from receiving a "click" event after the drag ended by applying
`pointer-events: none` to the iframe container, an ancestor of the toolbar,
during the drag. 076594f85f09f4bd0c41d9064af266fdd7ffe058 broke this by
applying `pointer-events: auto` directly to the toolbar container, overriding
the `pointer-events` style from the parent elements. The fix is to apply the
`pointer-events` style change to the toolbar container directly during the drag.

Fixes https://github.com/hypothesis/client/issues/7058

src/annotator/sidebar.tsx
@@ -637,8 +637,9 @@ export class Sidebar implements Destroyable {
         // Disable animated transition of sidebar position
         frame.classList.add('sidebar-no-transition');
 
-        // Disable pointer events on the iframe.
-        frame.style.pointerEvents = 'none';
+        // Disable pointer events on the toolbar, so toolbar buttons don't
+        // receive clicks when the drag ends.
+        this.toolbar.container.style.pointerEvents = 'none';
 
         this._dragResizeState.initial = parseInt(
           getComputedStyle(frame).marginLeft,
@@ -648,8 +649,8 @@ export class Sidebar implements Destroyable {
       case 'dragend':
         frame.classList.remove('sidebar-no-transition');
 
-        // Re-enable pointer events on the iframe.
-        frame.style.pointerEvents = '';
+        // Re-enable pointer events on the toolbar.
+        this.toolbar.container.style.pointerEvents = '';
 
         // Snap open or closed.
         if (

src/annotator/toolbar.tsx
@@ -19,7 +19,8 @@ export type ToolbarOptions = {
  * highlight visibility etc.
  */
 export class ToolbarController {
-  private _container: HTMLElement;
+  container: HTMLElement;
+
   private _activeTool: AnnotationTool | null;
   private _newAnnotationType: 'annotation' | 'note';
   private _useMinimalControls: boolean;
@@ -39,7 +40,7 @@ export class ToolbarController {
   constructor(container: HTMLElement, options: ToolbarOptions) {
     const { createAnnotation, setSidebarOpen, setHighlightsVisible } = options;
 
-    this._container = container;
+    this.container = container;
     this._activeTool = null;
     this._useMinimalControls = false;
     this._newAnnotationType = 'note';
@@ -72,7 +73,7 @@ export class ToolbarController {
   }
 
   getWidth() {
-    const content = this._container.firstChild as HTMLElement;
+    const content = this.container.firstChild as HTMLElement;
     return content.getBoundingClientRect().width;
   }
 
@@ -172,7 +173,7 @@ export class ToolbarController {
         toggleSidebarRef={this._sidebarToggleButton}
         useMinimalControls={this.useMinimalControls}
       />,
-      this._container,
+      this.container,
     );
   }
 }

src/annotator/test/sidebar-test.js
@@ -165,6 +165,7 @@ describe('Sidebar', () => {
     const toggleButton = document.createElement('button');
     fakeToolbar = {
       activeTool: null,
+      container: document.createElement('div'),
       getWidth: sinon.stub().returns(100),
       useMinimalControls: false,
       sidebarOpen: false,
@@ -691,7 +692,7 @@ describe('Sidebar', () => {
         assert.isTrue(
           sidebar.iframeContainer.classList.contains('sidebar-no-transition'),
         );
-        assert.equal(sidebar.iframeContainer.style.pointerEvents, 'none');
+        assert.equal(fakeToolbar.container.style.pointerEvents, 'none');
       });
     });
 
@@ -708,7 +709,7 @@ describe('Sidebar', () => {
         assert.isFalse(
           sidebar.iframeContainer.classList.contains('sidebar-no-transition'),
         );
-        assert.equal(sidebar.iframeContainer.style.pointerEvents, '');
+        assert.equal(fakeToolbar.container.style.pointerEvents, '');
       });
 
       it('opens sidebar if final width is above threshold', () => {