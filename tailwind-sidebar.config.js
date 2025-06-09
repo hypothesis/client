f656e8664886f05a7f0f44ddc0e0102d09834c18
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-06-09 14:39:51 +0200

    Add annotation-ui to tailwind content config

--- tailwind-sidebar.config.js
+++ tailwind-sidebar.config.js
@@ -6,6 +6,7 @@ export default {
     './src/sidebar/components/**/*.{js,ts,tsx}',
     './src/shared/components/**/*.{js,ts,tsx}',
     './dev-server/ui-playground/components/**/*.{js,ts,tsx}',
+    './node_modules/@hypothesis/annotation-ui/lib/**/*.{js,ts,tsx}',
     './node_modules/@hypothesis/frontend-shared/lib/**/*.{js,ts,tsx}',
   ],
 };
