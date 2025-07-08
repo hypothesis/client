2dc20ec4d05e3dc098eddd9af5c9e2836b1d14b4
Robert Knight <robertknight@gmail.com>
2025-07-08 12:42:33 +0100

    Fix share action being shown incorrectly in LMS app
    This is a partial revert of https://github.com/hypothesis/client/pull/7186.
    Prior to that PR the annotation share control was hidden in two cases:
    
     1. Sharing is globally disabled for the client. This is true in the LMS.
     2. The annotation does not have a share URL
    
    Case (2) is handled by the `AnnotationShareControl` component in the
    @hypothesis/annotation-ui package, but the checks for case (1) were removed and
    that still needs to be handled in the Hypothesis client.
    
    Fixes https://github.com/hypothesis/support/issues/199

--- src/sidebar/helpers/test/annotation-sharing-test.js
+++ src/sidebar/helpers/test/annotation-sharing-test.js
@@ -1,6 +1,22 @@
 import * as sharingUtil from '../annotation-sharing';
 
 describe('sidebar/helpers/annotation-sharing', () => {
+  let fakeServiceConfig;
+  let fakeServiceSettings;
+
+  beforeEach(() => {
+    fakeServiceSettings = {};
+    fakeServiceConfig = sinon.stub().returns(fakeServiceSettings);
+
+    sharingUtil.$imports.$mock({
+      '../config/service-config': { serviceConfig: fakeServiceConfig },
+    });
+  });
+
+  afterEach(() => {
+    sharingUtil.$imports.$restore();
+  });
+
   describe('pageSharingLink', () => {
     it('generates a bouncer link based on the document URI and group id', () => {
       assert.equal(
@@ -43,6 +59,28 @@ describe('sidebar/helpers/annotation-sharing', () => {
       it('returns false for any URL not beginning with http or https', () => {
         assert.isFalse(sharingUtil.isShareableURI(invalidURI));
       });
+    });
+  });
+
+  describe('sharingEnabled', () => {
+    it('returns true if no service settings present', () => {
+      fakeServiceConfig.returns(null);
+      assert.isTrue(sharingUtil.sharingEnabled({}));
+    });
+
+    it('returns true if service settings do not have a `enableShareLinks` prop', () => {
+      // service config is an empty object
+      assert.isTrue(sharingUtil.sharingEnabled({}));
+    });
+
+    it('returns true if service settings `enableShareLinks` is non-boolean', () => {
+      fakeServiceConfig.returns({ enableShareLinks: 'foo' });
+      assert.isTrue(sharingUtil.sharingEnabled({}));
+    });
+
+    it('returns false if service settings really sets it to `false`', () => {
+      fakeServiceConfig.returns({ enableShareLinks: false });
+      assert.isFalse(sharingUtil.sharingEnabled({}));
     });
   });
 });
