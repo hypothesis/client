89759db90b7246410e6d15477613c8266846c408
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-09-18 11:45:10 +0200

    Update to @hypothesis/frontend-shared 10.3.0

--- dev-server/ui-playground/index.js
+++ dev-server/ui-playground/index.js
@@ -1,3 +1,3 @@
-import { startApp } from '@hypothesis/frontend-shared/lib/pattern-library';
+import { startApp } from '@hypothesis/frontend-shared/pattern-library';
 
 startApp({ baseURL: '/ui-playground' });
