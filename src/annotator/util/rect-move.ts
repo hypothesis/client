b1b9cb08d450d63a35f958fafd0cd42a10510520
Gabriel Morador <gabriel.morador@hypothes.is>
2026-02-09 17:09:58 -0300

    fix: linter

--- src/annotator/util/rect-move.ts
+++ src/annotator/util/rect-move.ts
@@ -42,8 +42,6 @@ export function applyMoveArrowKeyToRect(
   viewport: ViewportBounds,
 ): Rect {
   const { left, top, right, bottom } = rect;
-  const width = right - left;
-  const height = bottom - top;
 
   switch (key) {
     case 'ArrowUp': {
